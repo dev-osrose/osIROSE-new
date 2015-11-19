daa9a4f6feb5399e07da96c1229b8344e5ab9854
Christopher Torres <christopher.torres@azgstudio.com>
2015-11-19 13:03:28 -0500

    Update unittest to allow us to create tests that fail

--- UnitTest/main.cpp
+++ UnitTest/main.cpp
@@ -9,11 +9,37 @@
 #endif
 
 using ::testing::InitGoogleTest;
+using ::testing::Test;
+using ::testing::TestCase;
+using ::testing::TestInfo;
+using ::testing::UnitTest;
 
 int main( int argc, char* argv[] )
 {
 	InitGoogleTest(&argc, argv);
-	//TODO: actually use the arg's to enable and disable tests
 
-	return RUN_ALL_TESTS();
+	UnitTest& unit_test = *UnitTest::GetInstance();
+
+	int ret_val = RUN_ALL_TESTS();
+	int unexpectedly_failed_tests = 0;
+	for (int i = 0; i < unit_test.total_test_case_count(); ++i)
+	{
+		const TestCase& test_case = *unit_test.GetTestCase(i);
+		for (int j = 0; j < test_case.total_test_count(); ++j)
+		{
+			const TestInfo& test_info = *test_case.GetTestInfo(j);
+			// Counts failed tests that were not meant to fail (those without
+			// 'Fails' in the name).
+			if (test_info.result()->Failed() && strcmp(test_info.name(), "Fails") != 0)
+			{
+				unexpectedly_failed_tests++;
+			}
+		}
+	}
+
+	// Test that were meant to fail should not affect the test program outcome.
+	if (unexpectedly_failed_tests == 0)
+		ret_val = 0;
+
+	return ret_val;
 }
