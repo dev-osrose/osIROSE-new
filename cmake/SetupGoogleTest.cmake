1bfa68cff75e27c2f1a7203c11d9f2a83ded42b0
Christopher Torres <christopher.torres@azgstudio.com>
2015-11-20 13:41:42 -0500

    They build correctlygit add -A

--- cmake/SetupGoogleTest.cmake
+++ cmake/SetupGoogleTest.cmake
@@ -2,29 +2,7 @@ INCLUDE(Compiler)
 INCLUDE(ExternalProject)
 
 FUNCTION(SETUP_GOOGLE_TEST)
-    SET_DIRECTORY_PROPERTIES(properties EP_PREFIX "${CMAKE_BINARY_DIR}/tools")
-    ExternalProject_ADD(googletest
-        GIT_SUBMODULES googletest
-        GIT_REPOSITORY https://github.com/google/googletest.git
-        SOURCE_DIR "${CMAKE_BINARY_DIR}/tools/googletest/googletest"
-        CMAKE_ARGS "-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}" "-DCMAKE_CXX_FLAGS=-fPIC"
-        INSTALL_COMMAND ""
-    )
-    ExternalProject_Add_Step(googletest enter_dir
-    	COMMAND mv -f ${CMAKE_BINARY_DIR}/tools/googletest/googletest ${CMAKE_BINARY_DIR}/tools/temp_move/
-    	COMMAND rm -r ${CMAKE_BINARY_DIR}/tools/googletest
-    	COMMAND mv -f ${CMAKE_BINARY_DIR}/tools/temp_move/ ${CMAKE_BINARY_DIR}/tools/googletest/
-    	COMMENT "Fixing google test directories"
-    	DEPENDEES mkdir update patch download 
-    	DEPENDERS configure build install
-    )
-    
-    ExternalProject_GET_PROPERTY(googletest SOURCE_DIR)
-    ExternalProject_GET_PROPERTY(googletest BINARY_DIR)
-
-    SET(GTEST_INCLUDE_DIR ${SOURCE_DIR}/include PARENT_SCOPE)
-    SET(GTEST_BINARY_DIR ${BINARY_DIR} PARENT_SCOPE)
-    
+   
 #    SET_DIRECTORY_PROPERTIES(properties EP_PREFIX "${CMAKE_BINARY_DIR}/tools")
 #    ExternalProject_ADD(googlemock
 #        GIT_SUBMODULES googletest
--- cmake/SetupProtobuf.cmake
+++ cmake/SetupProtobuf.cmake
@@ -2,17 +2,5 @@ INCLUDE(Compiler)
 INCLUDE(ExternalProject)
 
 FUNCTION(SETUP_PROTOBUF)
-    SET_DIRECTORY_PROPERTIES(properties EP_PREFIX "${CMAKE_BINARY_DIR}/tools")
-    ExternalProject_ADD(protobuf
-        GIT_SUBMODULES protobuf
-        GIT_REPOSITORY https://github.com/google/protobuf.git
-        SOURCE_DIR "${CMAKE_BINARY_DIR}/tools/protobuf"
-        CMAKE_ARGS "-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}" "-DCMAKE_CXX_FLAGS=-fPIC"
-        INSTALL_COMMAND ""
-    )
-    ExternalProject_GET_PROPERTY(protobuf SOURCE_DIR)
-    ExternalProject_GET_PROPERTY(protobuf BINARY_DIR)
 
-    SET(PROTOBUF_INCLUDE_DIR ${SOURCE_DIR}/gtest/include PARENT_SCOPE)
-    SET(PROTOBUF_BINARY_DIR ${BINARY_DIR}/gtest PARENT_SCOPE)
 ENDFUNCTION()
