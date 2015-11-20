4cedd47487a16c9abda8da686863fb36ad93fecb
Christopher Torres <christopher.torres@azgstudio.com>
2015-11-20 12:37:29 -0500

    Added cmake helpers

---
+++ cmake/SetupGoogleTest.cmake
@@ -0,0 +1,40 @@
+INCLUDE(Compiler)
+INCLUDE(ExternalProject)
+
+FUNCTION(SETUP_GOOGLE_TEST)
+    SET_DIRECTORY_PROPERTIES(properties EP_PREFIX "${CMAKE_BINARY_DIR}/tools")
+    ExternalProject_ADD(googletest
+        GIT_SUBMODULES googletest
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        SOURCE_DIR "${CMAKE_BINARY_DIR}/tools/googletest/googletest"
+        CMAKE_ARGS "-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}" "-DCMAKE_CXX_FLAGS=-fPIC"
+        INSTALL_COMMAND ""
+    )
+    ExternalProject_Add_Step(googletest enter_dir
+    	COMMAND mv -f ${CMAKE_BINARY_DIR}/tools/googletest/googletest ${CMAKE_BINARY_DIR}/tools/googletest/
+    	COMMENT "--------------------------"
+    	DEPENDERS configure
+    	ALWAYS 1
+    )
+    
+    ExternalProject_GET_PROPERTY(googletest SOURCE_DIR)
+    ExternalProject_GET_PROPERTY(googletest BINARY_DIR)
+
+    SET(GTEST_INCLUDE_DIR ${SOURCE_DIR}/gtest/include PARENT_SCOPE)
+    SET(GTEST_BINARY_DIR ${BINARY_DIR}/gtest PARENT_SCOPE)
+    
+    
+#    SET_DIRECTORY_PROPERTIES(properties EP_PREFIX "${CMAKE_BINARY_DIR}/tools")
+#    ExternalProject_ADD(googlemock
+#        GIT_SUBMODULES googletest
+#        GIT_REPOSITORY https://github.com/google/googletest.git
+#        SOURCE_DIR "${CMAKE_BINARY_DIR}/tools/googletest/googlemock"
+#        CMAKE_ARGS "-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}" "-DCMAKE_CXX_FLAGS=-fPIC"
+#        INSTALL_COMMAND ""
+#    )
+#    ExternalProject_GET_PROPERTY(googlemock SOURCE_DIR)
+#    ExternalProject_GET_PROPERTY(googlemock BINARY_DIR)
+    
+#    SET(GMOCK_INCLUDE_DIR ${SOURCE_DIR}/include PARENT_SCOPE)
+#    SET(GMOCK_BINARY_DIR ${BINARY_DIR} PARENT_SCOPE)
+ENDFUNCTION()
