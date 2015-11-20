2c844189e2b1837a30e4dbe5888d16a9482defa0
Christopher Torres <christopher.torres@azgstudio.com>
2015-11-20 14:23:17 -0500

    Updated install script

--- ci/install.sh
+++ ci/install.sh
@@ -5,15 +5,12 @@ if [ -z "$TRAVIS_OS_NAME" ]; then
 fi
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    sudo apt-get install -qq cdbs cmake libboost-dev lcov libmysqlclient-dev mysql-community-source
-    gem install coveralls-lcov
+	sudo apt-get install -qq cdbs cmake libboost-dev lcov libmysqlclient-dev mysql-community-source
+	gem install coveralls-lcov
     
-    mkdir 3rdParty
+	mkdir 3rdParty
 	mkdir 3rdParty/include
 	mkdir 3rdParty/lib
-	
-elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
-
 else
     echo "Unknown OS ($TRAVIS_OS_NAME). Stopping build ..."
     exit 1
