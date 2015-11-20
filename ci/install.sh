067c2ecbe6475bd04d8b91ec6fd99514d8a4cdd4
Christopher Torres <christopher.torres@azgstudio.com>
2015-11-20 14:48:25 -0500

    tar.gz type

--- ci/install.sh
+++ ci/install.sh
@@ -7,7 +7,7 @@ fi
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
 
 	wget http://downloads.sourceforge.net/ltp/lcov-1.12.tar.gz
-	tar -xf lcov-1.12
+	tar -xf lcov-1.12.tar.gz
 	cd lcov-1.12
 	make install
 
