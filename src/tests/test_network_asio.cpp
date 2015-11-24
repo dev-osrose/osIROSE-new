46cdcb53b594d3eb9a300b286b88bf7dd16efc4c
Christopher Torres <christopher.torres@azgstudio.com>
2015-11-23 20:41:50 -0500

    Updated network test. May need some waits in there to make sure connections were actually made.

--- src/tests/test_network_asio.cpp
+++ src/tests/test_network_asio.cpp
@@ -23,3 +23,24 @@ TEST( NetworkingASIO, TestConnect )
 	EXPECT_NO_FATAL_FAILURE( network.Disconnect( ) );
 	EXPECT_NO_FATAL_FAILURE( network.Shutdown( ) );
 }
+
+TEST( NetworkingASIO, TestListen )
+{
+	EXPECT_EQ( true, network.Init( "0.0.0.0", 29000 ) ); // We are going to connect to google's website
+	EXPECT_NO_FATAL_FAILURE( network.Listen() );
+	EXPECT_NO_FATAL_FAILURE( network.Shutdown() );
+}
+
+//TEST( NetworkingASIO, TestListenAndConnect )
+//{
+//	CNetwork_Asio netConnect;
+//	EXPECT_EQ( true, network.Init( "0.0.0.0", 29000 ) ); // We are going to connect to google's website
+//	EXPECT_NO_FATAL_FAILURE( network.Listen() );
+//
+//	EXPECT_EQ( true, netConnect.Init( "127.0.0.1", 29000 ) );
+//	EXPECT_NO_FATAL_FAILURE( netConnect.Connect( ) );
+//	EXPECT_NO_FATAL_FAILURE( netConnect.Disconnect( ) );
+//	EXPECT_NO_FATAL_FAILURE( netConnect.Shutdown( ) );
+//
+//	EXPECT_NO_FATAL_FAILURE( network.Shutdown() );
+//}
