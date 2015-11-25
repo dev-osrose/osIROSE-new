9bff78730287555f1b147fb9be67419281da2fcf
Christopher Torres <christopher.torres@azgstudio.com>
2015-11-25 10:19:03 -0500

    Updated network mock to call the real functions as well. [ci skip]

--- src/tests/mock/mock_network_asio.h
+++ src/tests/mock/mock_network_asio.h
@@ -1,9 +1,33 @@
 #include "gmock/gmock.h"  // Brings in Google Mock.
 
+using ::testing::_;
+using ::testing::Invoke;
+
 #include "cnetwork_asio.h"
 class CNetwork_Asio_Mock : public CNetwork_Asio
 {
 public:
+	CNetwork_Asio_Mock()
+	{
+		ON_CALL(*this, OnConnected())
+			.WillByDefault(Invoke(&real_, &CNetwork_Asio::OnConnected()));
+		ON_CALL(*this, OnListen())
+					.WillByDefault(Invoke(&real_, &CNetwork_Asio::OnListen()));
+		ON_CALL(*this, OnListening())
+					.WillByDefault(Invoke(&real_, &CNetwork_Asio::OnListening()));
+		ON_CALL(*this, OnDisconnect())
+					.WillByDefault(Invoke(&real_, &CNetwork_Asio::OnDisconnect()));
+		ON_CALL(*this, OnDisconnected())
+					.WillByDefault(Invoke(&real_, &CNetwork_Asio::OnDisconnected()));
+		ON_CALL(*this, OnReceive())
+					.WillByDefault(Invoke(&real_, &CNetwork_Asio::OnReceive()));
+		ON_CALL(*this, OnReceived(_))
+					.WillByDefault(Invoke(&real_, &CNetwork_Asio::OnReceived()));
+		ON_CALL(*this, OnSend(_))
+					.WillByDefault(Invoke(&real_, &CNetwork_Asio::OnSend()));
+		ON_CALL(*this, OnSent())
+					.WillByDefault(Invoke(&real_, &CNetwork_Asio::OnSent()));
+	}
 //	MOCK_METHOD0( OnConnect, bool());
 	MOCK_METHOD0( OnConnected, bool() );
 	MOCK_METHOD0( OnListen, bool() );
@@ -14,4 +38,7 @@ public:
 	MOCK_METHOD2( OnReceived, bool(uint8_t* _buffer, uint16_t _size) );
 	MOCK_METHOD2( OnSend, bool(uint8_t* _buffer, uint16_t _size) );
 	MOCK_METHOD0( OnSent, bool() );
+
+private:
+	CNetwork_Asio real_;
 };
