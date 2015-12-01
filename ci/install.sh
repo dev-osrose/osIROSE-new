1d49da2e51ec7cb6ae3b381193896ac13996e527
Christopher Torres <RavenX8@users.noreply.github.com>
2015-12-01 12:59:00 -0500

    Update install.sh

--- ci/install.sh
+++ ci/install.sh
@@ -6,6 +6,18 @@ fi
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
 
+	if [ ! -d "$HOME/protobuf/lib" ]; then
+		cd tools/protobuf
+		./autogen.sh
+		./configure --prefix=$HOME/protobuf
+		make
+		make install
+		
+		cd ../../
+	else
+  		echo 'Using cached protobuf directory.';
+	fi
+	
 	wget http://downloads.sourceforge.net/ltp/lcov-1.12.tar.gz
 	tar -xf lcov-1.12.tar.gz
 	cd lcov-1.12
@@ -17,19 +29,6 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
 	mkdir 3rdParty
 	mkdir 3rdParty/include
 	mkdir 3rdParty/lib
-	
-	if [ ! -d "$HOME/protobuf/lib" ]; then
-		cd tools/protobuf
-		./autogen.sh
-		./configure --prefix=$HOME/protobuf > nul
-		make
-		#make check
-		make install
-	else
-  		echo 'Using cached directory.';
-	fi
-
-	cd ../../
 else
     echo "Unknown OS ($TRAVIS_OS_NAME). Stopping build ..."
     exit 1
