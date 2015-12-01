d679d2e23a6122c9d5a14c7466578030a3f84e40
Christopher Torres <RavenX8@users.noreply.github.com>
2015-12-01 10:26:23 -0500

    Update install.sh

--- ci/install.sh
+++ ci/install.sh
@@ -6,26 +6,31 @@ fi
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
 
-	wget http://downloads.sourceforge.net/ltp/lcov-1.12.tar.gz
-	tar -xf lcov-1.12.tar.gz
-	cd lcov-1.12
-	sudo make install
+	#wget http://downloads.sourceforge.net/ltp/lcov-1.12.tar.gz
+	#tar -xf lcov-1.12.tar.gz
+	#cd lcov-1.12
+	#sudo make install
 
 	cd ..
-	sudo apt-get install -qq cdbs cmake libboost-dev libmysqlclient-dev
+	#sudo apt-get install -qq cdbs cmake libboost-dev libmysqlclient-dev
 	gem install coveralls-lcov
     
 	mkdir 3rdParty
 	mkdir 3rdParty/include
 	mkdir 3rdParty/lib
 	
-	cd tools/protobuf
-	./autogen.sh
-	./configure --prefix=/usr > nul
-	make
-	#make check
-	sudo make install
+	if [ ! -d "$HOME/protobuf/lib" ]; then
+		cd tools/protobuf
+		./autogen.sh
+		./configure --prefix=$HOME/protobuf > nul
+		make
+		#make check
+		make install
+	else
+  		echo 'Using cached directory.';
+	fi
 	
+	ls -R $HOME/protobuf
 	cd ../../
 else
     echo "Unknown OS ($TRAVIS_OS_NAME). Stopping build ..."
