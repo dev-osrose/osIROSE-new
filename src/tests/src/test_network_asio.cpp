267c0a985152c981c3308358f244f7a6d708c1fa
Christopher Torres <christopher.torres@azgstudio.com>
2015-12-02 00:12:46 -0500

    Updated project to link to gmock Clearing cache Deleted unused bash script

--- src/tests/src/test_network_asio.cpp
+++ src/tests/src/test_network_asio.cpp
@@ -12,53 +12,23 @@ TEST( TestAsioNetworking, TestInit )
 {
 	CNetwork_Asio network;
 	EXPECT_EQ( true, network.Init( "127.0.0.1", 29000 ) );
-	EXPECT_EQ( 29000, network.GetPort() );
-        EXPECT_EQ( "127.0.0.1", network.GetIpAddress() );
+	EXPECT_EQ( 29000, network.GetPort( ) );
+	EXPECT_EQ( "127.0.0.1", network.GetIpAddress( ) );
 }
 
 TEST( TestAsioNetworking, TestInitHostLessThanTwo )
 {
 	CNetwork_Asio network;
 	EXPECT_EQ( false, network.Init( "0", 29000 ) );
-	EXPECT_NE( 29000, network.GetPort() );
-	EXPECT_NE( "0", network.GetIpAddress() );
+	EXPECT_NE( 29000, network.GetPort( ) );
+	EXPECT_NE( "0", network.GetIpAddress( ) );
 }
 
 TEST( TestAsioNetworking, TestConnectIp )
 {
-//	std::mutex mutex;
-//	std::condition_variable cv;
-//	bool done = false;
-
 	CNetwork_Asio network;
 	EXPECT_EQ( true, network.Init( "63.117.14.24", 80 ) ); // We are going to connect to google's website
 	EXPECT_NO_FATAL_FAILURE( network.Connect( ) );
-//	EXPECT_CALL( network, OnConnect() )
-//                                .WillOnce(testing::Invoke([&]()->int {
-//                                            std::lock_guard<std::mutex> lock(mutex);
-//                                            done = true;
-//                                            cv.notify_all();
-//                                            return 1; }));
-
-//	EXPECT_CALL( network, OnConnected() )
-//				.WillOnce(testing::Invoke([&]()->int {
-//				            std::lock_guard<std::mutex> lock(mutex);
-//				            done = true;
-//				            cv.notify_all();
-//				            return 1; }));
-
-
-//	std::unique_lock<std::mutex> lock(mutex);
-//	std::unique_lock<std::mutex> lock2(mutex2);
-//	EXPECT_TRUE(cv.wait_for(lock,
-//					std::chrono::seconds(1),
-//					[&done] { return done; })
-//					);
-//	EXPECT_TRUE(cv.wait_for(lock,
-//                                        std::chrono::seconds(1),
-//                                        [&done] { return done; })
-//                                        );
-//	std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	EXPECT_NO_FATAL_FAILURE( network.Shutdown( ) );
 }
 
@@ -67,95 +37,49 @@ TEST( TestAsioNetworking, TestRecv )
 	CNetwork_Asio network;
 	EXPECT_EQ( true, network.Init( "63.117.14.24", 80 ) );
 	EXPECT_NO_FATAL_FAILURE( network.Connect( ) );
-	std::this_thread::sleep_for(std::chrono::milliseconds(500)); // Make sure we wait a little for data to come in
 	EXPECT_NO_FATAL_FAILURE( network.Shutdown( ) );
 }
 
 TEST( TestAsioNetworking, TestReconnect )
 {
 	CNetwork_Asio network;
-        EXPECT_EQ( true, network.Init( "63.117.14.24", 80 ) ); // We are going to connect to google's website
-        EXPECT_NO_FATAL_FAILURE( network.Connect( ) );
+	EXPECT_EQ( true, network.Init( "63.117.14.24", 80 ) ); // We are going to connect to google's website
+	EXPECT_NO_FATAL_FAILURE( network.Connect( ) );
 	EXPECT_NO_FATAL_FAILURE( network.Disconnect( ) );
-	std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	EXPECT_NO_FATAL_FAILURE( network.Reconnect( ) );
 	EXPECT_NO_FATAL_FAILURE( network.Shutdown( ) );
 }
 
 TEST( TestAsioNetworking, TestConnectHostName )
 {
-//	::testing::FLAGS_gmock_verbose = "info";
-//	std::mutex mutex;
-//        std::condition_variable cv;
-//        bool done = false;
-
-	CNetwork_Asio network;
+	CNetwork_Asio_Mock network;
 	EXPECT_NO_FATAL_FAILURE( network.Init( "google.com", 80 ) ); // We are going to connect to google's website using hostname.
+	EXPECT_CALL( network, OnConnect( ) );
+	EXPECT_CALL( network, OnConnected( ) );
 	EXPECT_NO_FATAL_FAILURE( network.Connect( ) );
-//	EXPECT_CALL( network, OnConnect() ).Times(1);
-//	EXPECT_CALL( network, OnConnected() )
-//                                .WillOnce(testing::Invoke([&]()->int {
-//                                            std::lock_guard<std::mutex> lock(mutex);
-//                                            done = true;
-//                                            cv.notify_all();
-//                                            return 1; }));
-	std::this_thread::sleep_for(std::chrono::milliseconds(500));
-//	std::unique_lock<std::mutex> lock(mutex);
-//	EXPECT_TRUE(cv.wait_for(lock,
-//                                        std::chrono::seconds(4),
-//                                        [&done] { return done; })
-//                                        );
 	EXPECT_NO_FATAL_FAILURE( network.Shutdown( ) );
 }
 
 TEST( TestAsioNetworking, TestListen )
 {
-//	::testing::FLAGS_gmock_verbose = "info";
-//	std::mutex mutex;
-//        std::condition_variable cv;
-//        bool done = false;
-
 	CNetwork_Asio network;
 	EXPECT_EQ( true, network.Init( "127.0.0.1", 23124 ) ); // We are going to connect to google's website
-	EXPECT_NO_FATAL_FAILURE( network.Listen() );
-//	EXPECT_CALL( network, OnListening() )
-//                                        .WillOnce( testing::Invoke([&]()->int {
-//                                                        std::lock_guard<std::mutex> lock(mutex);
-//                                                        done = true;
-//                                                        cv.notify_all();
-//                                                        return 1; }));
-
-//	std::unique_lock<std::mutex> lock(mutex);
-//        EXPECT_TRUE(cv.wait_for(lock, std::chrono::seconds(1), [&done] { return done; }));
-
-	EXPECT_NO_FATAL_FAILURE( network.Shutdown() );
+	EXPECT_NO_FATAL_FAILURE( network.Listen( ) );
+	EXPECT_NO_FATAL_FAILURE( network.Shutdown( ) );
 }
 
 TEST( TestAsioNetworking, TestListenAndConnect )
 {
-//	std::mutex mutex;
-//	std::condition_variable cv;
-//	bool done = false;
-
 	CNetwork_Asio netConnect, network;
 	EXPECT_EQ( true, network.Init( "127.0.0.1", 23456 ) ); // We are going to connect to google's website
-	EXPECT_NO_FATAL_FAILURE( network.Listen() );
-//	EXPECT_CALL( network, OnListening() )
-//					.WillOnce( testing::Invoke([&]()->int {
-//							std::lock_guard<std::mutex> lock(mutex);
-//							done = true;
-//							cv.notify_all();
-//							return 1; }));
+	EXPECT_NO_FATAL_FAILURE( network.Listen( ) );
 
-//	std::unique_lock<std::mutex> lock(mutex);
-//	EXPECT_TRUE(cv.wait_for(lock, std::chrono::seconds(1), [&done] { return done; }));
-
-	std::this_thread::sleep_for(std::chrono::milliseconds(500));
+	std::this_thread::sleep_for( std::chrono::milliseconds( 100 ) );
 	EXPECT_EQ( true, netConnect.Init( "127.0.0.1", 23456 ) );
 	EXPECT_NO_FATAL_FAILURE( netConnect.Connect( ) );
-	std::this_thread::sleep_for(std::chrono::milliseconds(500)); // Change this to condition variables
+	std::this_thread::sleep_for( std::chrono::milliseconds( 100 ) ); // Change this to condition variables
 	EXPECT_NO_FATAL_FAILURE( netConnect.Disconnect( ) );
 	EXPECT_NO_FATAL_FAILURE( netConnect.Shutdown( ) );
 
-	EXPECT_NO_FATAL_FAILURE( network.Shutdown() );
+	EXPECT_NO_FATAL_FAILURE( network.Shutdown( ) );
 }
