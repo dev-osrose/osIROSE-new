53dcb9c5758c55ebeb4a55ac3cffdec21c08fbbc
Christopher Torres <christopher.torres@azgstudio.com>
2015-12-07 18:41:57 -0500

    Fix for appveyor

---
+++ ci/msvc_install.bat
@@ -0,0 +1,2 @@
+Robocopy "./tools/googletest/googlemock" "./tools/protobuf/gmock" /E /MT:2 /R:0 /DCOPY:T
+Robocopy "./tools/googletest/googletest" "./tools/protobuf/gmock/gtest" /E /MT:2 /R:0 /DCOPY:T
