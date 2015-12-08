681dc4cde0c8dfbabcb257c6e4647ed09e7181ee
Christopher Torres <christopher.torres@azgstudio.com>
2015-12-07 19:05:25 -0500

    Fix appveyor failing on a successful robocopy

--- ci/msvc_install.bat
+++ ci/msvc_install.bat
@@ -1,2 +1,8 @@
 Robocopy "./tools/googletest/googlemock" "./tools/protobuf/gmock" /E /MT:2 /R:0 /DCOPY:T
 Robocopy "./tools/googletest/googletest" "./tools/protobuf/gmock/gtest" /E /MT:2 /R:0 /DCOPY:T
+IF %ERRORLEVEL% LSS 8 goto end
+goto :eof
+
+:end
+exit 0
+
