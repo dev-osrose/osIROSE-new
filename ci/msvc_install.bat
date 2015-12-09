d4f0d22283b417539af4459a39a45da28173ad9b
Christopher Torres <christopher.torres@azgstudio.com>
2015-12-09 11:53:23 -0500

    Fix for appveyor failing due to the build dir already exsiting

--- ci/msvc_install.bat
+++ ci/msvc_install.bat
@@ -1,3 +1,4 @@
+mkdir build
 Robocopy "./tools/googletest/googlemock" "./tools/protobuf/gmock" /E /MT:2 /R:0 /DCOPY:T
 Robocopy "./tools/googletest/googletest" "./tools/protobuf/gmock/gtest" /E /MT:2 /R:0 /DCOPY:T
 IF %ERRORLEVEL% LSS 8 goto end
