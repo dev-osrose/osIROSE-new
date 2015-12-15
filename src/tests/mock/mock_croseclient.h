c1fc1d57fb70075fbbe237a0a63592b16d10850a
Christopher Torres <christopher.torres@azgstudio.com>
2015-12-15 11:02:22 -0500

    Added missing mock class

---
+++ src/tests/mock/mock_croseclient.h
@@ -0,0 +1,25 @@
+#ifndef __MOCK_CROSECLIENT_H__
+#define __MOCK_CROSECLIENT_H__
+
+#include "gmock/gmock.h" // Brings in Google Mock.
+
+using ::testing::_;
+using ::testing::Invoke;
+
+#include "croseclient.h"
+class CRoseClient_Mock : public CRoseClient
+{
+        public:
+        protected:
+
+	virtual bool OnSend( uint8_t* _buffer )
+	{
+		m_Crypt.encodeClientPacket( _buffer );
+		return true;
+	}
+
+        private:
+        CNetwork_Asio real_;
+};
+
+#endif
