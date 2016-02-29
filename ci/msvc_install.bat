64f8b4caa9aff53af0ee7cad3894fea4d5e9c3bf
Christopher Torres <christopher.torres@azgstudio.com>
2016-02-29 14:21:05 -0500

    update msvc_install.bat

--- ci/msvc_install.bat
+++ ci/msvc_install.bat
@@ -1,9 +1,14 @@
 mkdir build
 Robocopy "./tools/googletest/googlemock" "./tools/protobuf/gmock" /E /MT:2 /R:0 /DCOPY:T
 Robocopy "./tools/googletest/googletest" "./tools/protobuf/gmock/gtest" /E /MT:2 /R:0 /DCOPY:T
-IF %ERRORLEVEL% LSS 8 goto end
+IF %ERRORLEVEL% LSS 8 goto build
 goto :eof
 
-:end
+:build
+mkdir protobuf
+cd protobuf
+cmake ../tools/protobuf/cmake .
+cmake --build . --config %CONFIGURATION% -- /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+dir
 exit 0
 
