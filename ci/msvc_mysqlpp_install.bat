5fbde33c3cc4483f2f9fecd588d55ef1e69bfdd4
Christopher Torres <christopher.torres@azgstudio.com>
2016-03-01 12:56:56 -0500

    Added files to compile to the cmake file. Fixed typo in bat file.

--- ci/msvc_mysqlpp_install.bat
+++ ci/msvc_mysqlpp_install.bat
@@ -2,7 +2,7 @@ cd C:/projects/
 7z e mysqlpp.tar.gz && 7z x mysqlpp.tar -pC:\projects\mysqlpp -y > C:\projects\mysqlpp_install.txt
 mkdir C:/projects/osIROSE/mysqlpp
 Robocopy "C:/projects/mysql++-3.2.2/lib" "C:/projects/osIROSE/mysqlpp" /E /MT:2 /R:0 /DCOPY:T
-copy "C:\projects\osIROSE\cmake\mysqlpp_CMakeFile.txt" "C:\projects\osIROSE\mysqlpp\CMakeFile.txt" /Y
+copy "C:\projects\osIROSE\cmake\mysqlpp_CMakeFile.txt" "C:\projects\osIROSE\mysqlpp\CMakeLists.txt" /Y
 IF %ERRORLEVEL% LSS 8 goto build
 goto :eof
 
