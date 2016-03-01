22144d0cfd220d47d8ec5e568f2c5abc9e9ea776
Christopher Torres <RavenX8@users.noreply.github.com>
2016-03-01 15:12:22 -0500

    Update msvc_install.bat

--- ci/msvc_install.bat
+++ ci/msvc_install.bat
@@ -1,5 +1,5 @@
 mkdir build
-mkdir 3rdParty/build
+mkdir 3rdParty\build
 Robocopy "./tools/googletest/googlemock" "./tools/protobuf/gmock" /E /MT:2 /R:0 /DCOPY:T
 Robocopy "./tools/googletest/googletest" "./tools/protobuf/gmock/gtest" /E /MT:2 /R:0 /DCOPY:T
 IF %ERRORLEVEL% LSS 8 goto build
