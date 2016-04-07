4f9f03f9ebb685405bdb5006a5fd7579b14497a4
Christopher Torres <christopher.torres@azgstudio.com>
2016-04-07 10:39:57 -0400

    Created a new server test

---
+++ src/tests/src/test_servers.cpp
@@ -0,0 +1,55 @@
+#include "gtest/gtest.h"
+
+#include <stdint.h>
+#include "epackettype.h"
+#include "crosepacket.h"
+#include "cloginserver.h"
+#include "ccharserver.h"
+#include "cmapserver.h"
+
+#include "ccharisc.h"
+#include "cmapisc.h"
+
+using namespace RoseCommon;
+
+TEST(TestFinalServers, TestISCConnections) {
+  CLoginServer loginIsc(true);
+  CCharServer charIsc(true);
+  CCharISC* charIscClient = new CCharISC();
+  CMapServer mapIsc(true);
+  CMapISC* mapIscClient = new CMapISC();
+
+  loginIsc.Init("127.0.0.1", 29010);
+
+  charIsc.Init("127.0.0.1", 29110);
+  charIscClient->Init("127.0.0.1", 29010);
+  charIscClient->SetLogin(true);
+
+  mapIsc.Init("127.0.0.1", 29210);
+  mapIscClient->Init("127.0.0.1", 29110);
+  mapIscClient->SetChar(true);
+
+  loginIsc.Listen();
+  charIsc.Listen();
+  mapIsc.Listen();
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+  CCharServer::GetISCList().push_front(charIscClient);
+  CMapServer::GetISCList().push_front(mapIscClient);
+
+  charIscClient->Connect();
+//  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  mapIscClient->Connect();
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+
+  mapIscClient->Shutdown();
+  charIscClient->Shutdown();
+
+  mapIsc.Shutdown();
+  charIsc.Shutdown();
+  loginIsc.Shutdown();
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+}
