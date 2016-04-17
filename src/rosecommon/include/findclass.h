1c5e97166b73536a8a06aa1647660b6e63bb5713
Christopher Torres <christopher.torres@azgstudio.com>
2016-04-17 18:55:05 -0400

    Added new packets

--- src/rosecommon/include/findclass.h
+++ src/rosecommon/include/findclass.h
@@ -13,11 +13,20 @@ struct find_recv_class {};
 template <>
 struct find_recv_class<ePacketType::PAKCS_LOGIN_REQ> { CliLoginReq type; };
 
+template <>
+struct find_recv_class<ePacketType::PAKCS_CHANNEL_LIST_REQ> { CliChannelReq type; };
+
+//template <>
+//struct find_recv_class<ePacketType::PAKCS_SRV_SELECT_REQ> { CliLoginReq type; };
+
 template <ePacketType T>
 struct find_send_class {};
 
 template <>
 struct find_send_class<ePacketType::PAKLC_LOGIN_REPLY> { SrvLoginReply type; };
+
+template <>
+struct find_send_class<ePacketType::PAKLC_CHANNEL_LIST_REPLY> { SrvChannelReply type; };
 }
 
 #endif /* !_FINDCLASS_H_ */
--- src/rosecommon/include/packetclasses.h
+++ src/rosecommon/include/packetclasses.h
@@ -31,6 +31,33 @@ class CliLoginReq : public CRosePacket {
 		std::string username_;
 };
 
+class CliChannelReq : public CRosePacket
+{
+public:
+  CliChannelReq( uint8_t buffer[MAX_PACKET_SIZE] ) : CRosePacket( buffer )
+  {
+    if (type() != ePacketType::PAKCS_CHANNEL_LIST_REQ)
+      throw std::runtime_error( "Not the right packet!" );
+    *this >> server_id_;
+  }
+
+  virtual ~CliChannelReq()
+  {
+  }
+
+  uint32_t server_id() const
+  {
+    return server_id_;
+  }
+
+private:
+  uint32_t server_id_;
+};
+
+//-----------------------------------------------
+// Send Packets
+//-----------------------------------------------
+
 class SrvLoginReply : public CRosePacket {
 	public:
 		SrvLoginReply(uint8_t result, uint16_t right, uint16_t type) : CRosePacket(ePacketType::PAKLC_LOGIN_REPLY),
@@ -55,6 +82,27 @@ class SrvLoginReply : public CRosePacket {
 		uint8_t result_;
 		uint16_t right_;
 		uint16_t type_;
+};
+
+class SrvChannelReply : public CRosePacket {
+	public:
+		SrvChannelReply(uint32_t serverid, uint8_t count) : CRosePacket(ePacketType::PAKLC_CHANNEL_LIST_REPLY),
+		server_id_(serverid), channel_count_(count) {
+			*this << server_id_ << channel_count_;
+		}
+
+		virtual ~SrvChannelReply() {}
+
+		uint8_t		server_id() const {return server_id_;}
+		uint16_t	channel_count() const {return channel_count_;}
+
+		void	addChannel(const std::string &name, uint8_t id, uint16_t user_capacity_percentage, uint8_t low_age = 0, uint8_t high_age = 0) {
+			*this << id << low_age << high_age << user_capacity_percentage << name;
+		}
+
+	private:
+		uint32_t server_id_;
+		uint8_t channel_count_;
 };
 
 }
