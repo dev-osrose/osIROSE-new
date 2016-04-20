c0c888e2e974f6e4032b8776304066f72c0b8826
L3nn0x <dragon.super@laposte.net>
2016-04-20 14:05:28 +0000

    Used the database instead of the database pool

--- src/tests/src/test_mysql_database.cpp
+++ src/tests/src/test_mysql_database.cpp
@@ -190,17 +190,17 @@ TEST(TestMySQL_Database, TestSQLEscape) {
 	struct Filename {
 		constexpr static const char *str() { return "test.ini"; }
 	};
-	auto &pool = Core::databasePoolFilename<Filename>::getInstance();
+	auto &database = Core::databasePoolFilename<Filename>::getInstance().getDatabase();
 	EXPECT_EQ(Core::CMySQL_Database::escapeDataNoConnection("test"), "test");
 	EXPECT_EQ(Core::CMySQL_Database::escapeDataNoConnection("1test1"), "1test1");
 	EXPECT_EQ(Core::CMySQL_Database::escapeDataNoConnection("\\test1"), "\\\\test1");
 	EXPECT_EQ(Core::CMySQL_Database::escapeDataNoConnection("'test1"), "\\'test1");
 	EXPECT_EQ(Core::CMySQL_Database::escapeDataNoConnection("\"test1"), "\\\"test1");
 	EXPECT_EQ(Core::CMySQL_Database::escapeDataNoConnection("\%test1"), "\\\%test1");
-	EXPECT_EQ(pool.escapeData("test"), "test");
-	EXPECT_EQ(pool.escapeData("1test1"), "1test1");
-	EXPECT_EQ(pool.escapeData("\\test1"), "\\\\test1");
-	EXPECT_EQ(pool.escapeData("'test1"), "\\'test1");
-	EXPECT_EQ(pool.escapeData("\"test1"), "\\\"test1");
-	EXPECT_EQ(pool.escapeData("\%test1"), "\\\%test1");
+	EXPECT_EQ(database.escapeData("test"), "test");
+	EXPECT_EQ(database.escapeData("1test1"), "1test1");
+	EXPECT_EQ(database.escapeData("\\test1"), "\\\\test1");
+	EXPECT_EQ(database.escapeData("'test1"), "\\'test1");
+	EXPECT_EQ(database.escapeData("\"test1"), "\\\"test1");
+	EXPECT_EQ(database.escapeData("\%test1"), "\\\%test1");
 }
