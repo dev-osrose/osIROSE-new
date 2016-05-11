43571eaad2a9fe88ccf4d5f8b689f7dbeca2516d
Christopher Torres <christopher.torres@azgstudio.com>
2016-05-11 18:31:12 -0400

    Correct mistake in windows bat file

--- ci/msvc_install.bat
+++ ci/msvc_install.bat
@@ -1,8 +1,8 @@
 mkdir build
-#mkdir 3rdparty\build
+REM mkdir 3rdparty\build
 Robocopy "./tools/googletest/googlemock" "./tools/protobuf/gmock" /E /MT:2 /R:0 /DCOPY:T
 Robocopy "./tools/googletest/googletest" "./tools/protobuf/gmock/gtest" /E /MT:2 /R:0 /DCOPY:T
-#Robocopy "./tools/spdlog/include" "./3rdparty/include" /E /MT:2 /R:0 /DCOPY:T
+REM Robocopy "./tools/spdlog/include" "./3rdparty/include" /E /MT:2 /R:0 /DCOPY:T
 IF %ERRORLEVEL% LSS 8 goto build
 goto :eof
 
