ce6fad62f4cf78eed11277f752c22c76fd6cec8d
Christopher Torres <christopher.torres@azgstudio.com>
2016-05-12 21:37:07 -0400

    possible fix for linux

--- ci/script.sh
+++ ci/script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$SUITE" = "tests" ]; then
-	cmake -DENABLE_TESTING=ON -DENABLE_TESTING_COVERAGE=ON .. && make
+	cmake -DENABLE_TESTING=ON -DENABLE_TESTING_COVERAGE=ON -DBUILD_MYSQLPP .. && make
 	make coverage
 	coveralls-lcov coverage.info.cleaned
 else
