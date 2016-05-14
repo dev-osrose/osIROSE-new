6444a75922025055efb4308dfb5a1d06b69e20e6
Christopher Torres <christopher.torres@azgstudio.com>
2016-05-14 18:37:52 +0000

    Update travis

--- ci/script.sh
+++ ci/script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$SUITE" = "tests" ]; then
-	cmake -DENABLE_TESTING=ON -DENABLE_TESTING_COVERAGE=ON -DBUILD_MYSQLPP=ON .. && make
+	cmake -DENABLE_TESTING=ON -DENABLE_TESTING_COVERAGE=ON -DBUILD_PROTOBUF=OFF -DBUILD_MYSQLPP=OFF .. && make
 	make coverage
 	coveralls-lcov coverage.info.cleaned
 else
