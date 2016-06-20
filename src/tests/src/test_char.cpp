f8a24e82dfcf0b50c69956b780aa78982699b5c6
Christopher Torres <christopher.torres@azgstudio.com>
2016-06-20 09:48:51 -0400

    Update character server unit test.

--- src/tests/src/test_char.cpp
+++ src/tests/src/test_char.cpp
@@ -11,18 +11,20 @@
 #include "cmysql_database.h"
 #include "cmysql_databasepool.h"
 #include "database.h"
+#include "mock/login/mock_cloginclient.h"
 
 using namespace RoseCommon;
 
 TEST(TestCharServer, TestClientPacketPath) {
   CCharServer network;
-  CCharClient netConnect;
+  CLoginClient_Mock netConnect;
   EXPECT_EQ(true, network.Init("127.0.0.1", 29112));
   EXPECT_NO_FATAL_FAILURE(network.Listen());
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(50));
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
   EXPECT_EQ(true, netConnect.Init("127.0.0.1", 29112));
   EXPECT_NO_FATAL_FAILURE(netConnect.Connect());
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
   {
     std::string query = fmt::format("CALL CreateSession({}, {}, {});", 1, 1, 0);
@@ -31,29 +33,33 @@ TEST(TestCharServer, TestClientPacketPath) {
     database.QExecute(query);
   }
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(50));
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
   {
     auto pak = std::unique_ptr<CliJoinServerReq>(
         new CliJoinServerReq(1, "cc03e747a6afbbcbf8be7668acfebee5"));
     netConnect.Send(*pak);
   }
+  std::this_thread::sleep_for(std::chrono::milliseconds(50));
   {
     auto pak = std::unique_ptr<CliCreateCharReq>(
         new CliCreateCharReq("Raven", 1, 1, 1, 1, 1, 10));
     netConnect.Send(*pak);
   }
+  std::this_thread::sleep_for(std::chrono::milliseconds(50));
   {
     auto pak =
         std::unique_ptr<CliDeleteCharReq>(new CliDeleteCharReq("Raven", 1, 0));
     netConnect.Send(*pak);
   }
+  std::this_thread::sleep_for(std::chrono::milliseconds(50));
   {
     auto pak = std::unique_ptr<CliSelectCharReq>(
         new CliSelectCharReq("Raven", 1, 0, 0));
     netConnect.Send(*pak);
   }
 
-  std::string query = fmt::format("DELETE FROM sessions WHERE id = {}", 1, 1);
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  std::string query = fmt::format("DELETE FROM sessions WHERE id = {}", 1);
 
   Core::IDatabase& database = Core::databasePool.getDatabase();
   database.QExecute(query);
