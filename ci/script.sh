38aab3d6d01e45a7ef42e18d50a8428dc596f304
Christopher Torres <christopher.torres@azgstudio.com>
2016-09-21 18:00:39 +0000

    Fixed script.sh

--- ci/script.sh
+++ ci/script.sh
@@ -4,7 +4,7 @@ if [ "$SUITE" = "coverage" ]; then
 	cmake -DENABLE_TESTING=ON -DENABLE_TESTING_COVERAGE=ON .. && make
 	make coverage
 	coveralls-lcov coverage.info.cleaned
-else if [ "$SUITE" = "tests" ]; then
+elif [ "$SUITE" = "tests" ]; then
   cmake -DENABLE_TESTING=ON -DENABLE_TESTING_COVERAGE=OFF .. && make
   ctest --output-on-failure
 else
