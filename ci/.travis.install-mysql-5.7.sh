6444fedf5960ff04d70ed6645e3534f9c78ea977
Christopher Torres <christopher.torres@azgstudio.com>
2016-09-21 22:22:12 +0000

    Change the default password for mysql

--- ci/.travis.install-mysql-5.7.sh
+++ ci/.travis.install-mysql-5.7.sh
@@ -11,3 +11,7 @@ sudo dpkg --install mysql-apt-config_0.7.3-1_all.deb
 sudo apt-get update -q
 sudo apt-get install -q -y -o Dpkg::Options::=--force-confnew mysql-server
 sudo mysql_upgrade
+sudo service mysql restart
+
+TEMP_PASSWORD=`sudo cat /var/log/mysql/error.log | grep "A temporary password is generated for" | awk '{print $NF}'`
+sudo mysql -u root -p $TEMP_PASSWORD -e "SET PASSWORD = PASSWORD('');"
