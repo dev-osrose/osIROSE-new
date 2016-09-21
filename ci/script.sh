84daaf620b2cefe06dd859d358425273e5c83a31
Christopher Torres <christopher.torres@azgstudio.com>
2016-09-21 17:34:46 +0000

    Overhauled .travis.yml

--- ci/script.sh
+++ ci/script.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-if [ "$SUITE" = "tests" ]; then
-	cmake -DENABLE_TESTING=ON -DENABLE_TESTING_COVERAGE=ON -DBUILD_PROTOBUF=OFF -DBUILD_MYSQLPP=OFF .. && make
+if [ "$SUITE" = "coverage" ]; then
+	cmake -DENABLE_TESTING=ON -DENABLE_TESTING_COVERAGE=ON .. && make
 	make coverage
 	coveralls-lcov coverage.info.cleaned
+else if [ "$SUITE" = "tests" ]; then
+  cmake -DENABLE_TESTING=ON -DENABLE_TESTING_COVERAGE=OFF .. && make
+  ctest --output-on-failure
 else
 	cmake .. && make
 fi
