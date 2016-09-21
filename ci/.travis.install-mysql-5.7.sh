32a0c90aed915aea8242f717ec292eae7ef97abe
Christopher Torres <christopher.torres@azgstudio.com>
2016-09-21 20:55:58 +0000

    Uninstall mysql 5.5 before installing the new one

--- ci/.travis.install-mysql-5.7.sh
+++ ci/.travis.install-mysql-5.7.sh
@@ -1,3 +1,10 @@
+
+sudo apt-get remove --purge "^mysql.*"
+sudo apt-get autoremove
+sudo apt-get autoclean
+sudo rm -rf /var/lib/mysql
+sudo rm -rf /var/log/mysql
+
 echo mysql-apt-config mysql-apt-config/select-server select mysql-5.7 | sudo debconf-set-selections
 wget http://dev.mysql.com/get/mysql-apt-config_0.7.3-1_all.deb
 sudo dpkg --install mysql-apt-config_0.7.3-1_all.deb
