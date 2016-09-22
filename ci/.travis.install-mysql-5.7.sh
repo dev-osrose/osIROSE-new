7fb9f6507ab03cb376d894c4923f82115be713a7
Christopher Torres <RavenX8@users.noreply.github.com>
2016-09-21 23:17:11 -0400

    Update .travis.install-mysql-5.7.sh

--- ci/.travis.install-mysql-5.7.sh
+++ ci/.travis.install-mysql-5.7.sh
@@ -1,12 +1,12 @@
 
-sudo apt-get remove libmysqlclient-dev libmysqlclient18
-sudo apt-get purge libmysqlclient-dev libmysqlclient18
+#sudo apt-get remove libmysqlclient-dev libmysqlclient18
+#sudo apt-get purge libmysqlclient-dev libmysqlclient18
 
 echo mysql-apt-config mysql-apt-config/select-server select mysql-5.7 | sudo debconf-set-selections
 wget http://dev.mysql.com/get/mysql-apt-config_0.7.3-1_all.deb
 sudo dpkg --install mysql-apt-config_0.7.3-1_all.deb
 sudo apt-get update -q
-sudo apt-get install -q -y -o Dpkg::Options::=--force-confnew mysql-server mysql-client libmysqlclient-dev libmysql++-dev
+sudo apt-get install -q -y -o Dpkg::Options::=--force-confnew mysql-server mysql-client libmysql++-dev
 sudo mysql_upgrade
 #sudo service mysql stop
 
