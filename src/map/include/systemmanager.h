c30616bad83e50c0ebb7481fc7341dfd3819ba95
L3nn0x <dragon.super@laposte.net>
2016-11-02 18:48:29 +0100

    Changed system holder to have faster and safer retrieval

--- src/map/include/systemmanager.h
+++ src/map/include/systemmanager.h
@@ -6,6 +6,7 @@
 #include "crosepacket.h"
 #include "epackettype.h"
 #include "entityComponents.h"
+#include <typeindex>
 
 class System;
 
@@ -15,25 +16,20 @@ class SystemManager {
 
         template <typename T, typename... Args>
         T& add(Args&&... args) {
-            systems_.emplace_back(new T(*this, std::forward<Args>(args)...));
-            return *dynamic_cast<T*>(systems_.back().get());
+            systems_[typeid(T)] = std::make_shared<T>(*this, std::forward<Args>(args)...);
+            return *dynamic_cast<T*>(systems_.at(typeid(T)).get());
         }
 
         template <typename T>
         void remove() {
-            for (auto it = systems_.begin(); it != systems_.end(); ++it) {
-                if (dynamic_cast<T*>(it->get())) {
-                    systems_.erase(it);
-                    break;
-                }
-            }
+            systems_.erase(typeid(T));
         }
 
         template <typename T>
         T* get() {
-            for (auto &it : systems_)
-                if (T *tmp = dynamic_cast<T*>(it.get()))
-                    return tmp;
+            try {
+                return dynamic_cast<T*>(systems_.at(typeid(T)).get());
+            } catch (std::out_of_range) {}
             return nullptr;
         }
 
@@ -57,11 +53,11 @@ class SystemManager {
         template <class T, class U>
         void registerDispatcher(RoseCommon::ePacketType type, void(T::*method)(Entity, const U&)) {
             std::shared_ptr<System> res;
-            for (auto &it : systems_)
-                if (dynamic_cast<T*>(it.get()))
-                    res = it;
-            if (!res)
+            try {
+                res = systems_.at(typeid(T));
+            } catch (std::out_of_range) {
                 return;
+            }
             dispatch_[type].emplace_back([object = std::weak_ptr<System>(res), method](Entity entity, const RoseCommon::CRosePacket &packet) {
                     if (auto system = dynamic_cast<T*>(object.lock().get())) {
                         (system ->* method)(entity, dynamic_cast<const U&>(packet));
@@ -81,7 +77,7 @@ class SystemManager {
 
     private:
         EntityManager &entityManager_;
-        std::vector<std::shared_ptr<System>> systems_;
+        std::unordered_map<std::type_index, std::shared_ptr<System>> systems_;
         std::unordered_map<RoseCommon::ePacketType, std::vector<std::function<bool(Entity, const RoseCommon::CRosePacket&)>>> dispatch_;
 };
 
--- src/map/src/systemmanager.cpp
+++ src/map/src/systemmanager.cpp
@@ -3,5 +3,5 @@
 
 void SystemManager::update(double dt) {
     for (auto &it : systems_)
-        it->update(entityManager_, dt);
+        it.second->update(entityManager_, dt);
 }
