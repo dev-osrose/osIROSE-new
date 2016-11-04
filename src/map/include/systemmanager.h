434f83d74df2020e791bc0e7ae679ab0e26a41be
L3nn0x <dragon.super@laposte.net>
2016-11-04 02:35:52 +0100

    Now using a unordered_multimap for the dispatcher system

--- src/map/include/systemmanager.h
+++ src/map/include/systemmanager.h
@@ -37,22 +37,22 @@ class SystemManager {
         void update(double dt);
 
         bool dispatch(Entity entity, const RoseCommon::CRosePacket &packet) {
-            try {
-                auto res = dispatch_.at(packet.type());
-            } catch (std::out_of_range) {
+            auto res = dispatch_.equal_range(packet.type());
+            if (res.first == dispatch_.end())
                 return false;
+            for (auto it = res.first; it != res.second;) {
+                auto &func = it->second;
+                if (!func(entity, packet))
+                    dispatch_.erase(it++);
+                else
+                    ++it;
             }
-            res->second.erase(std::remove_if(res->second.begin(), res->second.end(), [entity, &packet] (auto &func) {
-                        try {
-                            return !func(entity, packet);
-                        } catch (std::bad_cast) {}
-                        return true;
-                    }), res->second.end());
-            if (res->second.size())
+            if (std::distance(res.first, res.second) > 0)
                 return true;
             return false;
         }
 
+        // FIXME : No possibility to unregister as of now
         template <class T, class U>
         void registerDispatcher(RoseCommon::ePacketType type, void(T::*method)(Entity, const U&)) {
             std::shared_ptr<System> res;
@@ -61,7 +61,7 @@ class SystemManager {
             } catch (std::out_of_range) {
                 return;
             }
-            dispatch_[type].emplace_back([object = std::weak_ptr<System>(res), method](Entity entity, const RoseCommon::CRosePacket &packet) {
+            dispatch_.emplace(type, [object = std::weak_ptr<System>(res), method](Entity entity, const RoseCommon::CRosePacket &packet) {
                     if (auto system = dynamic_cast<T*>(object.lock().get())) {
                         (system ->* method)(entity, dynamic_cast<const U&>(packet));
                         return true;
@@ -70,18 +70,9 @@ class SystemManager {
                     });
         }
 
-        void unregisterDispatcher(RoseCommon::ePacketType type) {
-            dispatch_.erase(type);
-        }
-
-        void unregisterDispatcher(RoseCommon::ePacketType type, size_t pos) {
-            dispatch_[type].erase(dispatch_[type].begin() + pos);
-        }
-
     private:
         EntityManager &entityManager_;
         std::unordered_map<std::type_index, std::shared_ptr<System>> systems_;
-        // TODO : use a std::unordered_multimap
-        std::unordered_map<RoseCommon::ePacketType, std::vector<std::function<bool(Entity, const RoseCommon::CRosePacket&)>>> dispatch_;
+        std::unordered_multimap<RoseCommon::ePacketType, std::function<bool(Entity, const RoseCommon::CRosePacket&)>> dispatch_;
 };
 
