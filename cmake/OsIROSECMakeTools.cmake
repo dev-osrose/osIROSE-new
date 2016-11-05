e2b430bc285f7018ee4cb157b36f144b2e3cec1c
Christopher Torres <christopher.torres@azgstudio.com>
2016-11-04 20:36:32 -0400

    Cleaned up a little more

--- cmake/OsIROSECMakeTools.cmake
+++ cmake/OsIROSECMakeTools.cmake
@@ -3,4 +3,37 @@ MACRO(OSIROSE_CONFIGURE_FILE_AUTOTOOLS_COMPAT)
   CONFIGURE_FILE(${ARGV})
 ENDMACRO(OSIROSE_CONFIGURE_FILE_AUTOTOOLS_COMPAT)
 
+
+FUNCTION(CREATE_VERSION_FILE)
+  # Create a version file using git data
+  if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
+    execute_process(
+      COMMAND git rev-parse --abbrev-ref HEAD
+      WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+      OUTPUT_VARIABLE GIT_BRANCH
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+  
+    execute_process(
+      COMMAND git log -1 --format=%h
+      WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+      OUTPUT_VARIABLE GIT_COMMIT_HASH
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+  else()
+    set(GIT_BRANCH "")
+    set(GIT_COMMIT_HASH "")
+  endif()
+  # --------------------------
+  
+  #start version file
+  message(STATUS "Generating version.h")
+  configure_file(
+    ${CMAKE_SOURCE_DIR}/cmake/version.h.in
+    ${CMAKE_BINARY_DIR}/generated/version.h
+  )
+  #end version file
+ENDFUNCTION()
+
+
 MESSAGE(STATUS "osIROSE CMake Tools loaded.")
