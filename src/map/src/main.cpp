eaf40c526168a9b570f77b91b52302f186366e20
Christopher Torres <christopher.torres@azgstudio.com>
2016-12-05 00:41:35 -0500

    Added missing command line parsing functionality.

--- src/map/src/main.cpp
+++ src/map/src/main.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
+#include <cxxopts.hpp>
 #include "iscpackets.pb.h"
 #include "cmapserver.h"
 #include "cmapisc.h"
@@ -47,55 +47,116 @@ void CheckUser()
   }
 #endif
 }
+
+void ParseCommandLine(int argc, char** argv)
+{
+  cxxopts::Options options(argv[0], "osIROSE login server");
+
+  try {
+    std::string config_file_path = "";
+    options.add_options()
+      ("f,file", "Config file path", cxxopts::value<std::string>(config_file_path)
+        ->default_value("server.ini"), "FILE_PATH")
+      ("l,level", "Logging level (0-9)", cxxopts::value<int>()
+        ->default_value("3"), "LEVEL")
+      ("ip", "Client listen IP Address", cxxopts::value<std::string>()
+        ->default_value("0.0.0.0"), "IP")
+      ("port", "Client listen port", cxxopts::value<int>()
+        ->default_value("29000"), "PORT")
+      ("iscip", "ISC listen IP Address", cxxopts::value<std::string>()
+        ->default_value("127.0.0.1"), "IP")
+      ("iscport", "ISC listen port", cxxopts::value<int>()
+        ->default_value("29010"), "PORT")
+      ("t,maxthreads", "Max thread count", cxxopts::value<int>()
+        ->default_value("512"), "COUNT")
+      ("h,help", "Print this help text")
+      ;
+
+    options.parse(argc, argv);
+
+    // Check to see if the user wants to see the help text
+    if (options.count("help"))
+    {
+      std::cout << options.help({ "", "Group" }) << std::endl;
+      exit(0);
+    }
+
+    Core::Config& config = Core::Config::getInstance(config_file_path);
+
+    // We are using if checks here because we only want to override the config file if the option was supplied
+    // Since this is a login server startup function we can get away with a little bit of overhead
+    if (options.count("level"))
+      config.mutable_login_server()->set_log_level(options["level"].as<int>());
+
+    if (options.count("ip"))
+      config.mutable_serverdata()->set_ip(options["ip"].as<std::string>());
+
+    if (options.count("port"))
+      config.mutable_login_server()->set_clientport(options["port"].as<int>());
+
+    if (options.count("iscip"))
+      config.mutable_serverdata()->set_isclistenip(options["iscip"].as<std::string>());
+
+    if (options.count("iscport"))
+      config.mutable_login_server()->set_iscport(options["iscport"].as<int>());
+
+    if (options.count("maxthreads"))
+      config.mutable_serverdata()->set_maxthreads(options["maxthreads"].as<int>());
+  }
+  catch (const cxxopts::OptionException& ex) {
+    std::cout << ex.what() << std::endl;
+    std::cout << options.help({ "", "Group" }) << std::endl;
+    exit(1);
+  }
+}
 }
 
 int main(int argc, char* argv[]) {
-  (void)argc;
-  (void)argv;
-
   try {
-  auto console = Core::CLog::GetLogger(Core::log_type::GENERAL);
-  if(auto log = console.lock())
-    log->info( "Starting up server..." );
+    ParseCommandLine(argc, argv);
 
-  Core::Config& config = Core::Config::getInstance();
-  Core::CLog::SetLevel((spdlog::level::level_enum)config.map_server().log_level());
-  DisplayTitle();
-  CheckUser();
-
-  if(auto log = console.lock()) {
-    log->set_level((spdlog::level::level_enum)config.map_server().log_level());
-    log->trace("Trace logs are enabled.");
-    log->debug("Debug logs are enabled.");
-  }
-  Core::NetworkThreadPool::GetInstance(config.serverdata().maxthreads());
-
-  CMapServer clientServer;
-  CMapServer iscServer(true);
-  CMapISC* iscClient = new CMapISC();
-  iscClient->Init(config.map_server().charip(), config.map_server().chariscport());
-  iscClient->SetType(iscPacket::ServerType::CHAR);
-
-  clientServer.Init(config.serverdata().ip(), config.map_server().clientport());
-  clientServer.Listen();
-  clientServer.GetISCList().push_front(iscClient);
-
-  iscServer.Init(config.serverdata().ip(), config.map_server().iscport());
-  iscServer.Listen();
-  iscClient->Connect();
-
-  auto start = Core::Time::GetTickCount();
-  while (clientServer.IsActive()) {
-      std::chrono::duration<double> diff = Core::Time::GetTickCount() - start;
-    clientServer.update(diff.count());
-    start = Core::Time::GetTickCount();
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  }
-
-  if(auto log = console.lock())
-    log->info( "Server shutting down..." );
-  Core::NetworkThreadPool::DeleteInstance();
-  spdlog::drop_all();
+    auto console = Core::CLog::GetLogger(Core::log_type::GENERAL);
+    if(auto log = console.lock())
+      log->info( "Starting up server..." );
+    
+    Core::Config& config = Core::Config::getInstance();
+    Core::CLog::SetLevel((spdlog::level::level_enum)config.map_server().log_level());
+    DisplayTitle();
+    CheckUser();
+    
+    if(auto log = console.lock()) {
+      log->set_level((spdlog::level::level_enum)config.map_server().log_level());
+      log->trace("Trace logs are enabled.");
+      log->debug("Debug logs are enabled.");
+    }
+    Core::NetworkThreadPool::GetInstance(config.serverdata().maxthreads());
+    
+    CMapServer clientServer;
+    CMapServer iscServer(true);
+    CMapISC* iscClient = new CMapISC();
+    iscClient->Init(config.map_server().charip(), config.map_server().chariscport());
+    iscClient->SetType(iscPacket::ServerType::CHAR);
+    
+    clientServer.Init(config.serverdata().ip(), config.map_server().clientport());
+    clientServer.Listen();
+    clientServer.GetISCList().push_front(iscClient);
+    
+    iscServer.Init(config.serverdata().ip(), config.map_server().iscport());
+    iscServer.Listen();
+    iscClient->Connect();
+    
+    auto start = Core::Time::GetTickCount();
+    while (clientServer.IsActive()) {
+        std::chrono::duration<double> diff = Core::Time::GetTickCount() - start;
+      clientServer.update(diff.count());
+      start = Core::Time::GetTickCount();
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+    
+    if(auto log = console.lock())
+      log->info( "Server shutting down..." );
+    Core::NetworkThreadPool::DeleteInstance();
+    spdlog::drop_all();
   }
   catch (const spdlog::spdlog_ex& ex) {
      std::cout << "Log failed: " << ex.what() << std::endl;
