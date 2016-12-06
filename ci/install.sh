939d3241874d927c1bcef6d04ddef9aee74244a1
Christopher Torres <RavenX8@users.noreply.github.com>
2016-12-06 16:49:24 -0500

    Update install.sh

--- ci/install.sh
+++ ci/install.sh
@@ -22,14 +22,14 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
 	cd ninja
 	./configure.py --bootstrap
 	#setenv PATH $PATH:`pwd`
-	cd ..
+	cd $PROOT
 
 	wget http://downloads.sourceforge.net/ltp/lcov-1.12.tar.gz
 	tar -xf lcov-1.12.tar.gz
 	cd lcov-1.12
 	make install
 
-	cd ..
+	cd $PROOT
 	gem install coveralls-lcov
     
 	mkdir 3rdparty
@@ -42,18 +42,18 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
 	cd bakefile-0.2.9/
 	./configure --prefix=$PROOT/bakefile > /dev/null
 	make -j4
-	export PATH=$PATH:$PROOT/bakefile/bin
-	export PATH=$PATH:$PROOT/bakefile/lib
+#	export PATH=$PATH:$PROOT/bakefile/bin
+#	export PATH=$PATH:$PROOT/bakefile/lib
 	make install
 	
-	cd ../tools/mysqlpp
+	cd $PROOT/tools/mysqlpp
 	./bootstrap
 	./configure
 
 	cd lib
 	perl querydef.pl
 	perl ssqls.pl
-	cd ../../..
+	cd $PROOT
 else
     echo "Unknown OS ($TRAVIS_OS_NAME). Stopping build ..."
     exit 1
