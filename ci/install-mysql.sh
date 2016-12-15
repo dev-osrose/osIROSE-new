8a25eef83ca88f7b96b84a357741c9279c60d6b2
Christopher Torres <christopher.torres@azgstudio.com>
2016-12-14 20:18:46 -0500

    Added install for mysql connector C

---
+++ ci/install-mysql.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+if [ -z "$TRAVIS_OS_NAME" ]; then
+    TRAVIS_OS_NAME=linux
+fi
+
+echo 'Building and installing mysql.';
+
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+	if [ ! -d "$HOME/mysql/lib" ]; then
+		echo 'Setting up mysql...';
+    wget http://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.6-src.tar.gz
+    tar -xf mysql-connector-c-6.1.6-src.tar.gz
+    cd mysql-connector-c-6.1.6-src
+    cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$HOME/mysql
+		make
+		make install
+		cd ..
+	else
+		echo 'Using cached mysql directory.';
+	fi
+else
+    echo "Unknown OS ($TRAVIS_OS_NAME). Stopping build ..."
+    exit 1
+fi
