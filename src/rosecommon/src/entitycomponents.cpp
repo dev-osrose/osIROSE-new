f8046ebce78c3b1a88dfdcc7f1ee375c9a6f3a43
L3nn0x <dragon.super@laposte.net>
2016-12-31 01:27:55 +0100

    Added missing helper code

--- src/rosecommon/src/entitycomponents.cpp
+++ src/rosecommon/src/entitycomponents.cpp
@@ -1,13 +1,4 @@
-#include "packetfactory.h"
 #include "entityComponents.h"
-
-namespace RoseCommon {
-
-std::unordered_map<ePacketType, std::function<std::unique_ptr<CRosePacket>(uint8_t[MAX_MACKET_SIZE])>> PacketFactory::mapping_ = {
-    registerPacket<CliMouseCmd>(ePacketType::PAKCS_MOUSE_CMD),
-};
-
-}
 
 uint16_t getId(Entity entity) {
     return entity.component<BasicInfo>()->id_;
