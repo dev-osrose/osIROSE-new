6f40208dfe90352af006309fbf51381091c78203
Chris Torres <redflames1003@gmail.com>
2017-04-25 21:03:32 +0000

    Fixed directory name clash for installing newer cmake on travis

--- ci/install-cmake.sh
+++ ci/install-cmake.sh
@@ -8,8 +8,8 @@ echo 'Building and installing cmake.';
 
 if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
   CMAKE_URL="https://cmake.org/files/v3.7/cmake-3.7.2-Linux-x86_64.tar.gz"
-  mkdir cmake && travis_retry wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
-  export PATH=${DEPS_DIR}/cmake/bin:${PATH}
+  mkdir cmake_app && travis_retry wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake_app
+  export PATH=${TRAVIS_BUILD_DIR}/cmake_app/bin:${PATH}
 else
   brew install cmake
 fi
