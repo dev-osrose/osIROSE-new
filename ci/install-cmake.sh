1fe2d274bd3d96839b89104c510a7725e1d1b479
Chris Torres <redflames1003@gmail.com>
2017-04-25 21:08:46 +0000

    Updated cmake install script

--- ci/install-cmake.sh
+++ ci/install-cmake.sh
@@ -8,7 +8,7 @@ echo 'Building and installing cmake.';
 
 if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
   CMAKE_URL="https://cmake.org/files/v3.7/cmake-3.7.2-Linux-x86_64.tar.gz"
-  mkdir cmake_app && travis_retry wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake_app
+  mkdir cmake_app && wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake_app
   export PATH=${TRAVIS_BUILD_DIR}/cmake_app/bin:${PATH}
 else
   brew upgrade cmake || brew install cmake
