eeeb9495291cae10413dab8c7dfbe01142d62e3c
n3f4s <tiv.crb@gmail.com>
2017-05-04 21:52:59 +0200

    use variables in absolute path for default env

--- scripts/default-env.sh
+++ scripts/default-env.sh
@@ -1,7 +1,7 @@
-export PROOT=/home/vincent/dev/osIROSE-new
+export PROOT=${HOME}/osIROSE-new
 export GCC_VERSION=5
 export GCOV_PATH=/usr/bin/gcov #-${GCOV_VERSION}
-export LCOV_PATH=/home/vincent/usr/bin/lcov
-export GENHTML_PATH=/home/vincent/usr/bin/genhtml
-export PREFIX=/home/vincent
+export LCOV_PATH=${PROOT}/usr/bin/lcov
+export GENHTML_PATH=${PROOT}/usr/bin/genhtml
+export PREFIX=${PROOT}
 export BUILD_TYPE=Debug
