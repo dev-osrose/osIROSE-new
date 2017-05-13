ddb2eac450d7f4779fa708d58240eac75017ad46
L3nn0x <dragon.super@laposte.net>
2017-05-13 18:25:16 +0200

    Updated travais

--- ci/script.sh
+++ ci/script.sh
@@ -2,9 +2,9 @@
 
 if [ "$SUITE" = "coverage" ]; then
 #  cmake -GNinja -DENABLE_TESTING=ON -DENABLE_TESTING_COVERAGE=ON -DBUILD_MYSQLPP=ON .. && ninja && ninja coverage && coveralls-lcov coverage.info.cleaned
-  cmake -DENABLE_TESTING=ON -DENABLE_TESTING_COVERAGE=ON -DBUILD_MYSQLPP=ON .. && make && make coverage && coveralls-lcov coverage.info.cleaned
+  cmake -DENABLE_TESTING=ON -DENABLE_TESTING_COVERAGE=ON .. && make && make coverage && coveralls-lcov coverage.info.cleaned
 elif [ "$SUITE" = "tests" ]; then
-  cmake -DENABLE_TESTING=ON -DENABLE_TESTING_COVERAGE=OFF -DBUILD_MYSQLPP=ON .. && make && ctest --output-on-failure
+  cmake -DENABLE_TESTING=ON -DENABLE_TESTING_COVERAGE=OFF .. && make VERBOSE=1 && ctest --output-on-failure
 else
 	cmake -GNinja .. && ninja
 fi
