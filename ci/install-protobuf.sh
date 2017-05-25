7a8d2613201b54945273462a300103cb057f5a47
L3nn0x <dragon.super@laposte.net>
2017-05-25 22:29:21 +0200

    force travis to recompile protobuf

--- ci/install-protobuf.sh
+++ ci/install-protobuf.sh
@@ -7,7 +7,7 @@ fi
 echo 'Building and installing protobuf.';
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-	if [[ ! -d "$HOME/protobuf/lib" || ! -f "$HOME/protobuf/bin/protoc" ]]; then
+#	if [[ ! -d "$HOME/protobuf/lib" || ! -f "$HOME/protobuf/bin/protoc" ]]; then
 		echo 'Setting up protobuf...';
 		cd 3rdparty/protobuf
 		./autogen.sh
@@ -15,9 +15,9 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
 		make -j 4 && make install
 
 		cd ../../
-	else
+#	else
 		echo 'Using cached protobuf directory.';
-	fi
+#	fi
 else
     echo "Unknown OS ($TRAVIS_OS_NAME). Stopping build ..."
     exit 1
