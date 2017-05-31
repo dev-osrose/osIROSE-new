5f0aecd312f6bdfe654be0e64442ec487762cdde
Chris Torres <redflames1003@gmail.com>
2017-05-31 15:25:42 +0000

    Fix for appveyor compile error

--- ci/msvc_install.bat
+++ ci/msvc_install.bat
@@ -1,8 +1,8 @@
 mkdir build
 REM mkdir 3rdparty\build
-Robocopy "./tools/googletest/googlemock" "./tools/protobuf/gmock" /E /MT:2 /R:0 /DCOPY:T
-Robocopy "./tools/googletest/googletest" "./tools/protobuf/gmock/gtest" /E /MT:2 /R:0 /DCOPY:T
-REM Robocopy "./tools/spdlog/include" "./3rdparty/include" /E /MT:2 /R:0 /DCOPY:T
+Robocopy "./3rdparty/googletest/googlemock" "./3rdparty/protobuf/gmock" /E /MT:2 /R:0 /DCOPY:T
+Robocopy "./3rdparty/googletest/googletest" "./3rdparty/protobuf/gmock/gtest" /E /MT:2 /R:0 /DCOPY:T
+REM Robocopy "./3rdparty/spdlog/include" "./3rdparty/include" /E /MT:2 /R:0 /DCOPY:T
 IF %ERRORLEVEL% LSS 8 goto build
 goto :eof
 
