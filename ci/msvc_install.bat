a24dc1309b378bc35b4d08c626a1b86d956cb1ce
L3nn0x <dragon.super@laposte.net>
2017-07-19 22:30:38 +0200

    updated msvc install

--- ci/msvc_install.bat
+++ ci/msvc_install.bat
@@ -3,6 +3,7 @@ REM mkdir 3rdparty\build
 Robocopy "./3rdparty/googletest/googlemock" "./3rdparty/protobuf/gmock" /E /MT:2 /R:0 /DCOPY:T
 Robocopy "./3rdparty/googletest/googletest" "./3rdparty/protobuf/gmock/gtest" /E /MT:2 /R:0 /DCOPY:T
 REM Robocopy "./3rdparty/spdlog/include" "./3rdparty/include" /E /MT:2 /R:0 /DCOPY:T
+pip install pyparsing
 IF %ERRORLEVEL% LSS 8 goto build
 goto :eof
 
