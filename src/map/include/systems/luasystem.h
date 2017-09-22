9aabf85e2c8e45641b954c1a025407cfcc0404e1
L3nn0x <dragon.super@laposte.net>
2017-09-22 09:27:42 +0100

    Update luasystem.h

--- src/map/include/systems/luasystem.h
+++ src/map/include/systems/luasystem.h
@@ -4,6 +4,8 @@
 
 #include <sol.hpp>
 
+#include <unordered_set>
+
 namespace Systems {
 
 class LuaSystem : public System {
@@ -11,10 +13,30 @@ class LuaSystem : public System {
         LuaSystem(SystemManager &manager) : System(manager) {}
         virtual ~LuaSystem() = default;
 
-        virtual void update(EntityManager &es, double dt) {}
+        void registerLuaUpdate(Entity e, const std::string& luaFunc, double dt) {
+            callbacks_.insert({e, state_.load(luaFunc), dt});
+        }
+    
+        virtual void update(EntityManager&, double dt) {
+            for (auto &it : callbacks_) {
+                it.dt += dt;
+                if (it.dt >= timeout) {
+                    func(it.e, it.dt);
+                    it.dt = 0.f;
+                }
+            }
+        }
     
     private:
         sol::state state_;
+        
+        struct Callback {
+            Entity e;
+            sol::load_result func;
+            double timeout;
+            double dt = 0;
+        };
+        std::unordered_set<Callback> callbacks_;
 };
 
 }
