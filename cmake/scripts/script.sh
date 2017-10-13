25466cf5a7344a997bf8f5934de66528920dcece
Chris Torres <redflames1003@gmail.com>
2017-10-13 20:12:25 +0000

    Removed build job limit for travis

--- cmake/scripts/script.sh
+++ cmake/scripts/script.sh
@@ -2,9 +2,9 @@
 
 if [ "$SUITE" = "coverage" ]; then
 #  cmake -GNinja -DBUILD_TESTS=ON -DWITH_COVERAGE_REPORTS=ON .. && cmake --build . && cmake --build . --target coverage && coveralls-lcov coverage.info.cleaned
-  cmake -DBUILD_TESTS=ON -DWITH_COVERAGE_REPORTS=ON .. && cmake --build . -- -j 4 && cmake --build . --target coverage && coveralls-lcov coverage.info.cleaned
+  cmake -DBUILD_TESTS=ON -DWITH_COVERAGE_REPORTS=ON .. && cmake --build . && cmake --build . --target coverage && coveralls-lcov coverage.info.cleaned
 elif [ "$SUITE" = "tests" ]; then
-  cmake -GNinja -DBUILD_TESTS=ON -DWITH_COVERAGE_REPORTS=OFF .. && cmake --build . -- -j 4 && ctest --output-on-failure
+  cmake -GNinja -DBUILD_TESTS=ON -DWITH_COVERAGE_REPORTS=OFF .. && cmake --build . && ctest --output-on-failure
 else
-  cmake -GNinja -DBUILD_TESTS=OFF -DWITH_COVERAGE_REPORTS=OFF .. && cmake --build . -- -j 4
+  cmake -GNinja -DBUILD_TESTS=OFF -DWITH_COVERAGE_REPORTS=OFF .. && cmake --build .
 fi
