11705167bc180d6d5e922226d5f95f47f82af349
Chris Torres <redflames1003@gmail.com>
2017-10-13 17:28:58 +0000

    Forgot to change make to ninja

--- cmake/scripts/script.sh
+++ cmake/scripts/script.sh
@@ -4,7 +4,7 @@ if [ "$SUITE" = "coverage" ]; then
 #  cmake -GNinja -DBUILD_TESTS=ON -DWITH_COVERAGE_REPORTS=ON .. && ninja && ninja coverage && coveralls-lcov coverage.info.cleaned
   cmake -DBUILD_TESTS=ON -DWITH_COVERAGE_REPORTS=ON .. && make && make coverage && coveralls-lcov coverage.info.cleaned
 elif [ "$SUITE" = "tests" ]; then
-  cmake -GNinja -DBUILD_TESTS=ON -DWITH_COVERAGE_REPORTS=OFF .. && make && ctest --output-on-failure
+  cmake -GNinja -DBUILD_TESTS=ON -DWITH_COVERAGE_REPORTS=OFF .. && ninja && ctest --output-on-failure
 else
 	cmake -GNinja .. && ninja
 fi
