0f4f5e40906614bca75170d06a04eafe602fcc1e
Chris Torres <redflames1003@gmail.com>
2017-10-13 21:09:14 +0000

    Coverage suite will now build with ninja as well

--- cmake/scripts/script.sh
+++ cmake/scripts/script.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 if [ "$SUITE" = "coverage" ]; then
-#  cmake -GNinja -DBUILD_TESTS=ON -DWITH_COVERAGE_REPORTS=ON .. && cmake --build . && cmake --build . --target coverage && coveralls-lcov coverage.info.cleaned
-  cmake -DWITH_COVERAGE_REPORTS=ON .. && cmake --build . && cmake --build . --target coverage && coveralls-lcov coverage.info.cleaned
+  cmake -GNinja -DWITH_COVERAGE_REPORTS=ON .. && cmake --build . -- -j 4 && cmake --build . --target coverage && coveralls-lcov coverage.info.cleaned
 elif [ "$SUITE" = "tests" ]; then
   cmake -GNinja .. && cmake --build . -- -j 4 && ctest --output-on-failure
 else
