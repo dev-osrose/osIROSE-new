0fc14edfe655003cd48c1cbb6887936416e7c733
Chris Torres <redflames1003@gmail.com>
2017-10-13 18:51:24 +0000

    Updated other cmake modules for ninja builds

--- cmake/modules/BuildGTest.cmake
+++ cmake/modules/BuildGTest.cmake
@@ -3,14 +3,33 @@ if(MSVC11)
   set(ADD_CXX_FLAGS "/D_VARIADIC_MAX=10")
 endif()
 
-ExternalProject_Add(
-  gtest
-  GIT_SUBMODULES googletest...
-  SOURCE_DIR ${CMAKE_THIRD_PARTY_DIR}/googletest/googletest
-  CMAKE_ARGS -DBUILD_SHARED_LIBS=ON -DCMAKE_CXX_FLAGS=${ADD_CXX_FLAGS} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -Dgtest_force_shared_crt:BOOL=ON
-  BUILD_IN_SOURCE 1
-  INSTALL_COMMAND ""
-)
+# This is a workaround for Ninja not allowing us to build if these libs weren't built before
+if(WIN32)
+  ExternalProject_Add(
+    gtest
+    GIT_SUBMODULES googletest...
+    SOURCE_DIR ${CMAKE_THIRD_PARTY_DIR}/googletest/googletest
+    CMAKE_ARGS -DBUILD_SHARED_LIBS=ON -DCMAKE_CXX_FLAGS=${ADD_CXX_FLAGS} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -Dgtest_force_shared_crt:BOOL=ON
+    BUILD_IN_SOURCE 1
+    INSTALL_COMMAND ""
+  )
+else()
+  set(_byproducts
+    ${CMAKE_THIRD_PARTY_DIR}/googletest/googletest/libgtest.so
+  )
+  
+  ExternalProject_Add(
+    gtest
+    GIT_SUBMODULES googletest...
+    SOURCE_DIR ${CMAKE_THIRD_PARTY_DIR}/googletest/googletest
+    CMAKE_ARGS -DBUILD_SHARED_LIBS=ON -DCMAKE_CXX_FLAGS=${ADD_CXX_FLAGS} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -Dgtest_force_shared_crt:BOOL=ON
+    BUILD_BYPRODUCTS ${_byproducts}
+    BUILD_IN_SOURCE 1
+    INSTALL_COMMAND ""
+  )
+endif()
+
+
 
 ExternalProject_Get_Property(
   gtest
