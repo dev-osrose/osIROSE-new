0755f07114f1e94eea62a0cda8d3763b6c747c6a
L3nn0x <dragon.super@laposte.net>
2017-11-09 11:22:48 +0000

    Update entitysystem.cpp

--- src/map/src/entitysystem.cpp
+++ src/map/src/entitysystem.cpp
@@ -163,8 +163,8 @@ Entity EntitySystem::loadCharacter(uint32_t charId, bool platinium, uint32_t id)
 
     entity.assign<Lua>();
 
-    systemManager_.get<Systems::LuaSystem>()->loadScript(entity, "function test()\ntestCpp('test')\nend");
-    systemManager_.get<Systems::LuaSystem>()->callLuaFunction(entity, "test");
+    systemManager_.get<Systems::LuaSystem>()->loadScript(entity, "function onInit()\ntestCpp('test')\nend");
+    systemManager_.get<Systems::LuaSystem>()->callLuaFunction<Systems::LuaSystem::onInit>(entity);
 
     registerEntity(entity);
     return entity;
