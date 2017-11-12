b9f946fd285dc09dc882c85b219a8839b917329f
L3nn0x <dragon.super@laposte.net>
2017-11-13 00:34:34 +0100

    Updated code

--- src/rosecommon/include/components/lua.h
+++ src/rosecommon/include/components/lua.h
@@ -2,14 +2,13 @@
 
 #include <memory>
 
-#include "luaapi.h"
+#include <sol.hpp>
 
 struct Lua {
     Lua() = default;
     Lua(const Lua&) = delete;
-    Lua(Lua&& other) : env_(std::move(other.env_)), api_(std::move(other.api_)) {}
+    Lua(Lua&& other) : env_(std::move(other.env_)) {}
     Lua& operator=(Lua&& other) { Lua tmp(std::move(other)); std::swap(*this, tmp); return *this; }
 
     std::unique_ptr<sol::environment> env_;
-    std::unique_ptr<LuaApi> api_;
 };
--- src/rosecommon/include/luaapi.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <sol.hpp>
-
-class LuaApi {
-    public:
-        virtual void setupApi(sol::environment& env) = 0;
-        virtual ~LuaApi() = default;
-};
