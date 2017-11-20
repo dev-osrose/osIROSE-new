88803440fb6a755f81b1701de51e62af8c064714
L3nn0x <dragon.super@laposte.net>
2017-11-20 15:49:46 +0000

    Update lua.h

--- src/rosecommon/include/components/lua.h
+++ src/rosecommon/include/components/lua.h
@@ -2,13 +2,13 @@
 
 #include <memory>
 
-#include <sol.hpp>
+#include "luaapi.h"
 
 struct Lua {
     Lua() = default;
     Lua(const Lua&) = delete;
-    Lua(Lua&& other) : env_(std::move(other.env_)) {}
+    Lua(Lua&& other) : api_(std::move(other.api_)) {}
     Lua& operator=(Lua&& other) { Lua tmp(std::move(other)); std::swap(*this, tmp); return *this; }
 
-    std::unique_ptr<sol::environment> env_;
+    std::unique_ptr<LuaAPI> api_;
 };
