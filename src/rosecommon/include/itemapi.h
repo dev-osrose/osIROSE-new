dd812698ed441617d31b81b955b78f6cfc2b1192
L3nn0x <dragon.super@laposte.net>
2017-11-27 15:12:36 +0000

    Update itemapi.h

--- src/rosecommon/include/itemapi.h
+++ src/rosecommon/include/itemapi.h
@@ -9,6 +9,9 @@ class ItemAPI {
     ItemAPI(sol::environment&& env) : env_(std::move(env)) {
       // build the C++/lua connectors here
     }
+  
+    sol::environment& getEnv() { return env_; }
+    const sol::environment& getEnv() const { return env_; }
   private:
     sol::environment env_;
 };
