8344392ff8f539143ac3a88f5dc77f459bf2451b
L3nn0x <dragon.super@laposte.net>
2017-11-27 15:36:41 +0000

    Update itemapi.h

--- src/rosecommon/include/itemapi.h
+++ src/rosecommon/include/itemapi.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "sol/environment.hpp"
+#include "logconsole.h"
 #include <memory>
 
 namespace RoseCommon {
@@ -8,7 +9,7 @@ namespace RoseCommon {
 class ItemAPI {
   public:
     ItemAPI() = default;
-    ItemAPI(sol::environment&& env) : env_(std::make_unique(std::move(env))) {
+    ItemAPI(sol::environment&& env) : env_(std::make_unique(std::move(env))), logger_(Core::CLog::GetLogger(Core::log_type::SYSTEM).lock()) {
       // build the C++/lua connectors here
     }
   
@@ -16,6 +17,7 @@ class ItemAPI {
     const sol::environment* getEnv() const { return env_.get(); }
   private:
     std::unique_ptr<sol::environment> env_;
+    std::shared_ptr<spdlog::logger> logger_;
 };
 
 }
