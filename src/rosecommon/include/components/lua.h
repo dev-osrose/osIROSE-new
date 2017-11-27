cb63e2aa9a741d08efbcd90b55a93c1a1765e992
L3nn0x <dragon.super@laposte.net>
2017-11-27 14:05:40 +0000

    Update lua.h

--- src/rosecommon/include/components/lua.h
+++ src/rosecommon/include/components/lua.h
@@ -1,14 +1,11 @@
 #pragma once
 
-#include <memory>
-
-#include "luaapi.h"
-
+template <typename LuaAPI>
 struct Lua {
     Lua() = default;
     Lua(const Lua&) = delete;
     Lua(Lua&& other) : api_(std::move(other.api_)) {}
     Lua& operator=(Lua&& other) { Lua tmp(std::move(other)); std::swap(*this, tmp); return *this; }
 
-    std::unique_ptr<LuaAPI> api_;
+    LuaAPI api_;
 };
