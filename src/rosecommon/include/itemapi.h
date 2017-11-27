e982e29df6aebe5b2848d5fec235e0a3354b6659
L3nn0x <dragon.super@laposte.net>
2017-11-27 15:24:35 +0000

    Update itemapi.h

--- src/rosecommon/include/itemapi.h
+++ src/rosecommon/include/itemapi.h
@@ -1,19 +1,21 @@
 #pragma once
 
 #include "sol/environment.hpp"
+#include <memory>
 
 namespace RoseCommon {
 
 class ItemAPI {
   public:
-    ItemAPI(sol::environment&& env) : env_(std::move(env)) {
+    ItemAPI() = default;
+    ItemAPI(sol::environment&& env) : env_(std::make_unique(std::move(env))) {
       // build the C++/lua connectors here
     }
   
-    sol::environment& getEnv() { return env_; }
-    const sol::environment& getEnv() const { return env_; }
+    sol::environment* getEnv() { return env_.get(); }
+    const sol::environment* getEnv() const { return env_.get(); }
   private:
-    sol::environment env_;
+    std::unique_ptr<sol::environment> env_;
 };
 
 }
