b591bf12a83857eb346d07b7b66337de24200713
L3nn0x <dragon.super@laposte.net>
2017-11-27 15:33:53 +0000

    Update entityapi.h

--- src/rosecommon/include/entityapi.h
+++ src/rosecommon/include/entityapi.h
@@ -1,7 +1,22 @@
 #pragma once
 
+#include "sol/environment.hpp"
+#include <memory>
+
 namespace RoseCommon {
 
-class EntityAPI {};
+class EntityAPI {
+  public:
+    EntityAPI() = default;
+    EntityAPI(sol::environment&& env) : env_(std::move(std::make_unique(env))) {}
+  
+    sol::environment* getEnv() { return env_.get(); }
+    const sol::environment* getEnv() const { return env_.get(); }
+  
+    void onInit() { auto f = env["onInit"]; f(); }
+  
+  private:
+    std::unique_ptr<sol::environment> env_;
+};
 
 }
