40ea07cf1f1e715181854c9622e01f5ca00fa63b
L3nn0x <dragon.super@laposte.net>
2017-11-27 15:29:26 +0000

    Update entitysystem.cpp

--- src/map/src/entitysystem.cpp
+++ src/map/src/entitysystem.cpp
@@ -160,7 +160,7 @@ Entity EntitySystem::loadCharacter(uint32_t charId, bool platinium, uint32_t id)
     auto wishlist = entity.assign<Wishlist>();
     wishlist->loadFromResult(wishRes);
 
-    entity.assign<Lua>();
+    entity.assign<Lua<EntityAPI>>();
 
     systemManager_.get<Systems::LuaSystem>()->loadScript(entity, "function onInit()\ndisplay('test')\nend");
 
