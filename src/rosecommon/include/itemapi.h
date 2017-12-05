34fedd6f91f409970f28a571f3cefea7ff171dfc
L3nn0x <dragon.super@laposte.net>
2017-12-05 01:05:27 +0100

    Added first edition of lua api

--- src/rosecommon/include/itemapi.h
+++ src/rosecommon/include/itemapi.h
@@ -9,7 +9,19 @@ class ItemAPI : public LuaAPI {
     ItemAPI() = default;
     ItemAPI(sol::environment&& env) : LuaAPI(std::move(env)) {
       // build the C++/lua connectors here
+      /*env_.set_function("getAttr", [this](Entity e, std::string attr) {
+        logger_->info("getAttr called for entity {} and attr {}", getId(e), attr);
+      });*/
     }
+
+    /*void onInit() { env_["onInit"](); }
+    void onDelete() { env_["onDelete"](); }
+    bool onEquip(Entity e) { return env_["onEquip"](e); } // character's entity
+    bool onUnequip(Entity e) { return env_["onUnequip"](e); } // character's entity
+    bool onDrop(Entity e) { return env_["onDrop"](e); } // self's entity
+    bool onPickup(Entity e) { return env_["onPickup"](e); } // character's entity
+    bool onUse(Entity e) { return env_["onUse"](e); } // character's entity
+    */
 };
 
 }
