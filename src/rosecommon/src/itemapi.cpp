831779ade7929f2685fa8695a79920f3df8db4dc
L3nn0x <dragon.super@laposte.net>
2017-12-22 10:48:53 +0000

    Update itemapi.cpp

--- src/rosecommon/src/itemapi.cpp
+++ src/rosecommon/src/itemapi.cpp
@@ -1,12 +1,13 @@
 #include "itemapi.h"
-#include "entitysystem.h"
+#include "entitycomponents.h"
 
 using namespace RoseCommon;
 
-ItemAPI::ItemAPI(EntitySystem& es, sol::environment&& env) : LuaAPI(es, std::move(env)) {
+ItemAPI::ItemAPI(sol::environment&& env) : LuaAPI(std::move(env)) {
       // build the C++/lua connectors here
-      env_.set_function("getAttr", [this](uint32_t charId, std::string attr) {
-        Entity e = entitySystem->getEntity(charId);
+      env_.set_function("getAttr", [this](void* entity, std::string attr) {
+        throw_assert(entity, "The entity cannot be nullptr");    
+        Entity e = *(Entity*)entity;
         logger_->info("getAttr called for client {} and attr {}", getId(e), attr);
       });
     }
