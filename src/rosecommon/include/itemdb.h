ca7559118d7310e4b4a1f7314414aef1b2cede89
L3nn0x <dragon.super@laposte.net>
2017-12-22 11:09:53 +0000

    Update itemdb.h

--- src/rosecommon/include/itemdb.h
+++ src/rosecommon/include/itemdb.h
@@ -27,6 +27,7 @@
 #include "singleton.h"
 #include "item.h"
 #include "hash.h"
+#include "iterate.h"
 
 namespace RoseCommon
 {
@@ -86,7 +87,7 @@ namespace RoseCommon
     using key_t = std::tuple<uint8_t, uint16_t>;
     struct key_hash : public std::unary_function<key_t, std::size_t> {
         std::size_t operator()(const key_t& k) const {
-            return std::apply(Core::hash_val, k);
+            return Core::apply(k, Core::hash_val);
         }
     };
 
