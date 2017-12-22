d9ed91f6c6c9f8b1d659c954fba0702517bc151d
L3nn0x <dragon.super@laposte.net>
2017-12-22 10:53:00 +0000

    Update itemapi.cpp

--- src/rosecommon/src/itemapi.cpp
+++ src/rosecommon/src/itemapi.cpp
@@ -9,5 +9,12 @@ ItemAPI::ItemAPI(sol::environment&& env) : LuaAPI(std::move(env)) {
         throw_assert(entity, "The entity cannot be nullptr");    
         Entity e = *(Entity*)entity;
         logger_->info("getAttr called for client {} and attr {}", getId(e), attr);
+        return 42;
       });
+      
+      env_.set_function("setAttr", [this](void* entity, std::string attr, int32_t value) {
+        throw_assert(entity, "The entity cannot be nullptr");
+        Entity e = *(Entity*)entity;
+        logger_->info("setAttr called for client {} and attr {} value {}", getId(e), attr, value);
+      }
     }
