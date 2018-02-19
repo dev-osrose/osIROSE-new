3dc04e1c2da5fe15d2693855f94aeabc1c53a14e
L3nn0x <dragon83.super@gmail.com>
2018-02-19 11:30:59 +0000

    Update lua API

--- src/rosecommon/src/itemapi.cpp
+++ src/rosecommon/src/itemapi.cpp
@@ -12,9 +12,15 @@ ItemAPI::ItemAPI(sol::environment&& env) : LuaAPI(std::move(env)) {
         return 42;
       });
       
-      env_.set_function("setAttr", [this](void* entity, std::string attr, int32_t value) {
+      env_.set_function("addBonusAttr", [this](void* entity, std::string attr, int32_t value) {
         throw_assert(entity, "The entity cannot be nullptr");
         Entity e = *(Entity*)entity;
-        logger_->info("setAttr called for client {} and attr {} value {}", getId(e), attr, value);
-      }
+        logger_->info("addBonusAttr called for client {} and attr {} value {}", getId(e), attr, value);
+      });
+      
+      env_.set_function("removeBonusAttr", [this](void* entity, std::string attr, int32_t value) {
+        throw_assert(entity, "The entity cannot be nullptr");
+        Entity e = *(Entity*)entity;
+        logger_->info("removeBonusAttr called for client {} and attr {} value {}", getId(e), attr, value);
+      });
     }
