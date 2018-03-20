5fd0032d94cb76867d9e1f8c5d453b78f27fe1ce
Christopher Torres <christopher.torres@azgstudio.com>
2018-03-19 23:35:31 -0400

    Updated build options

--- cmake/modules/CheckOptions.cmake
+++ cmake/modules/CheckOptions.cmake
@@ -4,24 +4,30 @@
 
 option(PROJECT_DEBUG "Enable debugging messages support" ON)
 
-OPTION(BUILD_LUA "Build Lua" ON)
-option(BUILD_TOOLS "Build server tools" ON)
+option(OFFICIAL_BUILD "do you want to compile an official build?" OFF)
+option(BUILD_LUA "Build Lua" ON)
 option(BUILD_API_DOC "Build doxygen documentation" OFF)
 
-option(BUILD_TESTS "Build various unit tests." ON)
-option(WITH_GTEST "Add GTest support" ON)
-option(WITH_GMOCK "Add GMock support" ON)
+set(DEV_SETTING ON)
+if(OFFICIAL_BUILD)
+  message(STATUS "Official Build is set - Turning off dev options")
+  set(DEV_SETTING OFF)
+endif()
+
+option(BUILD_TOOLS "Build server tools" ${DEV_SETTING})
+option(BUILD_TESTS "Build various unit tests." ${DEV_SETTING})
+option(WITH_GTEST "Add GTest support" ${DEV_SETTING})
+option(WITH_GMOCK "Add GMock support" ${DEV_SETTING})
 if(BUILD_TESTS OR WITH_GTEST)
   include(CTest)
   enable_testing()
+  
+  option(WITH_COVERAGE_REPORTS "Enable generating code coverage report" OFF)
 endif()
 
 option(WITH_CRASH_REPORTS "Enable crash dump generation" ON)
-option(WITH_COVERAGE_REPORTS "Enable generating code coverage report" OFF)
 option(WITH_MYSQL "Use MySQL connection" ON)
-
 option(DEBUG "enable debug build" OFF)
-option(OFFICIAL_BUILD "do you want to compile an official build?" OFF)
 
 option(FORCE_SYS_DEPS "Force the use of system libs")
 if(FORCE_SYS_DEPS)
