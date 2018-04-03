379096ba5a53df0732e667351fb070e945c21a8f
L3nn0x <dragon.super@laposte.net>
2018-04-04 00:27:25 +0200

    Simplified safe lua call code with std::optional

--- src/rosecommon/include/itemapi.h
+++ src/rosecommon/include/itemapi.h
@@ -1,25 +1,26 @@
 #pragma once
 
 #include "luaapi.h"
+#include <optional>
 
 namespace RoseCommon {
 
 class ItemAPI : public LuaAPI {
   public:
     ItemAPI() = default;
-    virtual ~ItemAPI() { if (isCreated_) onDelete(); }
+    virtual ~ItemAPI() = default;
     ItemAPI(sol::environment&& env);
 
-    void onInit() { safeVoidLuaCall("OnInit"); }
-    void onDelete() { safeVoidLuaCall("OnDelete"); }
-    bool onEquip(void* entity) { return safeLuaCall<bool>("OnEquip", entity); }
-    bool onUnequip(void* entity) { return safeLuaCall<bool>("OnUnequip", entity); }
-    bool onDrop(void* entity) { return safeLuaCall<bool>("OnDrop", entity); }
-    bool onPickup(void* entity) { return safeLuaCall<bool>("OnPickup", entity); }
-    bool onUse(void* entity) { return safeLuaCall<bool>("OnUse", entity); }
+    void onInit() { safeLuaCall<bool>("OnInit"); }
+    void onDelete() { safeLuaCall<bool>("OnDelete"); }
+    bool onEquip(void* entity) { return safeLuaCall<bool>("OnEquip", entity).value_or(false); }
+    bool onUnequip(void* entity) { return safeLuaCall<bool>("OnUnequip", entity).value_or(false); }
+    bool onDrop(void* entity) { return safeLuaCall<bool>("OnDrop", entity).value_or(false); }
+    bool onPickup(void* entity) { return safeLuaCall<bool>("OnPickup", entity).value_or(false); }
+    bool onUse(void* entity) { return safeLuaCall<bool>("OnUse", entity).value_or(false); }
 
     template <typename T, typename... Args>
-    T safeLuaCall(const std::string& func, const Args&... args) {
+    std::optional<T> safeLuaCall(const std::string& func, const Args&... args) {
         logger_->trace("Calling lua function {}", func);
         if (!isCreated_) {
             logger_->warn("Trying to call lua function {}() when the lua environment hasn't been created", func);
@@ -32,22 +33,6 @@ class ItemAPI : public LuaAPI {
         sol::error err = res;
         logger_->error("{}() lua call failed with: {}", func, err.what());
         return {};
-    }
-
-    template <typename... Args>
-    void safeVoidLuaCall(const std::string& func, const Args... args) {
-        logger_->trace("Calling lua function {}", func);
-        if (!isCreated_) {
-            logger_->warn("Trying to call lua function {}() when the lua environment hasn't been created", func);
-            return;
-        }
-        sol::protected_function f = env_[func];
-        auto res = f(args...);
-        if (res.valid())
-            return;
-        sol::error err = res;
-        logger_->error("{}() lua call failed with: {}", func, err.what());
-        return;
     }
 };
 
