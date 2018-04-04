e6775ad0e5fb6b96c0737ee824a636a50cf7c082
Christopher Torres <RavenX8@users.noreply.github.com>
2018-04-03 20:13:24 -0400

    Update docker_start_server.sh

--- cmake/scripts/docker_start_server.sh
+++ cmake/scripts/docker_start_server.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+if [ -n $CONFIG_FILE ]; then
+  CONFIG_FILE="-f $CONFIG_FILE"
+else
+  CONFIG_FILE="-f /config/login-server.json"
+fi
+
+if [ -n $AUTOCONFIG_URL ]; then
+  AUTOCONFIG_URL="--url $URL"
+else
+  AUTOCONFIG_URL=""
+fi
+
 # Check to see if we have a server set
 if [ -n $SERVER ]; then
   if [ -e $SERVER ]; then
-    if [ -n $CONFIG_FILE ]; then
-      ./$SERVER -f $CONFIG_FILE
-    else
-      ./$SERVER -f /config/login-server.json
-    fi
+      ./$SERVER $CONFIG_FILE $AUTOCONFIG_URL
   else
     echo "$SERVER does not exist."
     exit 1
