002dc307da65117df6cab1de2a80a3edaf4f750b
L3nn0x <dragon.super@laposte.net>
2018-04-10 01:56:53 +0200

    Corrected a SIGSEV

--- src/rosecommon/src/itemapi.cpp
+++ src/rosecommon/src/itemapi.cpp
@@ -5,22 +5,24 @@ using namespace RoseCommon;
 
 ItemAPI::ItemAPI(sol::environment&& env) : LuaAPI(std::move(env)) {
     // build the C++/lua connectors here
-    env_.set_function("getAttr", [this](void* entity, std::string attr) {
+    env_.set_function("getAttr", [](void* entity, std::string attr) {
         throw_assert(entity, "The entity cannot be nullptr");    
-        Entity e = *(Entity*)entity;
-        logger_->info("getAttr called for client {} and attr {}", getId(e), attr);
+        //Entity e = *(Entity*)entity;
+        (void)attr;
         return 42;
     });
       
-    env_.set_function("addBonusAttr", [this](void* entity, int32_t attr, int32_t value) {
+    env_.set_function("addBonusAttr", [](void* entity, int32_t attr, int32_t value) {
         throw_assert(entity, "The entity cannot be nullptr");
-        Entity e = *(Entity*)entity;
-        logger_->info("addBonusAttr called for client {} and attr {} value {}", getId(e), attr, value);
+        //Entity e = *(Entity*)entity;
+        (void)attr;
+        (void)value;
     });
       
-    env_.set_function("removeBonusAttr", [this](void* entity, int32_t attr, int32_t value) {
+    env_.set_function("removeBonusAttr", [](void* entity, int32_t attr, int32_t value) {
         throw_assert(entity, "The entity cannot be nullptr");
-        Entity e = *(Entity*)entity;
-        logger_->info("removeBonusAttr called for client {} and attr {} value {}", getId(e), attr, value);
+        //Entity e = *(Entity*)entity;
+        (void)attr;
+        (void)value;
     });
 }
