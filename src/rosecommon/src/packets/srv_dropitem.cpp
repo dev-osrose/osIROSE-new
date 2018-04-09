1adbadb735b465e15fe0def0c8fc948e66532a62
L3nn0x <dragon.super@laposte.net>
2018-04-09 23:33:18 +0200

    Added srv drop packet

---
+++ src/rosecommon/src/packets/srv_dropitem.cpp
@@ -0,0 +1,44 @@
+#include "srv_dropitem.h"
+#include "throwassert.h"
+
+namespace RoseCommon {
+
+SrvDropItem::SrvDropItem() : CRosePacket(ePacketType::PAKWC_DROP_ITEM) {}
+
+SrvDropItem::SrvDropItem(uint8_t buffer[MAX_PACKET_SIZE]) : CRosePacket(buffer) {
+	throw_assert(type() == ePacketType::PAKWC_DROP_ITEM, "Not the right packet: " << to_underlying(type()));
+	auto position = entity_.component<Position>();
+	auto item = entity_.component<RoseCommon::Item>();
+	auto basicInfo = entity_.component<BasicInfo>();
+
+	*this >> position->x_;
+	*this >> position->y_;
+	//TODO : implement roseCommon->Item_
+    (void)item;
+	*this >> basicInfo->id_;
+	*this >> basicInfo->ownerId_;
+
+}
+
+SrvDropItem::SrvDropItem(Entity entity) : CRosePacket(ePacketType::PAKWC_DROP_ITEM), entity_(entity) {}
+
+Entity SrvDropItem::entity() const {
+	return entity_;
+}
+
+
+void SrvDropItem::pack() {
+	auto position = entity_.component<Position>();
+	auto item = entity_.component<RoseCommon::Item>();
+	auto basicInfo = entity_.component<BasicInfo>();
+
+	*this << position->x_;
+	*this << position->y_;
+    *this << item->getHeader();
+    *this << item->getData();
+	*this << basicInfo->id_;
+	*this << basicInfo->ownerId_;
+
+}
+
+}
