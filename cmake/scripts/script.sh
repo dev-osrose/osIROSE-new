b4df9ec79324e22c1c622cc8e2edc3efb093d52e
Chris Torres <redflames1003@gmail.com>
2018-04-21 21:16:38 +0000

    changes stuff for running tests

--- cmake/scripts/script.sh
+++ cmake/scripts/script.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ "$SUITE" = "coverage" ]; then
-  cmake -GNinja -DWITH_COVERAGE_REPORTS=ON .. && cmake --build . --target all_unity -- -j 3 && cmake --build . --target coverage && coveralls-lcov coverage.info.cleaned
+  cmake -GNinja -DWITH_COVERAGE_REPORTS=ON .. && cmake --build . --target all_unity -- -j 3 && mv bin/unity/* bin/ && cmake --build . --target coverage && coveralls-lcov coverage.info.cleaned
 elif [ "$SUITE" = "tests" ]; then
-  cmake -GNinja .. && cmake --build . -- -j 3 && ctest --output-on-failure
+  cmake -GNinja .. && cmake --build . --target all_unity -- -j 3 && mv bin/unity/* bin/ && ctest --output-on-failure
 else
   cmake -GNinja -DOFFICIAL_BUILD=ON .. && cmake --build . -- -j 3
 fi
