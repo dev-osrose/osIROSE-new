4e2724e1f514f67f1fe46a6f4e51bb6ee32248c6
Chris Torres <redflames1003@gmail.com>
2018-04-21 19:06:09 +0000

    Updated unit tests settings

--- cmake/scripts/script.sh
+++ cmake/scripts/script.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ "$SUITE" = "coverage" ]; then
-  cmake -GNinja -DWITH_COVERAGE_REPORTS=ON .. && cmake --build . -- -j 4 && cmake --build . --target coverage && coveralls-lcov coverage.info.cleaned
+  cmake -GNinja -DWITH_COVERAGE_REPORTS=ON .. && cmake --build . --target all_unity -- -j 3 && cmake --build . --target coverage && coveralls-lcov coverage.info.cleaned
 elif [ "$SUITE" = "tests" ]; then
-  cmake -GNinja .. && cmake --build . -- -j 4 && ctest --output-on-failure
+  cmake -GNinja .. && cmake --build . -- -j 3 && ctest --output-on-failure
 else
-  cmake -GNinja -DBUILD_TESTS=OFF .. && cmake --build . -- -j 4
+  cmake -GNinja -DOFFICIAL_BUILD=ON .. && cmake --build . -- -j 3
 fi
