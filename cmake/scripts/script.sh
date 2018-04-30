67b99dcc690f359eaeafa99a13fac975e4eec719
Chris Torres <redflames1003@gmail.com>
2018-04-30 22:12:38 +0000

    Trying out ccache

--- cmake/scripts/script.sh
+++ cmake/scripts/script.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ "$SUITE" = "coverage" ]; then
-  cmake -GNinja -DWITH_COVERAGE_REPORTS=ON .. && cmake --build . -- -j 4 && cmake --build . --target coverage && coveralls-lcov coverage.info.cleaned
+  cmake -GNinja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DWITH_COVERAGE_REPORTS=ON .. && cmake --build . -- -j 4 && cmake --build . --target coverage && coveralls-lcov coverage.info.cleaned
 elif [ "$SUITE" = "tests" ]; then
-  cmake -GNinja .. && cmake --build . -- -j 4 && ctest --output-on-failure
+  cmake -GNinja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache .. && cmake --build . -- -j 4 && ctest --output-on-failure
 else
-  cmake -GNinja -DBUILD_TESTS=OFF .. && cmake --build . -- -j 4
+  cmake -GNinja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DBUILD_TESTS=OFF .. && cmake --build . -- -j 4
 fi
