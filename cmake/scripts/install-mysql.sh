6ebbfebe4a7936ce29d5ebc91276745a103cf46c
Chris Torres <redflames1003@gmail.com>
2018-05-01 15:42:41 +0000

    Fixed issue where mysql connector c didn't get cached

--- cmake/scripts/install-mysql.sh
+++ cmake/scripts/install-mysql.sh
@@ -15,7 +15,7 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
         tar -xf mysql-connector-c-6.1.6-src.tar.gz
         rm mysql-connector-c-6.1.6-src.tar.gz
         cd mysql-connector-c-6.1.6-src
-        cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$PROOT/3rdparty
+        cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$TRAVIS_BUILD_DIR/3rdparty
         make
         make install 
         cd .. && rm -r mysql-connector-c-6.1.6-src
