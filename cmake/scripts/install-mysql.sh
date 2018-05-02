d16d7f92f65bae4a567d0fc2d7443b6ab0e575b1
Chris Torres <redflames1003@gmail.com>
2018-05-02 16:38:24 +0000

    Fixed mysql connector c install directory

--- cmake/scripts/install-mysql.sh
+++ cmake/scripts/install-mysql.sh
@@ -15,7 +15,12 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
         tar -xf mysql-connector-c-6.1.6-src.tar.gz
         rm mysql-connector-c-6.1.6-src.tar.gz
         cd mysql-connector-c-6.1.6-src
-        cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$TRAVIS_BUILD_DIR/3rdparty
+        if [ "x$DESTDIR" == "x" ]; then
+          #The DESTDIR env var isn't set.
+          cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$TRAVIS_BUILD_DIR/3rdparty
+        else
+          cmake -G "Unix Makefiles"
+        fi
         make
         make install 
         cd .. && rm -r mysql-connector-c-6.1.6-src
