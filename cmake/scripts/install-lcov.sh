ccac9667bc970ecda26497fb2a193699c2f667e8
Chris Torres <redflames1003@gmail.com>
2018-05-02 21:06:55 +0000

    Added some more logging to lcov install

--- cmake/scripts/install-lcov.sh
+++ cmake/scripts/install-lcov.sh
@@ -8,6 +8,8 @@ echo 'Building and installing lcov.';
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   if [ ! -f "$TRAVIS_BUILD_DIR/3rdparty/usr/local/bin/lcov" ]; then
+    echo 'Setting up lcov...';
+    
     export DESTDIR=$TRAVIS_BUILD_DIR/3rdparty
     wget https://downloads.sourceforge.net/ltp/lcov-1.13.tar.gz
     tar -xf lcov-1.13.tar.gz
@@ -16,6 +18,8 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     make install
     cd ..
     rm -r lcov-1.13
+    
+    echo 'Finished installing lcov.';
   else
     echo 'Using cached lcov directory.';
   fi
