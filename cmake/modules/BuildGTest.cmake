36633fe56d9c34c4b0ca3bc3ac1d112fc778b2dd
Chris Torres <redflames1003@gmail.com>
2018-05-03 19:49:44 +0000

    Added gmock libs to the link list.

--- cmake/modules/BuildGTest.cmake
+++ cmake/modules/BuildGTest.cmake
@@ -1,3 +1,4 @@
+set(GTEST_INSTALL_DIR ${CMAKE_THIRD_PARTY_DIR})
 
 if(MSVC11)
   set(ADD_CXX_FLAGS "/D_VARIADIC_MAX=10")
@@ -20,20 +21,20 @@ if(WIN32)
     GIT_TAG 278aba369c41e90e9e77a6f51443beb3692919cf
     GIT_SHALLOW true
     
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=$ENV{PREFIX} -DCMAKE_CXX_FLAGS=${ADD_CXX_FLAGS} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DBUILD_GMOCK=${BUILD_GMOCK} -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS} -Dgmock_build_tests=OFF
-    INSTALL_DIR $ENV{PREFIX}
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${GTEST_INSTALL_DIR} -DCMAKE_CXX_FLAGS=${ADD_CXX_FLAGS} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DBUILD_GMOCK=${BUILD_GMOCK} -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS} -Dgmock_build_tests=OFF
+    INSTALL_DIR ${GTEST_INSTALL_DIR}
   )
 else()
   # This is a workaround for Ninja not allowing us to build if these libs weren't built before
   if(${BUILD_SHARED_LIBS})
     set(_byproducts
-      $ENV{PREFIX}/lib/libgtest.a
-      $ENV{PREFIX}/lib/libgmock.a
+      ${GTEST_INSTALL_DIR}/lib/libgtest.a
+      ${GTEST_INSTALL_DIR}/lib/libgmock.a
     )
   else()
     set(_byproducts
-      $ENV{PREFIX}/lib/libgtest.so
-      $ENV{PREFIX}/lib/libgmock.so
+      ${GTEST_INSTALL_DIR}/lib/libgtest.so
+      ${GTEST_INSTALL_DIR}/lib/libgmock.so
     )
   endif()
   
@@ -43,9 +44,9 @@ else()
     GIT_TAG 278aba369c41e90e9e77a6f51443beb3692919cf
     GIT_SHALLOW true
     
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=$ENV{PREFIX} -DCMAKE_CXX_FLAGS=${ADD_CXX_FLAGS} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DBUILD_GMOCK=${BUILD_GMOCK} -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS} -Dgmock_build_tests=OFF
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${GTEST_INSTALL_DIR} -DCMAKE_CXX_FLAGS=${ADD_CXX_FLAGS} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DBUILD_GMOCK=${BUILD_GMOCK} -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS} -Dgmock_build_tests=OFF
     BUILD_BYPRODUCTS ${_byproducts}
-    INSTALL_DIR $ENV{PREFIX}
+    INSTALL_DIR ${GTEST_INSTALL_DIR}
   )
 endif()
 
@@ -55,11 +56,6 @@ ExternalProject_Get_Property(
   install_dir
 )
 
-set(GTEST_INCLUDE_DIRS "${install_dir}/include/gtest")
-if(BUILD_GMOCK)
-  set(GTEST_INCLUDE_DIRS "${install_dir}/include/gmock;${GTEST_INCLUDE_DIRS}")
-endif()
-
 if(WIN32)
   set(GTEST_LIBRARIES "${install_dir}/${CMAKE_BUILD_TYPE}/gtest.lib")
   set(GTEST_INSTALL_LIBS "${install_dir}/${CMAKE_BUILD_TYPE}/gtest.dll")
@@ -67,10 +63,23 @@ else()
   if(${BUILD_SHARED_LIBS})
     set(GTEST_LIBRARIES "${install_dir}/lib/libgtest.so")
     set(GTEST_INSTALL_LIBS "${install_dir}/lib/libgtest.so")
+    if(BUILD_GMOCK)
+      set(GTEST_LIBRARIES "${install_dir}/lib/libgmock.so;${GTEST_LIBRARIES}")
+      set(GTEST_INSTALL_LIBS "${install_dir}/lib/libgmock.so;${GTEST_INSTALL_LIBS}")
+    endif()
   else()
     set(GTEST_LIBRARIES "${install_dir}/lib/libgtest.a")
     set(GTEST_INSTALL_LIBS "${install_dir}/lib/libgtest.a")
+    if(BUILD_GMOCK)
+      set(GTEST_LIBRARIES "${install_dir}/lib/libgmock.a;${GTEST_LIBRARIES}")
+      set(GTEST_INSTALL_LIBS "${install_dir}/lib/libgmock.a;${GTEST_INSTALL_LIBS}")
+    endif()
   endif()
+endif()
+
+set(GTEST_INCLUDE_DIRS "${install_dir}/include/gtest")
+if(BUILD_GMOCK)
+  set(GTEST_INCLUDE_DIRS "${install_dir}/include/gmock;${GTEST_INCLUDE_DIRS}")
 endif()
 
 set(GTEST_BOTH_LIBRARIES ${GTEST_LIBRARIES} ${GTEST_MAIN_LIBRARIES})
