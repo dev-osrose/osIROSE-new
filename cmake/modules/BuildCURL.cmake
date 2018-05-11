aecb81a613e9bfd5b909a126cc1bfb27f6299c18
Chris Torres <redflames1003@gmail.com>
2018-05-11 15:56:40 +0000

    Fixed Gtest errors on windows

--- cmake/modules/BuildCURL.cmake
+++ cmake/modules/BuildCURL.cmake
@@ -4,7 +4,6 @@ if(WIN32)
     GIT_REPOSITORY https://github.com/curl/curl.git
     GIT_TAG curl-7_59_0
     CMAKE_ARGS -DBUILD_TESTING=OFF -DHTTP_ONLY=ON
-    BUILD_IN_SOURCE 1
   )
 else()
   ExternalProject_Add(
@@ -12,23 +11,22 @@ else()
     GIT_REPOSITORY https://github.com/curl/curl.git
     GIT_TAG curl-7_59_0
     CMAKE_ARGS -DBUILD_TESTING=OFF -DHTTP_ONLY=ON
-    BUILD_IN_SOURCE 1
   )
 endif()
 
 ExternalProject_Get_Property(
   curl
-  source_dir
+  binary_dir
 )
 
 set(CURL_INCLUDE_DIRS "${source_dir}/include")
 
 if(WIN32)
-  set(CURL_LIBRARIES "${source_dir}/${CMAKE_BUILD_TYPE}/libcurl.lib")
-  set(CURL_INSTALL_LIBS "${source_dir}/${CMAKE_BUILD_TYPE}/libcurl.dll")
+  set(CURL_LIBRARIES "${binary_dir}/${CMAKE_BUILD_TYPE}/libcurl.lib")
+  set(CURL_INSTALL_LIBS "${binary_dir}/${CMAKE_BUILD_TYPE}/libcurl.dll")
 else()
-  set(CURL_LIBRARIES "${source_dir}/libcurl.so")
-  set(CURL_INSTALL_LIBS "${source_dir}/libcurl.so")
+  set(CURL_LIBRARIES "${binary_dir}/libcurl.so")
+  set(CURL_INSTALL_LIBS "${binary_dir}/libcurl.so")
 endif()
 
 install_external_library(curl ${CURL_INSTALL_LIBS})
