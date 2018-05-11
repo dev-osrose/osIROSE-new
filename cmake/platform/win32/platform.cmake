77a829ecf205decddcb8022fa39a047a85ac7401
Chris Torres <redflames1003@gmail.com>
2018-05-11 13:59:09 +0000

    More windows updates

--- cmake/platform/win32/platform.cmake
+++ cmake/platform/win32/platform.cmake
@@ -3,6 +3,12 @@ add_compiler_flags(-DASIO_STANDALONE)
 
 add_compiler_flags(-D_SILENCE_ALL_CXX17_DEPRECATION_WARNINGS) # Remove all of the really annoying c++17 deprection warnings
 
+if(CMAKE_CFG_INTDIR)
+  set(${CMAKE_CFG_INTDIR} TRUE)
+  set(CMAKE_BUILD_TYPE ${CMAKE_CFG_INTDIR})
+endif()
+
+
 macro(use_unicode_here)
   add_definitions(-D_UNICODE -DUNICODE)
 endmacro()
