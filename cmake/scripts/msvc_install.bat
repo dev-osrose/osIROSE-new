bd0a736694d3657760bcc44b9775ecc2df7d0e5a
Chris Torres <redflames1003@gmail.com>
2018-05-21 21:04:47 +0000

    Fixed mkdir syntax on windows

--- cmake/scripts/msvc_install.bat
+++ cmake/scripts/msvc_install.bat
@@ -1,5 +1,5 @@
 mkdir build
-mkdir 3rdparty/include
+mkdir 3rdparty\include
 REM mkdir 3rdparty\build
 REM Robocopy "./3rdparty/spdlog/include" "./3rdparty/include" /E /MT:2 /R:0 /DCOPY:T
 pip install pyparsing
