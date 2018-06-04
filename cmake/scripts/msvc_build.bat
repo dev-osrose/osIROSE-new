84493a8bb6cc3f201ddd00d402028fc1e386b024
Chris Torres <redflames1003@gmail.com>
2018-06-04 12:33:07 +0000

    Update uploaded artifact on appveyor to include the scripts dir

--- cmake/scripts/msvc_build.bat
+++ cmake/scripts/msvc_build.bat
@@ -1,8 +1,9 @@
 @call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
 SET PATH=%PROJECT_PATH%\3rdparty\bin;%MYSQL_CONNECTOR_PATH%\lib;%MYSQL_PATH%\bin;%PYTHON_PATH%\Scripts;%PATH%
 
-cp "%MYSQL_CONNECTOR_PATH%\lib\libmysql.dll" "%PROJECT_PATH%\build\bin\%CONFIGURATION%
-cp "%MYSQL_CONNECTOR_PATH%\lib\libmysql.dll" "%PROJECT_PATH%\build\bin\
+REM cp "%MYSQL_CONNECTOR_PATH%\lib\libmysql.dll" "%PROJECT_PATH%\build\bin\%CONFIGURATION%
+cp "%MYSQL_CONNECTOR_PATH%\lib\libmysql.dll" "%PROJECT_PATH%\build\bin\"
+%PROJECT_PATH%\cmake\scripts\robocopy.bat "%PROJECT_PATH%\scripts" "%PROJECT_PATH%\build\bin\scripts" "*.lua"
 
 cmake -GNinja -D%CONFIGURATION%=TRUE -DCMAKE_BUILD_TYPE=%CONFIGURATION% ..
 cmake --build . --config %CONFIGURATION%
