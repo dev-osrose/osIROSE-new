46d4e85b6f7e7871738d01df50f357e859058b3b
L3nn0x <dragon83.super@gmail.com>
2018-06-04 17:15:05 +0100

    Update id_manager.h

--- src/map/include/id_manager.h
+++ src/map/include/id_manager.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <unordered_set>
+
 class IdManager {
     public:
         IdManager();
@@ -9,4 +11,8 @@ class IdManager {
         
         uint16_t get_free_id();
         void release_id(uint16_t id);
+    
+    private:
+        std::unordered_set<uint16_t> _free_ids;
+        uint16_t _max_id;
 };
