15a231d36dffdf2daca42c78f82511b1b38361ca
L3nn0x <dragon83.super@gmail.com>
2018-06-06 12:28:28 +0100

    Update entitysystem.h

--- src/map/include/entitysystem.h
+++ src/map/include/entitysystem.h
@@ -78,11 +78,11 @@ class EntitySystem {
 
   EntityManager& getEntityManager();
  
-  void create_warpgate(std::string alias, int dest_map_id, float dest_x, float dest_y, float dest_z,
+  Entity create_warpgate(std::string alias, int dest_map_id, float dest_x, float dest_y, float dest_z,
                       int map_id, float x, float y, float z, float angle,
                       float x_scale, float y_scale, float z_scale);
-  void create_npc(std::string npc_lua, int npc_id, int map_id, float x, float y, float z, float angle);
-  void create_spawner(std::string alias, int mob_id, int mob_count,
+  Entity create_npc(std::string npc_lua, int npc_id, int map_id, float x, float y, float z, float angle);
+  Entity create_spawner(std::string alias, int mob_id, int mob_count,
                      int spawner_limit, int spawner_interval, int spawner_range,
                      int map_id, float x, float y, float z);
 
