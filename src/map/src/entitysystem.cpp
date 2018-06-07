0ae3eb11672e17c0e71a0e25dac6d1be2c1d1e7e
L3nn0x <dragon83.super@gmail.com>
2018-06-07 11:37:14 +0100

    Update entitysystem.cpp

--- src/map/src/entitysystem.cpp
+++ src/map/src/entitysystem.cpp
@@ -230,14 +230,17 @@ void EntitySystem::saveCharacter(uint32_t charId, Entity entity) {
 Entity EntitySystem::create_warpgate(std::string alias, int dest_map_id, float dest_x, float dest_y, float dest_z,
                     int map_id, float x, float y, float z, float angle,
                     float x_scale, float y_scale, float z_scale) {
+  return {};
 }
 
 Entity EntitySystem::create_npc(std::string npc_lua, int npc_id, int map_id, float x, float y, float z, float angle) {
+  return {};
 }
 
 Entity EntitySystem::create_spawner(std::string alias, int mob_id, int mob_count,
                    int spawner_limit, int spawner_interval, int spawner_range,
                    int map_id, float x, float y, float z) {
+  return {};
 }
 
 void EntitySystem::bulk_destroy(const std::vector<Entity>& s) {
