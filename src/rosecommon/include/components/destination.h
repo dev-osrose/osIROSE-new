9dc5051e5fa5dcaff86a8e81b09ce9bb53a63870
L3nn0x <dragon83.super@gmail.com>
2018-06-11 10:09:51 +0100

    Update destination.h

--- src/rosecommon/include/components/destination.h
+++ src/rosecommon/include/components/destination.h
@@ -1,11 +1,10 @@
 #pragma once
 
 struct Destination {
-    Destination(float x, float y, uint16_t dist, uint16_t target = 0) : x_(x), y_(y), dist_(dist), target_(target) {}
+    Destination(float x, float y, uint16_t dist) : x_(x), y_(y), dist_(dist) {}
 
     float x_;
     float y_;
     uint16_t dist_;
-    uint16_t target_;
 };
 
