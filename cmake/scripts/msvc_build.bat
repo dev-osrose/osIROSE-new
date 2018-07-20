ebd374f044f35ff632661b6b8dab070cc240f637
Christopher Torres <RavenX8@users.noreply.github.com>
2018-07-20 11:38:15 -0400

    Updated path to 64 bit compiler env

--- cmake/scripts/msvc_build.bat
+++ cmake/scripts/msvc_build.bat
@@ -1,4 +1,4 @@
-@call "C:\Program Files\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 SET PATH=%PROJECT_PATH%\3rdparty\bin;%MYSQL_CONNECTOR_PATH%\lib;%MYSQL_PATH%\bin;%PYTHON_PATH%\Scripts;%PATH%
 
 cp "%MYSQL_CONNECTOR_PATH%\lib\libmysql.dll" "%PROJECT_PATH%\build\bin\"
