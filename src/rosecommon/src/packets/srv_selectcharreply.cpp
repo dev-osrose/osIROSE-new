dev-osrose/osIROSE-new

commit 682a54ebf40f0f2be8f299235432f0d42b841e6e

Switched to use std::chrono everywhere

src/rosecommon/src/packets/srv_selectcharreply.cpp
@@ -62,7 +62,7 @@ void SrvSelectCharReply::pack() {
 	*this << characterInfo->pkFlag_;
 	*this << characterInfo->stamina_;
 	for (auto &it : statusEffects->effects_) {
-        *this << it.expiredSeconds_ << it.value_ << it.unkown_;
+        *this << it.expired_.count() << it.value_ << it.unkown_;
 	}
 	*this << characterInfo->patHp_;
 	*this << characterInfo->patCooldownTime_;

src/map/CMakeLists.txt
@@ -26,6 +26,7 @@ SET( SOURCES_HEADER
   include/systems/inventorysystem.h
   include/systems/partysystem.h
   include/systems/mapsystem.h
+  include/systems/spawnersystem.h
   )
 
 SET( SOURCES_MAIN
@@ -46,6 +47,7 @@ SET( SOURCES_MAIN
   src/systems/inventorysystem.cpp
   src/systems/partysystem.cpp
   src/systems/mapsystem.cpp
+  src/systems/spawnersystem.cpp
   )
 
 add_executable(${PROJECT} ${SOURCES_MAIN} ${SOURCES_HEADER})

src/map/include/cmapserver.h
@@ -20,6 +20,7 @@
 #include "croseserver.h"
 #include "entitysystem.h"
 #include "script_loader.h"
+#include <chrono>
 
 class CMapServer : public RoseCommon::CRoseServer {
  public:
@@ -28,7 +29,7 @@ class CMapServer : public RoseCommon::CRoseServer {
 
   int16_t GetMapIDX() const { return map_idx_; }
 
-  void update(double dt);
+  void update(std::chrono::milliseconds dt);
 
   void SendPacket(const std::shared_ptr<CMapClient>& sender, RoseCommon::CRoseServer::eSendType type,
                          RoseCommon::CRosePacket& _buffer);

src/map/include/entitysystem.h
@@ -28,6 +28,7 @@
 #include <type_traits>
 #include <unordered_map>
 #include <vector>
+#include <chrono>
 #include "crosepacket.h"
 #include "item.h"
 #include "systems/system.h"
@@ -50,7 +51,7 @@ class EntitySystem {
  public:
   EntitySystem(CMapServer *server);
 
-  void update(double dt);
+  void update(std::chrono::milliseconds dt);
 
   void destroy(Entity entity, bool save);
 

src/map/include/systemmanager.h
@@ -27,6 +27,7 @@
 #include <typeindex>
 #include <unordered_map>
 #include <vector>
+#include <chrono>
 #include "crosepacket.h"
 #include "entitycomponents.h"
 #include "epackettype.h"
@@ -73,7 +74,7 @@ class SystemManager {
     return nullptr;
   }
 
-  void update(double dt);
+  void update(std::chrono::milliseconds dt);
 
   bool wouldDispatch(const RoseCommon::CRosePacket &packet) {
     auto res = dispatch_.equal_range(to_underlying(packet.type()));
@@ -115,6 +116,7 @@ class SystemManager {
   Entity getEntity(uint32_t charId) const;
 
   Entity buildItem(Entity creator, RoseCommon::Item &&);
+  Entity buildMob(Entity spawner);
 
   EntityManager &getEntityManager();
 

src/map/include/systems/chatsystem.h
@@ -15,7 +15,7 @@ class ChatSystem : public System {
   ChatSystem(SystemManager &manager);
   virtual ~ChatSystem() = default;
 
-  virtual void update(EntityManager &es, double dt);
+  virtual void update(EntityManager &es, std::chrono::milliseconds dt) override;
 
   void sendMsg(Entity entity, const std::string &msg);
 

src/map/include/systems/inventorysystem.h
@@ -17,7 +17,7 @@ class InventorySystem : public System {
   InventorySystem(SystemManager &manager);
   virtual ~InventorySystem() = default;
 
-  virtual void update(EntityManager &es, double dt);
+  virtual void update(EntityManager &es, std::chrono::milliseconds dt) override;
 
   static uint8_t findNextEmptySlot(Entity entity);
   static bool swapItems(Entity entity, uint8_t &a, uint8_t &b);

src/map/include/systems/luasystem.h
@@ -43,7 +43,7 @@ class LuaSystem : public System {
           return std::make_optional(std::move(lua));
         }
 
-    virtual void update(EntityManager&, double) {}
+    virtual void update(EntityManager&, std::chrono::milliseconds) override {}
 
         /*void unregisterEntity(Entity e) {
           auto it = std::find(callbacks_.begin(), callbacks_.end(), Callback{e});

commit 492e5f62b98f2984f455d84e18279d6f87dc78b4

Fixed missing name
Fixed issue where 682a54ebf40f0f2be8f299235432f0d42b841e6e broke the size of select character packet

src/rosecommon/src/packets/srv_selectcharreply.cpp
@@ -62,10 +62,10 @@ void SrvSelectCharReply::pack() {
 	*this << characterInfo->pkFlag_;
 	*this << characterInfo->stamina_;
 	for (auto &it : statusEffects->effects_) {
-        *this << it.expired_.count() << it.value_ << it.unkown_;
+        *this << (uint32_t)it.expired_.count() << it.value_ << it.unkown_;
 	}
 	*this << characterInfo->patHp_;
-	*this << characterInfo->patCooldownTime_;
+	*this << (uint32_t)characterInfo->patCooldownTime_.count();
 	for (auto &it : skills->skills_) {
         *this << it.id_;
 	}