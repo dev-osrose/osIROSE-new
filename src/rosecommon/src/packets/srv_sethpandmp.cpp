33a0448925e54445c7f2ef9cfbc9267d5499fb97
Raven <RavenX8@users.noreply.github.com>
2018-08-30 02:16:36 +0000

    Updated set hp and mp packet

--- src/rosecommon/src/packets/srv_sethpandmp.cpp
+++ src/rosecommon/src/packets/srv_sethpandmp.cpp
@@ -12,11 +12,27 @@ Entity SrvSetHpAndMp::entity() const {
 
 void SrvSetHpAndMp::pack() {
   auto basicInfo = entity_.component<BasicInfo>();
+  auto stats = entity_.component<Stats>();
   auto advanced = entity_.component<AdvancedInfo>();
 
   *this << basicInfo->id_;
-  *this << advanced->hp_;
-  *this << advanced->mp_;
+  
+  // If the value is 0, the client won't do anything
+  
+  if(stats) {
+    if(advanced->hp_ == stats->maxHp_)
+      *this << (uint16_t)0;
+    else
+      *this << (uint16_t)advanced->hp_;
+      
+    if(advanced->hp_ == stats->maxMp_)
+      *this << (uint16_t)0;
+    else
+      *this << (uint16_t)advanced->mp_;
+  } else {
+    *this << (uint16_t)0;
+    *this << (uint16_t)0;
+  }
 }
 
 }
