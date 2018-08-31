c394c30620dcc81894047ff0d6e08f44a1fe559d
Christopher Torres <RavenX8@users.noreply.github.com>
2018-08-31 18:36:57 -0400

    deleted scripts directory

--- scripts/spawns/field/mudojang.lua
+++
@@ -1,6 +0,0 @@
---[[ SPAWN LIST
-login_point(<warp_alias>, <dest_map_id>, <dest_x_pos>, <dest_y_pos>, <dest_z_pos>, <map_id>, <x_pos>, <y_pos>, <z_pos>, <angle>, <x_scale>, <y_scale>, <z_scale>);
-respawn_point(<warp_alias>, <dest_map_id>, <dest_x_pos>, <dest_y_pos>, <dest_z_pos>, <map_id>, <x_pos>, <y_pos>, <z_pos>, <angle>, <x_scale>, <y_scale>, <z_scale>);
---]]
-login_point(40, 5180.85, 5210.49);
-respawn_point(40, 5189.88, 5211.03);
--- scripts/warps/cities/JPT01.lua
+++
@@ -1,4 +0,0 @@
---[[ WARP LIST
-warp_gate(<warp_alias>, <dest_map_id>, <dest_x_pos>, <dest_y_pos>, <dest_z_pos>, <map_id>, <x_pos>, <y_pos>, <z_pos>, <angle>, <x_scale>, <y_scale>, <z_scale>);
---]]
-warp_gate("", 25, 5407.837, 4949.692, 38.38167, 2, 5639.49, 5563.82, -0.82, 5.288348, 1.3, 1.3, 1.3);
