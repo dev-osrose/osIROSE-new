6588629bf02bd233d86ccc85ae3bbdf66cb87248
L3nn0x <dragon.super@laposte.net>
2018-09-03 23:50:34 +0100

    Updated cmakelist

--- cmake/modules/FlatBuffersGenerator.cmake
+++ cmake/modules/FlatBuffersGenerator.cmake
@@ -1,17 +1,12 @@
-function(GENERATE_FLATBUFFERS_HEADER HDRS)
-    cmake_parse_arguments(ARG "DEBUG" "FLATBUFFERROOT;NAME;OUTPATH;EXPORT_MACRO;TARGET" "FLATBUFFERFILE" ${ARGN})
+function(GENERATE_FLATBUFFERS_HEADERS HDRS)
+    cmake_parse_arguments(ARG "DEBUG" "FLATBUFFERSROOT;OUTPATH;EXPORT_MACRO;TARGET" "FLATBUFFERSFILE" ${ARGN})
 
-    if(NOT ARG_FLATBUFFERFILE)
+    if(NOT ARG_FLATBUFFERSFILE)
         message(SEND_ERROR "Error: GENERATE_FLATBUFFERS_HEADER() called without any flatbuffers files")
     return()
-endif(NOT ARG_FLATBUFFERFILE)
+endif(NOT ARG_FLATBUFFERSFILE)
 
-  if(NOT ARG_NAME)
-      message(SEND_ERROR "Error: GENERATE_FLATBUFFERS_HEADER() called without any class name")
-    return()
-  endif(NOT ARG_NAME)
-
-  list(LENGTH ARG_FLATBUFFERSROOT FLATBUFFERS_LENGTH)
+list(LENGTH ARG_FLATBUFFERSROOT FLATBUFFERSROOT_LENGTH)
   if(FLATBUFFERSROOT_LENGTH GREATER 1)
       message(SEND_ERROR "Error: GENERATE_FLATBUFFERS_HEADER() called with too many flatbuffersroots, only one is allowed")
     return()
@@ -29,26 +24,27 @@ endif(NOT ARG_FLATBUFFERFILE)
   endif()
 
   set(FLATBUFFERSROOT ${CMAKE_CURRENT_SOURCE_DIR})
-  if(FLATBUFFERSOOT_LENGTH EQUAL 1)
+  if(FLATBUFFERSROOT_LENGTH EQUAL 1)
       set(FLATBUFFERSROOT ${ARG_FLATBUFFERSROOT})
   endif()
 
   if(ARG_DEBUG)
     message("OUTPATH: ${OUTPATH}")
     message("FLATBUFFERSFILE: ${ARG_FLATBUFFERSFILE}")
+    message("FLATBUFFERSROOT: ${FLATBUFFERSROOT}")
   endif()
 
   set(${HDRS})
   foreach(FLATBUFFERSFILE ${ARG_FLATBUFFERSFILE})
 
-    # ensure that the file ends with .proto
-    string(REGEX MATCH "\\.flatc$$" PROTOEND ${FLATBUFFERSFILE})
+    # ensure that the file ends with .fbs
+    string(REGEX MATCH "\\.fbs$$" PROTOEND ${FLATBUFFERSFILE})
     if(NOT PROTOEND)
-        message(SEND_ERROR "Flatbuffers file '${FLATBUFFERSFILE}' does not end with .flatc")
+        message(SEND_ERROR "Flatbuffers file '${FLATBUFFERSFILE}' does not end with .fbs")
     endif()
 
-    get_filename_component(FLATBUFFERS_PATH ${FLATBUFFERSFILE} PATH)
-    get_filename_component(ABS_FILE ${FLATBUFFERSFILE} ABSOLUTE)
+    get_filename_component(FLATBUFFERS_PATH "${FLATBUFFERSROOT}/${FLATBUFFERSFILE}" PATH)
+    get_filename_component(ABS_FILE "${FLATBUFFERSROOT}/${FLATBUFFERSFILE}" ABSOLUTE)
     get_filename_component(FILE_WE ${FLATBUFFERSFILE} NAME_WE)
 
     if(ARG_DEBUG)
@@ -60,7 +56,7 @@ endif(NOT ARG_FLATBUFFERFILE)
     endif()
 
     # find out of the file is in the specified proto root
-    # TODO clean the SQLROOT so that it does not form a regex itself?
+    # TODO clean the FLATBUFFERSROOT so that it does not form a regex itself?
     string(REGEX MATCH "^${FLATBUFFERSROOT}" IN_ROOT_PATH ${FLATBUFFERSFILE})
     string(REGEX MATCH "^${FLATBUFFERSROOT}" IN_ROOT_ABS_FILE ${ABS_FILE})
 
@@ -77,12 +73,12 @@ endif(NOT ARG_FLATBUFFERFILE)
     if(ARG_DEBUG)
       message("  ROOT_CLEANED_FILE=${ROOT_CLEANED_FILE}")
     endif()
-    string(REGEX REPLACE "\\.flatc$$" "" EXT_CLEANED_FILE ${ROOT_CLEANED_FILE})
+    string(REGEX REPLACE "\\.fbs$$" "" EXT_CLEANED_FILE ${ROOT_CLEANED_FILE})
     if(ARG_DEBUG)
         message("  EXT_CLEANED_FILE=${EXT_CLEANED_FILE}")
     endif()
 
-    set(H_FILE "${OUTPATH}/${ARG_NAME}.h")
+    set(H_FILE "${OUTPATH}/${EXT_CLEANED_FILE}.h")
 
     if(ARG_DEBUG)
       message("  H_FILE=${H_FILE}")
@@ -93,8 +89,8 @@ endif(NOT ARG_FLATBUFFERFILE)
     add_custom_command(
       OUTPUT "${H_FILE}"
       COMMAND ${CMAKE_COMMAND} -E make_directory ${OUTPATH}
-      COMMAND flatc
-      ARGS ${MATCH_PATH} "${OUTPATH}/${ARG_NAME}" ${ARG_NAME}
+      COMMAND ./bin/flatc
+      ARGS "-c -b -o" ${OUTPATH} ${ABS_FILE}
       DEPENDS ${ABS_FILE}
       COMMENT "Running flatc compiler on ${MATCH_PATH} with root ${FLATBUFFERSROOT}, generating: ${H_FILE}"
       VERBATIM)
