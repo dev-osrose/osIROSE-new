81347d52c8e72a833d6819fecb52a73b62e02dbd
L3nn0x <dragon.super@laposte.net>
2018-09-04 00:10:49 +0100

    Added as module

---
+++ cmake/modules/BuildFlatbuffers.cmake
@@ -0,0 +1,42 @@
+set(FLATBUFFERS_INSTALL_DIR ${CMAKE_THIRD_PARTY_DIR})
+
+if(WIN32)
+
+  ExternalProject_Add(
+    flatbuffers
+    GIT_REPOSITORY https://github.com/google/flatbuffers.git
+    GIT_TAG 3c54fd964b6beae9a92955415568a001c9cea23d
+    CMAKE_ARGS -G ${CMAKE_GENERATOR} -DFLATBUFFERS_BUILD_TESTS=OFF -DFLATBUFFERS_BUILD_FLATHASH=OFF -DFLATBUFFERS_BUILD_FLATLIB=OFF -DFLATBUFFERS_BUILD_FLATHASH=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+    INSTALL_DIR ${FLATBUFFERS_INSTALL_DIR}
+  )
+  
+  ExternalProject_Add_Step(
+    flatbuffers
+    copy-flatbuffers
+    DEPENDEES download
+    COMMAND ${CMAKE_SCRIPT_PATH}/robocopy.bat "<SOURCE_DIR>/src" "<INSTALL_DIR>/include/" "*.h"
+    COMMAND ${CMAKE_SCRIPT_PATH}/robocopy.bat "<SOURCE_DIR>/src" "<INSTALL_DIR>/include/" "*.hpp"
+  )
+else()
+  ExternalProject_Add(
+    flatbuffers
+    GIT_REPOSITORY https://github.com/google/flatbuffers.git
+    GIT_TAG 3c54fd964b6beae9a92955415568a001c9cea23d
+    CMAKE_ARGS -G ${CMAKE_GENERATOR} -DFLATBUFFERS_BUILD_TESTS=OFF -DFLATBUFFERS_BUILD_FLATHASH=OFF -DFLATBUFFERS_BUILD_FLATLIB=OFF -DFLATBUFFERS_BUILD_FLATHASH=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+    INSTALL_DIR ${FLATBUFFERS_INSTALL_DIR}
+    BUILD_IN_SOURCE TRUE
+
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND make
+    INSTALL_COMMAND make install INSTALL_TOP=<INSTALL_DIR>
+  )
+endif()
+
+ExternalProject_Get_Property(
+  flatbuffers
+  install_dir
+)
+
+set(FLATBUFFERS_INCLUDE_DIR "${install_dir}/include")
+
+mark_as_advanced(FLATBUFFERS_INCLUDE_DIR)
