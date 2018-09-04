11af39c4f056dd30d384a55bb0c91fe209adbc82
Raven <RavenX8@users.noreply.github.com>
2018-09-04 20:21:29 +0000

    fix sqlpp11 generation trying to generate headers before the exe is created

--- cmake/modules/SQLPPGenerator.cmake
+++ cmake/modules/SQLPPGenerator.cmake
@@ -95,7 +95,7 @@ function(GENERATE_SQL_HEADER HDRS)
       COMMAND ${CMAKE_COMMAND} -E make_directory ${OUTPATH}
       COMMAND python
       ARGS ${SQLPP11_EXE_PATH} ${MATCH_PATH} "${OUTPATH}/${ARG_NAME}" ${ARG_NAME}
-      DEPENDS ${ABS_FILE}
+      DEPENDS ${ABS_FILE} ${SQLPP11_EXE_PATH}
       COMMENT "Running C++ sqlpp11 ddl12cpp compiler on ${MATCH_PATH} with root ${SQLROOT}, generating: ${H_FILE}"
       VERBATIM)
 
