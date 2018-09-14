f7bca51a37896092de1a22af269655439409ce15
L3nn0x <dragon83.super@gmail.com>
2018-09-14 11:34:40 +0100

    Update cli_changemapreq.cpp

--- src/rosecommon/src/packets/cli_changemapreq.cpp
+++ src/rosecommon/src/packets/cli_changemapreq.cpp
@@ -9,8 +9,8 @@ CliChangeMapReq::CliChangeMapReq() : CRosePacket(ePacketType::PAKCS_CHANGE_MAP_R
 
 CliChangeMapReq::CliChangeMapReq(uint8_t buffer[MAX_PACKET_SIZE]) : CRosePacket(buffer) {
 	throw_assert(type() == ePacketType::PAKCS_CHANGE_MAP_REQ, "Not the right packet: " << to_underlying(type()));
-	*this >> weightRate_;
-	*this >> z_;
+	weightRate_ = read_uint8();
+	z_ = read_uint16();
 }
 
 CliChangeMapReq::CliChangeMapReq(uint8_t weightRate, uint16_t z) : CRosePacket(ePacketType::PAKCS_CHANGE_MAP_REQ), weightRate_(weightRate), z_(z) {}
@@ -23,10 +23,17 @@ uint16_t CliChangeMapReq::z() const {
 	return z_;
 }
 
+void CliChangeMapReq::weightRate(uint8_t rate) {
+	weightRate_ = rate;
+}
+
+void CliChangeMapReq::z(uint16_t z) {
+	z_ = z;
+}
 
 void CliChangeMapReq::pack() {
-	*this << weightRate_;
-	*this << z_;
+	write_uint8(weightRate_);
+	write_uint16(z_);
 }
 
 }
