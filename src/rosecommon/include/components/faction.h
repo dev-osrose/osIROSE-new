124897fa20cd7b8ae1d03045d27ab110c231fe5b
L3nn0x <dragon83.super@gmail.com>
2018-12-10 11:48:21 +0000

    Update faction.h

--- src/rosecommon/include/components/faction.h
+++ src/rosecommon/include/components/faction.h
@@ -8,6 +8,6 @@ struct Faction {
     uint8_t rank;
     uint8_t fame;
     std::array<uint16_t, 2> factionFame;
-    std::array<uint16_t, 10> points;
+    std::array<uint16_t, 10> points = {0};
 };
 }
