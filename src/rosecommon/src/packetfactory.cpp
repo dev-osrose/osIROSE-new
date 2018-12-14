350af38980ca9a583eab2eb52c6456aa46898234
L3nn0x <dragon83.super@gmail.com>
2018-12-14 12:24:34 +0000

    Update packetfactory.cpp

--- src/rosecommon/src/packetfactory.cpp
+++ src/rosecommon/src/packetfactory.cpp
@@ -3,18 +3,21 @@
 
 
 #include "cli_accept_req.h"
+#include "cli_alive.h"
 #include "cli_channel_list_req.h"
 #include "cli_char_list_req.h"
 #include "cli_create_char_req.h"
 #include "cli_delete_char_req.h"
 #include "cli_join_server_req.h"
 #include "cli_login_req.h"
+#include "cli_normal_chat.h"
 #include "cli_select_char_req.h"
 #include "cli_srv_select_req.h"
 #include "isc_alive.h"
 #include "isc_server_register.h"
 #include "isc_shutdown.h"
 #include "srv_accept_reply.h"
+#include "srv_billing_message.h"
 #include "srv_channel_list_reply.h"
 #include "srv_char_list_reply.h"
 #include "srv_create_char_reply.h"
@@ -24,6 +27,7 @@
 #include "srv_login_reply.h"
 #include "srv_quest_data.h"
 #include "srv_screen_shot_time_reply.h"
+#include "srv_select_char_reply.h"
 #include "srv_srv_select_reply.h"
 #include "srv_switch_server.h"
 
@@ -36,11 +40,13 @@ using namespace RoseCommon::Packet;
 
 void RoseCommon::register_recv_packets() {
     REGISTER_RECV_PACKET(ePacketType::PAKCS_ACCEPT_REQ, CliAcceptReq);
+    REGISTER_RECV_PACKET(ePacketType::PAKCS_ALIVE, CliAlive);
     REGISTER_RECV_PACKET(ePacketType::PAKCS_CHANNEL_LIST_REQ, CliChannelListReq);
     REGISTER_RECV_PACKET(ePacketType::PAKCS_CHAR_LIST_REQ, CliCharListReq);
     REGISTER_RECV_PACKET(ePacketType::PAKCS_CREATE_CHAR_REQ, CliCreateCharReq);
     REGISTER_RECV_PACKET(ePacketType::PAKCS_DELETE_CHAR_REQ, CliDeleteCharReq);
     REGISTER_RECV_PACKET(ePacketType::PAKCS_LOGIN_REQ, CliLoginReq);
+    REGISTER_RECV_PACKET(ePacketType::PAKCS_NORMAL_CHAT, CliNormalChat);
     REGISTER_RECV_PACKET(ePacketType::PAKCS_SELECT_CHAR_REQ, CliSelectCharReq);
     REGISTER_RECV_PACKET(ePacketType::PAKCS_SRV_SELECT_REQ, CliSrvSelectReq);
     REGISTER_RECV_PACKET(ePacketType::ISC_ALIVE, IscAlive);
@@ -59,10 +65,10 @@ void RoseCommon::register_send_packets() {
     REGISTER_SEND_PACKET(ePacketType::PAKCC_DELETE_CHAR_REPLY, SrvDeleteCharReply);
     REGISTER_SEND_PACKET(ePacketType::PAKWC_INVENTORY_DATA, SrvInventoryData);
     REGISTER_SEND_PACKET(ePacketType::PAKSC_JOIN_SERVER_REPLY, SrvJoinServerReply);
-    REGISTER_SEND_PACKET(ePacketType::PAKSC_JOIN_SERVER_REPLY, SrvLoginReply);
     REGISTER_SEND_PACKET(ePacketType::PAKLC_LOGIN_REPLY, SrvLoginReply);
     REGISTER_SEND_PACKET(ePacketType::PAKWC_QUEST_DATA, SrvQuestData);
     REGISTER_SEND_PACKET(ePacketType::PAKSC_SCREEN_SHOT_TIME_REPLY, SrvScreenShotTimeReply);
+    REGISTER_SEND_PACKET(ePacketType::PAKWC_SELECT_CHAR_REPLY, SrvSelectCharReply);
     REGISTER_SEND_PACKET(ePacketType::PAKLC_SRV_SELECT_REPLY, SrvSrvSelectReply);
     REGISTER_SEND_PACKET(ePacketType::PAKCC_SWITCH_SERVER, SrvSwitchServer);
 }
