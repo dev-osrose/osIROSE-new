a8a4e833e69b0a0eb5090a6f8b1d44c79f20d734
L3nn0x <dragon83.super@gmail.com>
2018-12-17 11:55:55 +0000

    Create nearby.h

---
+++ src/map/include/nearby.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "hash.h"
+#include "dataconsts.h"
+
+#include <unordered_map>
+#include <vector>
+#include <tuple>
+
+class EntitySystem;
+
+class Nearby {
+  public:
+    Nearby(const EntitySystem& entitySystem);
+    
+    void add_entity(RoseCommon::Entity entity);
+    void remove_entity(RoseCommon::Entity entity);
+    
+    bool is_nearby(RoseCommon::Entity first, RoseCommon::Entity second) const;
+    
+    std::vector<RoseCommon::Entity> get_nearby(RoseCommon::Entity entity) const;
+    
+    void update_position(RoseCommon::Entity entity);
+    
+  private:
+    using key_t = std::tuple<uint16_t, uint16_t>;
+    
+    std::unordered_map<key_t, std::vector<Entity>, Core::tuple_hash> grid;
+};
