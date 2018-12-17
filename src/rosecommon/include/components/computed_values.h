0529566ed4512f902cba0439e01c819c7c25a98b
L3nn0x <dragon83.super@gmail.com>
2018-12-17 10:25:09 +0000

    Update computed_values.h

--- src/rosecommon/include/components/computed_values.h
+++ src/rosecommon/include/components/computed_values.h
@@ -1,29 +1,10 @@
 #pragma once
 
 #include "dataconsts.h"
-#include <atomic>
 
 namespace Component {
 struct ComputedValues {
-    ComputedValues() = default;
-    ComputedValues(ComputedValues&&) = default;
-    ~ComputedValues() = default;
-    ComputedValues(const ComputedValues& other) : command(other.command), isOnMap(other.isOnMap.load()), moveMode(other.moveMode),
-                                                  runSpeed(other.runSpeed), atkSpeed(other.atkSpeed), weightRate(other.weightRate) {}
-
-    ComputedValues& operator=(ComputedValues&&) = default;
-    ComputedValues& operator=(const ComputedValues& other) {
-        command = other.command;
-        isOnMap.store(other.isOnMap.load());
-        moveMode = other.moveMode;
-        runSpeed = other.runSpeed;
-        atkSpeed = other.atkSpeed;
-        weightRate = other.weightRate;
-        return *this;
-    }
-
     RoseCommon::Command command;
-    mutable std::atomic_bool isOnMap;
     RoseCommon::MoveMode moveMode;
     uint16_t runSpeed;
     uint16_t atkSpeed;
