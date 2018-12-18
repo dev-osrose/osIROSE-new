b18bfebf08af0fd17888af2db4b8ec32892100a1
L3nn0x <dragon83.super@gmail.com>
2018-12-18 13:39:52 +0000

    Update normal_chat.cpp

--- src/map/src/chat/normal_chat.cpp
+++ src/map/src/chat/normal_chat.cpp
@@ -4,6 +4,7 @@
 #include "entity_system.h"
 #include "cli_normal_chat.h"
 #include "srv_normal_chat.h"
+#include "components/basic_info.h"
 
 using namespace RoseCommon;
 using namespace RoseCommon::Packet;
@@ -11,5 +12,8 @@ using namespace RoseCommon::Packet;
 void Chat::normal_chat(EntitySystem& entitySystem, Entity entity, const CliNormalChat& packet) {
     auto logger = Core::CLog::GetLogger(Core::log_type::GENERAL).lock();
     logger->trace("Chat::normal_chat()");
-    logger->debug("entity {}, message {}", entity, packet.get_message());
+    logger->trace("entity {}, message {}", entity, packet.get_message());
+    const auto& basicInfo = entitySystem->get_component<Component::BasicInfo>(entity);
+    auto p = SrvNormalChat::create(basicInfo.id, packet.get_message());
+    entitySystem->send_nearby(entity, p);
 }
