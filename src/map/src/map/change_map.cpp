ccb9e37457c832e00d9815e60f5685f23e7bccf3
L3nn0x <dragon83.super@gmail.com>
2019-01-15 11:15:26 +0000

    Update change_map.cpp

--- src/map/src/map/change_map.cpp
+++ src/map/src/map/change_map.cpp
@@ -2,11 +2,11 @@
 
 #include "logconsole.h"
 #include "entity_system.h"
-#include "cli_normal_chat.h"
-#include "srv_normal_chat.h"
 #include "cli_change_map_req.h"
+#include "srv_change_map_reply.h"
 #include "components/basic_info.h"
 #include "chat/whisper_chat.h"
+#include "cmapclient.h"
 
 using namespace RoseCommon;
 using namespace RoseCommon::Packet;
@@ -15,14 +15,19 @@ void Map::change_map_request(EntitySystem& entitySystem, Entity entity, const Cl
     auto logger = Core::CLog::GetLogger(Core::log_type::GENERAL).lock();
     logger->trace("Map::change_map_request");
     logger->trace("entity {}", entity);
-    entitySystem.send_to(entity, SrvChangeMapReply::create());
     const auto& basicInfo = entitySystem.get_component<Component::BasicInfo>(entity);
+    const auto& life = entitySystem.get_component<Component::Life>(entity);
+    const auto& mana = entitySystem.get_component<Component::Mana>(entity);
+    const auto& level = entitySystem.get_component<Component::Level>(entity);
+    entitySystem.send_to(entity, SrvChangeMapReply::create(
+        basicInfo.id, life.hp, mana.mp, level.xp, level.penalize_xp,
+        entitySystem.get_world_time(), basicInfo.teamId)); // FIXME: /!\ teamId is uint32_t but the packet expects a uint16_t /!\
     Chat::send_whisper(entitySystem, entity, fmt::format("You are client {}", basicInfo.id));
-    entitySystem.send_nearby_except_me(SrvPlayerChar::create());
+    entitySystem.send_nearby_except_me(CMapClient::create_srv_player_char(entitySystem, entity));
     const auto& nearby_entities = entitySystem.get_nearby(entity);
-    for (auto e : nearby_entities) {
-        if (e != entity) {
-        entitySystem.send_to(entity, SrvPlayerChar::create());
+    for (auto other : nearby_entities) {
+        if (other != entity) {
+        entitySystem.send_to(entity, CMapClient::create_srv_player_char(entitySystem, entity));
         }
     }
     if (const auto client_ptr = registry.try_get<const Component::Client>(entity)) {
