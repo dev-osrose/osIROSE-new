fc73d2e4afeae5eb56519f0a63e3088af29c766f
Raven <RavenX8@users.noreply.github.com>
2019-01-25 17:13:59 +0000

    seeing if flag will fix clang issue

--- cmake/platform/unix/compiler/Clang.cmake
+++ cmake/platform/unix/compiler/Clang.cmake
@@ -1,14 +1,13 @@
 message("-- using clang")
 
 add_compiler_flags(-std=c++17)
+add_compiler_flags(-frelaxed-template-template-args)
 add_compiler_flags(-Wno-keyword-compat -Wno-unused-command-line-argument)
 
-if( ${CMAKE_CXX_COMPILER_ID} STREQUAL Clang )
-  if(APPLE)
-    add_compiler_flags(-ftemplate-depth=900 -Wno-macro-redefined)
-  else()
-    add_compiler_flags(-ftemplate-depth=900 -Wno-expansion-to-defined)
-  endif()
+if(APPLE)
+  add_compiler_flags(-ftemplate-depth=900 -Wno-macro-redefined)
+else()
+  add_compiler_flags(-ftemplate-depth=900 -Wno-expansion-to-defined)
 endif()
 
 set(CLANG TRUE)
