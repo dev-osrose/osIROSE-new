a75ed2c2ff0f1d1ebcfc9258ee37d52ca2cec92d
L3nn0x <dragon83.super@gmail.com>
2019-01-29 14:09:05 +0000

    Update gm_commands.cpp

--- src/map/src/gm_commands.cpp
+++ src/map/src/gm_commands.cpp
@@ -96,11 +96,11 @@ void help(EntitySystem& entitySystem, RoseCommon::Entity entity, Parser<std::opt
     }
 }
 
-void execute_gm(EntitySystem& entitySystem, Entity entity, const std::string& message) {
+void execute_gm(EntitySystem& entitySystem, RoseCommon::Entity entity, const std::string& message) {
     if (message[0] != '/') {
         return;
     }
-    const uint16_t access_level = entitySystem.component<SocketConnector>(entity).access_level;
+    const uint16_t access_level = entitySystem.component<Component::SocketConnector>(entity).access_level;
     std::stringstream ss(message);
     std::string command;
     ss >> command;
