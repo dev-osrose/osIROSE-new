e0c5c352a054b6629b400bff53370eb7db1b0376
L3nn0x <dragon83.super@gmail.com>
2019-01-29 14:52:46 +0000

    Update gm_commands.cpp

--- src/map/src/gm_commands.cpp
+++ src/map/src/gm_commands.cpp
@@ -116,12 +116,12 @@ void execute_gm(EntitySystem& entitySystem, RoseCommon::Entity entity, const std
     std::stringstream ss(message);
     std::string command;
     ss >> command;
+    if (const auto it = aliases.find(command); it != aliases.cend()) {
+        command = it->second;
+    }
     if (const auto it = commands.find(command); it != commands.end() && access_level >= std::get<0>(it->second)) {
         std::get<1>(it->second)(entitySystem, entity, std::move(ss));
-    } else if (const auto it = aliases.find(command); it != aliases.end()) {
-        const auto jt = commands.find(it->second);
-        std::get<1>(jt->second)(entitySystem, entity, std::move(ss));
-    } else 
+    } else {
         Chat::send_whisper(entitySystem, entity, "Error, no known command/not enough permissions");
     }
 }
