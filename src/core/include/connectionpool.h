9df0cec13deeff3229024e5f800ca77f03f67a81
L3nn0x <dragon83.super@gmail.com>
2019-01-29 11:18:52 +0000

    Update connectionpool.h

--- src/core/include/connectionpool.h
+++ src/core/include/connectionpool.h
@@ -8,71 +8,87 @@
 #include <unordered_map>
 
 #include "singleton.h"
-#include "throwassert.h"
+#include "tuple_index.h"
 
 namespace Core {
 
-template <typename T>
-class ConnectionPool : public Singleton<ConnectionPool<T>> {
+template <typename... Connectors>
+class ConnectionPool : public Core::Singleton<ConnectionPool<Connectors...>> {
     public:
+        template <typename C>
         class Wrapper {
             public:
-                Wrapper(T& conn, const std::string &name,
-                        ConnectionPool<T> *callback)
-                    : _conn(conn), _name(name), _callback(callback) {}
+                Wrapper(typename C::type& conn, ConnectionPool<Connectors...>* callback)
+                : conn(conn), callback(callback) {}
+
                 ~Wrapper() {
-                    _callback->unMark(_name, &_conn);
+                    callback->unMark<C>(&conn);
                 }
 
                 template <typename U>
                 auto operator()(const U& u) {
-                    return _conn(u);
+                    return conn(u);
                 }
 
-                T* operator->() {
-                    return &_conn;
+                typename C::type* operator->() {
+                    return &conn;
                 }
 
-                T& get() {
-                    return _conn;
+                typename C::type& get() {
+                    return conn;
                 }
 
             private:
-                T &_conn;
-                const std::string &_name;
-                ConnectionPool<T> *_callback;
+                typename C::type& conn;
+                ConnectionPool<Connectors...>* callback;
         };
 
-        template <typename U>
-        void addConnector(const std::string &name, const U& factory) {
-            const std::function<std::unique_ptr<T>()> tmp = static_cast<decltype(tmp)>(factory);
-            _factories[name] = tmp;
+        template <typename C, typename T>
+        void addConnector(const T& factory) {
+            constexpr size_t index = Core::Index<C, std::tuple<Connectors...>>::value;
+            std::get<index>(connectors) = factory;
         }
 
-        Wrapper getConnection(const std::string &name) {
-            throw_assert(_factories.count(name), name << " cannot be constructed, no factory available");
-            for (auto &conn : _connections[name])
+        template <typename C>
+        Wrapper<C> getConnection() {
+            constexpr size_t index = Core::Index<C, std::tuple<Connectors...>>::value;
+            auto& connection = std::get<index>(connections);
+            for (auto& conn : connection) {
                 if (!conn.first) {
                     conn.first = true;
-                    return Wrapper(*conn.second.get(), name, this);
+                    return Wrapper<C>{*conn.second.get(), this};
                 }
-            _connections[name].emplace_back(std::make_pair(true, _factories[name]()));
-            return Wrapper(*_connections[name].back().second.get(), name, this);
+            }
+            auto& factory = std::get<index>(connectors);
+            connection.emplace_back(std::make_pair(true, factory()));
+            return Wrapper<C>{*connection.back().second.get(), this};
         }
 
     protected:
-        friend Wrapper;
-        void unMark(const std::string &name, T* value) {
-            for (auto &conn : _connections[name])
+        template <typename T>
+        friend class Wrapper;
+
+        template <typename C>
+        void unMark(typename C::type* value) {
+            constexpr size_t index = Index<C, std::tuple<Connectors...>>::value;
+            auto& connection = std::get<index>(connections);
+            for (auto &conn : connection) {
                 if (conn.first == true && conn.second.get() == value) {
                     conn.first = false;
                     break;
                 }
+            }
         }
 
     private:
-        std::unordered_map<std::string, std::function<std::unique_ptr<T>()>> _factories;
-        // false -> available
-        std::unordered_map<std::string, std::vector<std::pair<bool, std::unique_ptr<T>>>> _connections;
+
+        template <typename T>
+        using function_type = std::function<std::unique_ptr<typename T::type>()>;
+
+        template <typename T>
+        using connection_type = std::pair<bool, std::unique_ptr<typename T::type>>;
+
+        std::tuple<function_type<Connectors>...> connectors;
+        std::tuple<std::vector<connection_type<Connectors>>...> connections;
 };
 }
