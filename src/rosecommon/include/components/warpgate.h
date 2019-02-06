adecf19177adb04880e39646fab0c146be15352b
L3nn0x <dragon83.super@gmail.com>
2019-02-06 10:28:33 +0000

    Update warpgate.h

--- src/rosecommon/include/components/warpgate.h
+++ src/rosecommon/include/components/warpgate.h
@@ -2,13 +2,6 @@
 
 namespace Component {
 struct Warpgate {
-    static constexpr const float model_min_x = -1.75;
-    static constexpr const float model_min_y = -1.75;
-    static constexpr const float model_min_z = -0.004103546;
-    static constexpr const float model_max_x = 1.75;
-    static constexpr const float model_max_y = 1.75;
-    static constexpr const float model_max_z = 3.495897;
-
     float min_x;
     float min_y;
     float min_z;
