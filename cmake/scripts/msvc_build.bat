f9ad7a4d2a216103f90bd57477ecc36da365ed8c
Christopher Torres <RavenX8@users.noreply.github.com>
2019-02-06 23:10:29 -0500

    another change for appveyor

--- cmake/scripts/msvc_build.bat
+++ cmake/scripts/msvc_build.bat
@@ -1,5 +1,5 @@
 REM @call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-SET PATH=%PROJECT_PATH%\3rdparty\bin;%MYSQL_CONNECTOR_PATH%\lib;%MYSQL_PATH%\bin;%PYTHON_PATH%\Scripts;C:\OpenSSL-v111-Win64\bin;C:\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev1\mingw64\bin;%PATH%
+SET PATH=%PROJECT_PATH%\3rdparty\bin;%MYSQL_CONNECTOR_PATH%\lib;%MYSQL_PATH%\bin;%PYTHON_PATH%\Scripts;C:\OpenSSL-v111-Win64\bin;C:\Libraries\boost_1_67_0;C:\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev1\mingw64\bin;
 
 cp "%MYSQL_CONNECTOR_PATH%\lib\libmysql.dll" "%PROJECT_PATH%\build\bin\"
 cp "C:\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev1\mingw64\opt\bin\*.dll" "%PROJECT_PATH%\build\bin\"
