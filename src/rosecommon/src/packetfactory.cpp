64666db7cf487580a2ec191e6c4a9ec98a449daf
Raven <RavenX8@users.noreply.github.com>
2019-03-05 19:04:20 +0000

    Register for new messages

--- src/rosecommon/src/packetfactory.cpp
+++ src/rosecommon/src/packetfactory.cpp
@@ -4,15 +4,18 @@
 
 #include "cli_accept_req.h"
 #include "cli_alive.h"
+#include "cli_attack.h"
 #include "cli_change_map_req.h"
 #include "cli_channel_list_req.h"
 #include "cli_char_list_req.h"
 #include "cli_create_char_req.h"
 #include "cli_delete_char_req.h"
+#include "cli_hp_req.h"
 #include "cli_join_server_req.h"
 #include "cli_login_req.h"
 #include "cli_mouse_cmd.h"
 #include "cli_normal_chat.h"
+#include "cli_revive_req.h"
 #include "cli_select_char_req.h"
 #include "cli_srv_select_req.h"
 #include "cli_stop_moving.h"
@@ -21,12 +24,14 @@
 #include "isc_server_register.h"
 #include "isc_shutdown.h"
 #include "srv_accept_reply.h"
+#include "srv_attack.h"
 #include "srv_billing_message.h"
 #include "srv_change_map_reply.h"
 #include "srv_channel_list_reply.h"
 #include "srv_char_list_reply.h"
 #include "srv_create_char_reply.h"
 #include "srv_delete_char_reply.h"
+#include "srv_hp_reply.h"
 #include "srv_inventory_data.h"
 #include "srv_join_server_reply.h"
 #include "srv_login_reply.h"
@@ -54,14 +59,17 @@ using namespace RoseCommon::Packet;
 void RoseCommon::register_recv_packets() {
     REGISTER_RECV_PACKET(ePacketType::PAKCS_ACCEPT_REQ, CliAcceptReq);
     REGISTER_RECV_PACKET(ePacketType::PAKCS_ALIVE, CliAlive);
+    REGISTER_RECV_PACKET(ePacketType::PAKCS_ATTACK, CliAttack);
     REGISTER_RECV_PACKET(ePacketType::PAKCS_CHANGE_MAP_REQ, CliChangeMapReq);
     REGISTER_RECV_PACKET(ePacketType::PAKCS_CHANNEL_LIST_REQ, CliChannelListReq);
     REGISTER_RECV_PACKET(ePacketType::PAKCS_CHAR_LIST_REQ, CliCharListReq);
     REGISTER_RECV_PACKET(ePacketType::PAKCS_CREATE_CHAR_REQ, CliCreateCharReq);
     REGISTER_RECV_PACKET(ePacketType::PAKCS_DELETE_CHAR_REQ, CliDeleteCharReq);
+    REGISTER_RECV_PACKET(ePacketType::PAKCS_HP_REQ, CliHpReq);
     REGISTER_RECV_PACKET(ePacketType::PAKCS_LOGIN_REQ, CliLoginReq);
     REGISTER_RECV_PACKET(ePacketType::PAKCS_MOUSE_CMD, CliMouseCmd);
     REGISTER_RECV_PACKET(ePacketType::PAKCS_NORMAL_CHAT, CliNormalChat);
+    REGISTER_RECV_PACKET(ePacketType::PAKCS_REVIVE_REQ, CliReviveReq);
     REGISTER_RECV_PACKET(ePacketType::PAKCS_SELECT_CHAR_REQ, CliSelectCharReq);
     REGISTER_RECV_PACKET(ePacketType::PAKCS_SRV_SELECT_REQ, CliSrvSelectReq);
     REGISTER_RECV_PACKET(ePacketType::PAKCS_STOP_MOVING, CliStopMoving);
@@ -76,11 +84,13 @@ void RoseCommon::register_send_packets() {
     REGISTER_SEND_PACKET(ePacketType::ISC_SERVER_REGISTER, IscServerRegister);
     REGISTER_SEND_PACKET(ePacketType::ISC_SHUTDOWN, IscShutdown);
     REGISTER_SEND_PACKET(ePacketType::PAKSS_ACCEPT_REPLY, SrvAcceptReply);
+    REGISTER_SEND_PACKET(ePacketType::PAKWC_ATTACK, SrvAttack);
     REGISTER_SEND_PACKET(ePacketType::PAKLC_CHANNEL_LIST_REPLY, SrvChannelListReply);
     REGISTER_SEND_PACKET(ePacketType::PAKWC_CHANGE_MAP_REPLY, SrvChangeMapReply);
     REGISTER_SEND_PACKET(ePacketType::PAKCC_CHAR_LIST_REPLY, SrvCharListReply);
     REGISTER_SEND_PACKET(ePacketType::PAKCC_CREATE_CHAR_REPLY, SrvCreateCharReply);
     REGISTER_SEND_PACKET(ePacketType::PAKCC_DELETE_CHAR_REPLY, SrvDeleteCharReply);
+    REGISTER_SEND_PACKET(ePacketType::PAKWC_HP_REPLY, SrvHpReply);
     REGISTER_SEND_PACKET(ePacketType::PAKWC_INVENTORY_DATA, SrvInventoryData);
     REGISTER_SEND_PACKET(ePacketType::PAKSC_JOIN_SERVER_REPLY, SrvJoinServerReply);
     REGISTER_SEND_PACKET(ePacketType::PAKLC_LOGIN_REPLY, SrvLoginReply);
