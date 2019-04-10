e4752c5ac194551c882df088a4c59c4274792ea1
Ralph Minderhoud <ralphminderhoud@gmail.com>
2019-04-10 10:57:24 +0500

    Fix appveyor msys build

--- cmake/scripts/msys_build.bat
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PATH=%PROJECT_PATH%\3rdparty\bin;%MYSQL_CONNECTOR_PATH%\lib;%MYSQL_PATH%\bin;%PYTHON_PATH%\Scripts;C:\Libraries\boost_1_67_0;%PATH%
-
-cp "%MYSQL_CONNECTOR_PATH%\lib\libmysql.dll" "%PROJECT_PATH%\build\bin\"
-
-cmake -G"MSYS Makefiles" ..
-cmake --build .
