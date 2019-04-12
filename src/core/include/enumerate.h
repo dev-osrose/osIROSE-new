0eebe039173d753c89cc565462e67103eaae67fa
L3nn0x <dragon83.super@gmail.com>
2019-04-12 23:39:36 +0100

    Solve msvc compilation error

--- src/core/include/enumerate.h
+++ src/core/include/enumerate.h
@@ -4,28 +4,34 @@
 
 namespace Core {
 namespace {
-using std::begin;
-using std::end;
+template <typename TIter>
+struct iterator
+{
+    size_t i;
+    TIter iter;
+    bool operator != (const iterator & other) const { return iter != other.iter; }
+    void operator ++ () { ++i; ++iter; }
+    auto operator * () const { return std::tie(i, *iter); }
+};
+
+template <typename T, typename TIter>
+struct iterable_wrapper
+{
+    iterable_wrapper(T&& iterable) : iterable(std::forward<T>(iterable)) {}
+    T iterable;
+};
+
+template <typename T, typename TIter>
+constexpr auto begin(iterable_wrapper<T, TIter>& it) { return iterator<TIter>{ 0, begin(it.iterable) }; }
+template <typename T, typename TIter>
+constexpr auto end(iterable_wrapper<T, TIter>& it) { return iterator<TIter>{ 0, end(it.iterable) }; }
 }
+
 template <typename T,
           typename TIter = decltype(begin(std::declval<T>())),
           typename = decltype(end(std::declval<T>()))>
 constexpr auto enumerate(T && iterable)
 {
-    struct iterator
-    {
-        size_t i;
-        TIter iter;
-        bool operator != (const iterator & other) const { return iter != other.iter; }
-        void operator ++ () { ++i; ++iter; }
-        auto operator * () const { return std::tie(i, *iter); }
-    };
-    struct iterable_wrapper
-    {
-        T iterable;
-        auto begin() { using Core::begin; return iterator{ 0, begin(iterable) }; }
-        auto end() { using Core::end; return iterator{ 0, end(iterable) }; }
-    };
-    return iterable_wrapper{ std::forward<T>(iterable) };
+    return iterable_wrapper<T, TIter>{ std::forward<T>(iterable) };
 }
 }
