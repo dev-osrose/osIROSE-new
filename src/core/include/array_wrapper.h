e4c9efe23a0c8f68724251d383935db64cb69cd2
L3nn0x <dragon83.super@gmail.com>
2019-04-17 12:18:02 +0100

    Update array_wrapper.h

--- src/core/include/array_wrapper.h
+++ src/core/include/array_wrapper.h
@@ -5,14 +5,6 @@ template <typename T, size_t N, size_t L>
 struct array_wrapper {
     T& iterable;
 
-    constexpr auto operator[](size_t index) const {
-        return iterable[index];
-    }
-
-    constexpr auto& operator[](size_t index) {
-        return iterable[index];
-    }
-
     static constexpr size_t size() noexcept {
         return L;
     }
