b03c7d545fd640acf4f11404c4089d2768580cf7
L3nn0x <dragon83.super@gmail.com>
2019-04-18 10:36:47 +0100

    Update inventory.h

--- src/map/include/items/inventory.h
+++ src/map/include/items/inventory.h
@@ -27,9 +27,8 @@ ReturnValue equip_item(EntitySystem&, RoseCommon::Entity, size_t from, size_t to
 ReturnValue unequip_item(EntitySystem&, RoseCommon::Entity, size_t from);
 void drop_item(EntitySystem&, RoseCommon::Entity, float x, float y, RoseCommon::Entity owner = entt::null);
 
-void add_zuly(EntitySystem&, RoseCommon::Entity, int64_t zuly);
-// returns false if there isn't enough zuly in the inventory
-bool remove_zuly(EntitySystem&, RoseCommon::Entity, int64_t zuly);
+// you can add a negative amount (effectively removing zulies). Returns false if there isn't enough zuly in the inventory to remove
+bool add_zuly(EntitySystem&, RoseCommon::Entity, int64_t zuly);
 
 void equip_item_packet(EntitySystem&, RoseCommon::Entity, const RoseCommon::Packet::CliEquipItem&);
 void drop_item_packet(EntitySystem&, RoseCommon::Entity, const RoseCommon::Packet::CliDropItem&);
