e107c1323f6b1958c455c907f5515e723fd8d84f
L3nn0x <dragon83.super@gmail.com>
2019-04-23 13:50:51 +0100

    Update itemdb.cpp

--- src/rosecommon/src/itemdb.cpp
+++ src/rosecommon/src/itemdb.cpp
@@ -5,10 +5,9 @@
 
 using namespace RoseCommon;
 
-ItemDef::ItemDef(): type( 0 ),
+ItemDef::ItemDef(): type( ItemType::NONE ),
                     subtype( 0 ),
                     id( 0 ),
-                    item_type( ItemType::WEARABLE ),
                     buy_price( 0 ),
                     sell_price( 0 ),
                     weight( 0 ),
@@ -29,22 +28,13 @@ void ItemDatabase::initialize() {
   try {
     ItemDef newItem;
     for ( const auto& row : conn( sqlpp::select( sqlpp::all_of( itemdb ) ).from( itemdb ).where(itemdb.type != 0).order_by(itemdb.id.asc()) ) ) {
-      newItem.type = static_cast<uint8_t>(row.type);
+      newItem.type = static_cast<ItemType>(static_cast<uint8_t>(row.type));
       newItem.subtype = static_cast<uint8_t>(row.subtype);
       newItem.id = static_cast<uint16_t>(row.id);
-      if (newItem.type == 14) {
-          newItem.item_type = ItemType::RIDING;
-      } else if (newItem.type < EquippedPosition::MAX_EQUIP_ITEMS) {
-          newItem.item_type = ItemType::WEARABLE;
-      } else if (newItem.type == 10) {
-        newItem.item_type = ItemType::CONSUMABLE;
-      } else {
-        newItem.item_type = ItemType::ETC;
-      }
       newItem.buy_price = static_cast<uint32_t>(row.priceBuy);
       newItem.sell_price = static_cast<uint32_t>(row.priceSell);
       newItem.weight = static_cast<uint16_t>(row.weight);
-      newItem.is_stackable = newItem.item_type == ItemType::CONSUMABLE || newItem.item_type == ItemType::ETC;
+      newItem.is_stackable = newItem.type == ItemType::CONSUMABLE || newItem.type == ItemType::ETC || newItem.type == ItemType::ETC2;
       newItem.atk = static_cast<uint16_t>(row.attack);
       newItem.def = static_cast<uint16_t>(row.defense);
       newItem.slots = static_cast<uint8_t>(row.slots);
