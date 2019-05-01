17b30489630018e74e41b9fed8b834a7abe7b1e2
Raven <RavenX8@users.noreply.github.com>
2019-05-01 22:40:13 +0000

    turno on test mode

--- .travis-coverity-scan-build.sh
+++ .travis-coverity-scan-build.sh
@@ -17,44 +17,27 @@ TOOL_BASE=/tmp/coverity-scan-analysis
 UPLOAD_URL="https://scan.coverity.com/builds?project=dev-osrose%2FosIROSE-new"
 SCAN_URL="https://scan.coverity.com"
 
-# Verify Coverity Scan run condition
-#COVERITY_SCAN_RUN_CONDITION=${coverity_scan_run_condition:-true}
-#echo -ne "\033[33;1mTesting '${COVERITY_SCAN_RUN_CONDITION}' condition... "
-#if eval [ $COVERITY_SCAN_RUN_CONDITION ]; then
-#  echo -e "True.\033[0m"
-#else
-#  echo -e "False. Exit.\033[0m"
-#  exit 1
-#fi
-
 # Do not run on pull requests
 if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
   echo -e "\033[33;1mINFO: Skipping Coverity Analysis: branch is a pull request.\033[0m"
   exit 0
 fi
 
-# Verify this branch should run
-#IS_COVERITY_SCAN_BRANCH=`ruby -e "puts '${TRAVIS_BRANCH}' =~ /\\A$COVERITY_SCAN_BRANCH_PATTERN\\z/ ? 1 : 0"`
-#if [ "$IS_COVERITY_SCAN_BRANCH" = "1" ]; then
-#  echo -e "\033[33;1mCoverity Scan configured to run on branch ${TRAVIS_BRANCH}\033[0m"
-#else
-#  echo -e "\033[33;1mCoverity Scan NOT configured to run on branch ${TRAVIS_BRANCH}\033[0m"
-#  exit 1
-#fi
-
-# Verify upload is permitted
-AUTH_RES=`curl -s --form project="$COVERITY_SCAN_PROJECT_NAME" --form token="$COVERITY_SCAN_TOKEN" $SCAN_URL/api/upload_permitted`
-if [ "$AUTH_RES" = "Access denied" ]; then
-  echo -e "\033[33;1mCoverity Scan API access denied. Check COVERITY_SCAN_PROJECT_NAME and COVERITY_SCAN_TOKEN.\033[0m"
-  exit 1
-else
-  AUTH=`echo $AUTH_RES | ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['upload_permitted']"`
-  if [ "$AUTH" = "true" ]; then
-    echo -e "\033[33;1mCoverity Scan analysis authorized per quota.\033[0m"
-  else
-    WHEN=`echo $AUTH_RES | ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['next_upload_permitted_at']"`
-    echo -e "\033[33;1mCoverity Scan analysis NOT authorized until $WHEN.\033[0m"
+if [ "${COVERITY_SCAN_SCRIPT_TEST_MODE}" = false ]; then
+  # Verify upload is permitted
+  AUTH_RES=`curl -s --form project="$COVERITY_SCAN_PROJECT_NAME" --form token="$COVERITY_SCAN_TOKEN" $SCAN_URL/api/upload_permitted`
+  if [ "$AUTH_RES" = "Access denied" ]; then
+    echo -e "\033[33;1mCoverity Scan API access denied. Check COVERITY_SCAN_PROJECT_NAME and COVERITY_SCAN_TOKEN.\033[0m"
     exit 1
+  else
+    AUTH=`echo $AUTH_RES | ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['upload_permitted']"`
+    if [ "$AUTH" = "true" ]; then
+      echo -e "\033[33;1mCoverity Scan analysis authorized per quota.\033[0m"
+    else
+      WHEN=`echo $AUTH_RES | ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['next_upload_permitted_at']"`
+      echo -e "\033[33;1mCoverity Scan analysis NOT authorized until $WHEN.\033[0m"
+      exit 1
+    fi
   fi
 fi
 
@@ -97,7 +80,7 @@ SHA=`git rev-parse --short HEAD`
 #VERSION_SHA=$(cat VERSION)#$SHA
 
 # Verify Coverity Scan script test mode
-if [ "$coverity_scan_script_test_mode" = true ]; then
+if [ "${COVERITY_SCAN_SCRIPT_TEST_MODE}" = true ]; then
   echo -e "\033[33;1mCoverity Scan configured in script test mode. Exit.\033[0m"
   exit 1
 fi
