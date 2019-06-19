cec38110437403a3349739ffd8f0ffe707b50162
L3nn0x <dragon83.super@gmail.com>
2019-06-19 10:34:49 +0100

    Update crosepacket.h

--- src/rosecommon/include/crosepacket.h
+++ src/rosecommon/include/crosepacket.h
@@ -137,7 +137,7 @@ class CRosePacket {
 
     protected:
         virtual bool pack(CRoseBasePolicy& writer) const = 0;
-        void reset_size() { size_ = 0; }
+        void reset_size() { }
     
     private:
         uint16_t size_;
