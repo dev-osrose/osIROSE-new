09c3c1dae85833be70cf2a02bf598eef9a7a490c
L3nn0x <dragon83.super@gmail.com>
2019-07-17 11:21:01 +0100

    Update party_base.h

--- src/rosecommon/include/party_base.h
+++ src/rosecommon/include/party_base.h
@@ -8,21 +8,21 @@ struct PartyBase {
     PartyBase(RoseCommon::Entity leader);
 
     bool add_member(RoseCommon::Entity);
-    bool remove_member(RoseCommon::Entity);
+    bool remove_member(uint32_t tag);
 
-    bool is_member(RoseCommon::Entity) const;
+    bool is_member(uint32_t tag) const;
 
-    bool change_leader(RoseCommon::Entity);
+    bool change_leader(uint32_t tag);
 
     uint32_t xp = 0;
     int range = 700;
     uint8_t level = 1;
     uint8_t options = 0;
-    RoseCommon::Entity leader = entt::null;
+    uint32_t leader = 0;
     RoseCommon::Entity last_item = entt::null;
     RoseCommon::Entity last_etc = entt::null;
     RoseCommon::Entity last_zuly = entt::null;
     uint8_t capacity = 5;
-    std::vector<RoseCommon::Entity> members;
+    std::vector<uint32_t> members;
 };
 }
