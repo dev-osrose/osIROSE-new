a3ceb7472828844645a26f8c592607507110fe32
L3nn0x <dragon83.super@gmail.com>
2019-07-29 15:53:18 +0100

    Update lua_item.cpp

--- src/map/src/lua_item.cpp
+++ src/map/src/lua_item.cpp
@@ -6,7 +6,7 @@ void LuaItem::register_lua(sol::environment env, uint8_t type, uint16_t id,
     Data d;
     d.api = std::make_shared<ItemLuaApi>(env);
     d.api->register_add_bonus_attr(std::forward<decltype(addBonusAttr)>(addBonusAttr));
-    d.api->register_remove_bonus_attr(std::foward<decltype(removeBonusAttr)>(removeBonusAttr));
+    d.api->register_remove_bonus_attr(std::forward<decltype(removeBonusAttr)>(removeBonusAttr));
     d.env = env;
     data.emplace(std::tuple{type, id}, d);
 }
