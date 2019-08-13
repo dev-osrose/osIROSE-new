4a77d0478825143270e81a02607a39fa9689be7a
L3nn0x <dragon83.super@gmail.com>
2019-08-13 19:44:54 +0100

    Update getters.h

--- src/map/include/utils/getters.h
+++ src/map/include/utils/getters.h
@@ -3,7 +3,7 @@
 #include "entity_system.h"
 
 namespace Utils {
-int  get_attack_speed(EntitySystem& entitySystem, RoseCommon::Entity entity);
-int  get_move_speed(EntitySystem& entitySystem, RoseCommon::Entity entity);
-int  get_magic(EntitySystem& entitySystem, RoseCommon::Entity entity);
+int  get_attack_speed(EntitySystem& entitySystem, RoseCommon::Entity item);
+int  get_move_speed(EntitySystem& entitySystem, RoseCommon::Entity item);
+int  get_magic(EntitySystem& entitySystem, RoseCommon::Entity item);
 }
