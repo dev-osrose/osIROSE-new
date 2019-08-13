437374fa833ede919d857d4d1f2f6ff85fdd6feb
L3nn0x <dragon83.super@gmail.com>
2019-08-13 10:19:49 +0100

    stoof

--- src/map/src/utils/getters.cpp
+++ src/map/src/utils/getters.cpp
@@ -4,7 +4,7 @@ int  Utils::get_attack_speed(EntitySystem& entitySystem, RoseCommon::Entity enti
   auto logger = Core::CLog::GetLogger(Core::log_type::GENERAL).lock();
   if(entity) {
     if(auto* lua = entitySystem.try_get_component<Component::ItemLua>(entity); lua) {
-      if(auto api = lua->api.lock(); api)
+      if(auto api = lua->api.lock(); api) {
         return api->get_attack_speed();
       } else {
         logger->warn("Utils::get_attack_speed(): No lua api available for {}", entity);
@@ -20,7 +20,7 @@ int  Utils::get_move_speed(EntitySystem& entitySystem, RoseCommon::Entity entity
   auto logger = Core::CLog::GetLogger(Core::log_type::GENERAL).lock();
   if(entity) {
     if(auto* lua = entitySystem.try_get_component<Component::ItemLua>(entity); lua) {
-      if(auto api = lua->api.lock(); api)
+      if(auto api = lua->api.lock(); api) {
         return api->get_move_speed();
       } else {
         logger->warn("Utils::get_move_speed(): No lua api available for {}", entity);
@@ -36,7 +36,7 @@ int  Utils::get_magic(EntitySystem& entitySystem, RoseCommon::Entity entity) {
   auto logger = Core::CLog::GetLogger(Core::log_type::GENERAL).lock();
   if(entity) {
     if(auto* lua = entitySystem.try_get_component<Component::ItemLua>(entity); lua) {
-      if(auto api = lua->api.lock(); api)
+      if(auto api = lua->api.lock(); api) {
         return api->get_magic();
       } else {
         logger->warn("Utils::get_magic(): No lua api available for {}", entity);
