3b38f801cf83b3eb6397fe78708ce456afa7ae28
Raven <7156279+RavenX8@users.noreply.github.com>
2019-08-14 15:17:17 +0000

    Fix VS path issue on appveyor

--- cmake/scripts/msvc_build.bat
+++ cmake/scripts/msvc_build.bat
@@ -1,4 +1,4 @@
-@call "C:\Program Files\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 SET PATH=%PROJECT_PATH%\3rdparty\bin;%MYSQL_CONNECTOR_PATH%\lib;%MYSQL_PATH%\bin;%PYTHON_PATH%\Scripts;C:\OpenSSL-v111-Win64\bin;C:\Libraries\boost_1_67_0;%PATH%
 
 cp "%MYSQL_CONNECTOR_PATH%\lib\libmysql.dll" "%PROJECT_PATH%\build\bin\"
