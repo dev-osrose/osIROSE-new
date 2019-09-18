3d658cb356547207cc2d9a84276099377d89577f
L3nn0x <dragon83.super@gmail.com>
2019-09-18 15:59:09 +0100

    Update party.h

--- src/char/include/party.h
+++ src/char/include/party.h
@@ -2,6 +2,7 @@
 
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include "connection.h"
 
 struct Party {
@@ -17,41 +18,47 @@ struct Party {
   std::vector<uint32_t> members;
 };
 
-std::shared_ptr<Party> cache_fetch_party(uint32_t charId) {
-  auto conn = Core::connectionPool.getConnection<Core::Osirose>();
-  Core::Party partyTable{};
-  Core::PartyMembers partyMembersTable{};
-  
-  auto res = conn(sqlpp::select(sqlpp::all_of(partyTable)).from(
-    partyMembersTable.join(partyTable).on(partyMembersTable.id == partyTable.id))
-                  .where(partyMembersTable.memberId == charId));
-  
-  if (res.empty()) {
-    return {};
-  }
-  auto result = res.front();
-  
-  Party party;
-  party.id = result.id;
-  party.name = result.name;
-  party.leader = result.leaderId;
-  party.options = result.options;
-  party.level = result.level;
-  party.last_got_item_index = result.lastGotItemIndex;
-  party.last_got_zuly_index = result.lastGotZulyIndex;
-  party.last_got_etc_index = result.lastGotEtcIndex;
-  
-  // now we fetch all party members
-  auto res2 = conn(sqlpp::select(partyMembersTable.memberId).from(partyMembersTable)
-                   .where(partyMembersTable.id == party.id).order_by(partyMembersTable.rank.desc()));
-  
-  if (res.empty()) {
-    return {};
-  }
-  
-  for (auto& r : res2) {
-    party.members.push_back(r.memberId);
-  }
+std::shared_ptr<Party> cache_fetch_party(uint32_t charId);
+void cache_write_party(std::shared_ptr<Party> party);
+void cache_write_party_members(std::shared_ptr<Party> party);
+void cache_remove_party(std::shared_ptr<Party> party);
 
-  return std::make_shared<Party>(party);
-}
+class PartyCache {
+  public:
+    std::shared_ptr<Party> get_party(uint32_t charId) {
+      if (cache.contains(charId)) {
+        return cache.at(charId);
+      }
+      auto [it, inserted] = cache.insert({charId, cache_fetch_party(charId)});
+      
+      if (!inserted) {
+        return *it;
+      }
+      const auto& party = *it;
+      for (auto m : party->members) {
+        cache.insert({m, party});
+      }
+      return party;
+    }
+  
+  void add_member_to_party(std::shared_ptr<Party> party, uint32_t member) {
+    party->members.push_back(member);
+    cache.insert({member, party});
+    
+    cache_write_party_members(party);
+  }
+  
+  void remove_member_from_party(std::shared_ptr<Party>, uint32_t member) {
+    party->members.erase(std::remove(party->members.begin(), party->members.end(), member), party->members.end());
+    cache.erase(member);
+    
+    if (party->members.empty()) {
+      cache_remove_party(party);
+    } else {
+      cache_write_party_members(party);
+    }
+  }
+  
+  private:
+    std::unordered_map<uint32_t, std::shared_ptr<Party>> cache;
+};
