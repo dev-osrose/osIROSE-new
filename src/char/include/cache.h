b7a2b50f6b0ee0023ddf967a484b7b11e0f3078b
L3nn0x <dragon83.super@gmail.com>
2019-09-18 15:13:15 +0100

    Create cache.h

---
+++ src/char/include/cache.h
@@ -0,0 +1,27 @@
+#pragma once
+
+// a basic cache that calls a callback<V(const K&)> with the key when not present. K needs to implement std::hash<K>
+// cache invalidation is manual
+
+#include <unordered_map>
+#include <functional>
+
+template <typename K, typename V>
+class Cache {
+  public:
+    Cache(std::function<V(const K&)> callback) : callback(callback) {}
+    
+    const V& get_value(const K& k) {
+      if (cache.contains(k)) {
+        return cache.at(k);
+      }
+      return *cache.insert({k, callback(k)}).first;
+    }
+    
+    void delete_value(const K& k) {
+      cache.erase(k);
+    }
+
+  private:
+    std::unordered_map<K, V> cache;
+};
