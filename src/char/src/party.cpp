5702d4a2fbf1fe4f98bcfbfb1329199a09357248
L3nn0x <dragon83.super@gmail.com>
2019-09-18 16:51:32 +0100

    Update party.cpp

--- src/char/src/party.cpp
+++ src/char/src/party.cpp
@@ -1,7 +1,7 @@
 #include "party.h"
 
 std::shared_ptr<Party> cache_fetch_party(uint32_t charId) {
-auto conn = Core::connectionPool.getConnection<Core::Osirose>();
+  auto conn = Core::connectionPool.getConnection<Core::Osirose>();
   Core::Party partyTable{};
   Core::PartyMembers partyMembersTable{};
   
@@ -32,18 +32,62 @@ auto conn = Core::connectionPool.getConnection<Core::Osirose>();
     return {};
   }
   
-  for (auto& r : res2) {
+  for (const auto& r : res2) {
     party.members.push_back(r.memberId);
   }
 
-  return std::make_shared<Party>(party);
+  return {party};
+}
+
+void cache_create_party(std::shared_ptr<Party> party) {
+  if (!party) return;
+  
+  auto conn = Core::connectionPool.getConnection<Core::Osirose>();
+  Core::Party partyTable{};
+  
+  conn(sqlpp::insert_into(partyTable)
+       .set(partyTable.name = party->name)
+       .set(partyTable.leaderId = party->leader)
+       .set(partyTable.options = party->options)
+       .set(partyTable.level = party->level)
+       .set(partyTable.lastGotItemIndex = party->last_got_item_index)
+       .set(partyTable.lastGotEtcIndex = party->last_got_etc_index)
+       .set(partyTable.lastGotZulyIndex = party->last_got_zuly_index));
 }
 
 void cache_write_party(std::shared_ptr<Party> party) {
+  if (!party) return;
+  auto conn = Core::connectionPool.getConnection<Core::Osirose>();
+  Core::Party partyTable{};
+  
+  conn(sqlpp::update(partyTable)
+       .set(partyTable.name = party->name)
+       .set(partyTable.leaderId = party->leader)
+       .set(partyTable.options = party->options)
+       .set(partyTable.level = party->level)
+       .set(partyTable.lastGotItemIndex = party->last_got_item_index)
+       .set(partyTable.lastGotEtcIndex = party->last_got_etc_index)
+       .set(partyTable.lastGotZulyIndex = party->last_got_zuly_index)
+       .where(partyTable.id == party->id));
 }
 
 void cache_write_party_members(std::shared_ptr<Party> party) {
+  if (!party) return;
+  auto conn = Core::connectionPool.getConnection<Core::Osirose>();
+  Core::PartyMembers partyMembersTable{};
+
+  conn(sqlpp::remove_from(partyMembersTable).where(partyMembersTable.id == party->id));
+  auto insert = sqlpp::insert_into(partyMembersTable).columns(partyMembersTable.id, partyMembersTable.memberId);
+  for (auto m : party-> members) {
+    multi_insert.values.add(partyMembersTable.id = party->id, partyMembersTable.memberId = m);
+  }
+  conn(insert);
 }
 
 void cache_remove_party(std::shared_ptr<Party> party) {
+  if (!party) return;
+  auto conn = Core::connectionPool.getConnection<Core::Osirose>();
+  Core::Party partyTable{};
+  
+  conn(sqlpp::remove_from(partyTable).where(partyTable.id == party->id));
 }
