2d8a1ce8a86ba0ed716bbc6148724e4c6038c848
L3nn0x <dragon83.super@gmail.com>
2019-09-23 11:45:18 +0100

    Update change_map.cpp

--- src/map/src/map/change_map.cpp
+++ src/map/src/map/change_map.cpp
@@ -47,7 +47,7 @@ void Map::change_map_request(EntitySystem& entitySystem, Entity entity, const Cl
             entitySystem.send_to_entity(entity, other);
         }
     }
-    entitySystem.send_to_maps(IscClientStatus::create(basicInfo.charId, IscClientStatus::CONNECTED), {0});
+    entitySystem.send_to_maps(IscClientStatus::create(basicInfo.charId, IscClientStatus::CONNECTED), CHARSERVER);
 }
 
 void Map::teleport_request(EntitySystem& entitySystem, RoseCommon::Entity entity, const RoseCommon::Packet::CliTeleportReq& packet) {
