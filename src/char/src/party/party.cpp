27eb79fcf7a6939b23da1bc1e617c62bd2ad81aa
L3nn0x <dragon83.super@gmail.com>
2019-09-23 18:28:24 +0100

    Delete party.cpp

--- src/char/src/party/party.cpp
+++
@@ -1,7 +0,0 @@
-#include "party/party.h"
-#include "ccharserver.h"
-
-namespace Party {
-void party_request(const RoseCommon::Party::CliPartyReq& packet, CCharServer& server, uint32_t charId) {
-}
-}
