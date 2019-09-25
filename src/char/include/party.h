c452d639f3a6abb7c7783afd17879b7725a82c7a
L3nn0x <dragon83.super@gmail.com>
2019-09-25 11:19:12 +0100

    Update party.h

--- src/char/include/party.h
+++ src/char/include/party.h
@@ -36,57 +36,11 @@ void cache_remove_party(std::shared_ptr<Party> party);
 
 class PartyCache {
   public:
-    std::shared_ptr<Party> get_party(uint32_t charId) {
-      if (cache.count(charId) != 0) {
-        return cache.at(charId);
-      }
-      auto party = cache_fetch_party(charId);
-      if (!party) { // no party!
-        return {};
-      }
-      cache.insert({charId, party});
-
-      for (auto m : party->members) {
-        cache.insert({m, party});
-      }
-      return party;
-    }
-  
-    std::shared_ptr<Party> create_party(uint32_t charId) {
-      auto party = std::make_shared<Party>();
-      party->members.push_back(charId);
-      party->leader = charId;
-      cache_create_party(party);
-      cache_write_party_members(party);
-      party = cache_fetch_party(charId);
-      cache[charId] = party;
-      return party;
-    }
-  
-    void add_member_to_party(std::shared_ptr<Party> party, uint32_t member) {
-      party->members.push_back(member);
-      cache.insert({member, party});
-    
-      cache_write_party_members(party);
-    }
-  
-    void remove_member_from_party(std::shared_ptr<Party> party, uint32_t member) {
-      party->members.erase(std::remove(party->members.begin(), party->members.end(), member), party->members.end());
-      cache.erase(member);
-    
-      if (party->members.empty()) {
-        cache_remove_party(party);
-      } else {
-        cache_write_party_members(party);
-      }
-    }
-
-    void remove_party(std::shared_ptr<Party> party) {
-        for (auto member : party->members) {
-            cache.erase(member);
-        }
-        cache_remove_party(party);
-    }
+    std::shared_ptr<Party> get_party(uint32_t charId);
+    std::shared_ptr<Party> create_party(uint32_t charId);
+    void add_member_to_party(std::shared_ptr<Party> party, uint32_t member);
+    void remove_member_from_party(std::shared_ptr<Party> party, uint32_t member);
+    void remove_party(std::shared_ptr<Party> party);
   
   private:
     std::unordered_map<uint32_t, std::shared_ptr<Party>> cache;
