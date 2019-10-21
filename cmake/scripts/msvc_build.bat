d1c3c04a7d94735fac4959ad2298b440e7ba2a57
Raven <7156279+RavenX8@users.noreply.github.com>
2019-10-21 20:16:06 +0000

    updated appveyor build paths

--- cmake/scripts/msvc_build.bat
+++ cmake/scripts/msvc_build.bat
@@ -1,5 +1,5 @@
 @call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-SET PATH=%PROJECT_PATH%\3rdparty\bin;%MYSQL_CONNECTOR_PATH%\lib;%MYSQL_PATH%\bin;%PYTHON_PATH%\Scripts;C:\OpenSSL-v111-Win64\bin;C:\Libraries\boost_1_67_0;%PATH%
+SET PATH=%PROJECT_PATH%\build\3rdparty\bin;%MYSQL_CONNECTOR_PATH%\lib;%MYSQL_PATH%\bin;%PYTHON_PATH%\Scripts;C:\OpenSSL-v111-Win64\bin;C:\Libraries\boost_1_67_0;%PATH%
 
 cp "%MYSQL_CONNECTOR_PATH%\lib\libmysql.dll" "%PROJECT_PATH%\build\bin\"
 
