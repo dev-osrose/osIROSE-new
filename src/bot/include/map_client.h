95c2972d9f96467f8aad99e2effd213327a18f73
L3nn0x <dragon83.super@gmail.com>
2019-12-20 16:09:42 +0000

    Updated spaces

--- src/bot/include/map_client.h
+++ src/bot/include/map_client.h
@@ -14,28 +14,28 @@
 #include "srv_whisper_chat.h"
 
 class MapClient : public Client {
-public:
-    MapClient(uint32_t sessionId, uint32_t sessionSeed, std::unique_ptr<Core::INetwork> sock);
-    virtual ~MapClient() = default;
+    public:
+        MapClient(uint32_t sessionId, uint32_t sessionSeed, std::unique_ptr<Core::INetwork> sock);
+        virtual ~MapClient() = default;
 
-private:
-    uint32_t sessionId_;
-    uint32_t sessionSeed_;
-    RoseCommon::PacketDispatcher<> dispatcher;
+    private:
+        uint32_t sessionId_;
+        uint32_t sessionSeed_;
+        RoseCommon::PacketDispatcher<> dispatcher;
 
-    template <typename T>
-    void register_dispatcher(std::function<void(const T&)>&& func) {
-        dispatcher.add_dispatcher(T::PACKET_ID, std::move(func));
-    }
+        template <typename T>
+        void register_dispatcher(std::function<void(const T&)>&& func) {
+            dispatcher.add_dispatcher(T::PACKET_ID, std::move(func));
+        }
 
-    virtual bool handlePacket(uint8_t *buffer) override;
+        virtual bool handlePacket(uint8_t *buffer) override;
 
-    void accept_reply(const RoseCommon::Packet::SrvAcceptReply&);
-    void join_server_reply(const RoseCommon::Packet::SrvJoinServerReply&);
-    void select_char_reply(const RoseCommon::Packet::SrvSelectCharReply&);
-    void inventory_data(const RoseCommon::Packet::SrvInventoryData&);
-    void quest_data(const RoseCommon::Packet::SrvQuestData&);
-    void billing_message(const RoseCommon::Packet::SrvBillingMessage&);
-    void change_map_reply(const RoseCommon::Packet::SrvChangeMapReply&);
-    void whisper_chat(const RoseCommon::Packet::SrvWhisperChat& packet);
+        void accept_reply(const RoseCommon::Packet::SrvAcceptReply&);
+        void join_server_reply(const RoseCommon::Packet::SrvJoinServerReply&);
+        void select_char_reply(const RoseCommon::Packet::SrvSelectCharReply&);
+        void inventory_data(const RoseCommon::Packet::SrvInventoryData&);
+        void quest_data(const RoseCommon::Packet::SrvQuestData&);
+        void billing_message(const RoseCommon::Packet::SrvBillingMessage&);
+        void change_map_reply(const RoseCommon::Packet::SrvChangeMapReply&);
+        void whisper_chat(const RoseCommon::Packet::SrvWhisperChat& packet);
 };
