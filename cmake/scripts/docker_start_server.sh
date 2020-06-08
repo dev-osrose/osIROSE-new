b0830c43eecac1fd543c802389e3f9fa58f59fab
Raven <7156279+RavenX8@users.noreply.github.com>
2020-06-08 01:25:04 -0400

    updated docker start script

--- cmake/scripts/docker_start_server.sh
+++ cmake/scripts/docker_start_server.sh
@@ -1,82 +1,19 @@
 #!/bin/bash
+set -e
 
-if [ -n "$CONFIG_FILE" ]; then
-  CONFIG_FILE="-f $CONFIG_FILE"
-else
-  CONFIG_FILE="-f /config/login-server.json"
-fi
+[ -z "$CONFIG_FILE" ] && CONFIG_FILE="-f /srv/osirose/server.json" || CONFIG_FILE="-f $CONFIG_FILE"
+[ -z "$URL" ] && AUTOCONFIG_URL="" || AUTOCONFIG_URL="--url $URL"
+[ -z "$CORE_PATH" ] && CORE_PATH="" || CORE_PATH="--core_path $CORE_PATH"
+[ -z "$DB_HOST" ] && DB_HOST="" || DB_HOST="--db_host $DB_HOST"
+[ -z "$DB_PORT" ] && DB_PORT="" || DB_PORT="--db_port $DB_PORT"
+[ -z "$MYSQL_DATABASE" ] && DB_NAME="" || DB_NAME="--db_name $MYSQL_DATABASE"
+[ -z "$MYSQL_USER" ] && DB_USER_NAME="" || DB_USER_NAME="--db_user $MYSQL_USER"
+[ -z "$MYSQL_PASSWORD" ] && DB_USER_PASSWORD="" || DB_USER_PASSWORD="--db_pass $MYSQL_PASSWORD"
+[ -z "$SERVER_ISC_PORT" ] && SERVER_ISC_PORT="" || SERVER_ISC_PORT="--isc_port $SERVER_ISC_PORT"
+[ -z "$AUTOCONFIG_URL" ] && AUTOCONFIG_URL="" || AUTOCONFIG_URL="--url $AUTOCONFIG_URL"
+[ -z "$EXTERNAL_IP" ] && EXTERNAL_IP="" || EXTERNAL_IP="--external_ip $EXTERNAL_IP"
+[ -z "$LISTEN_IP" ] && LISTEN_IP="" || LISTEN_IP="--client_ip $LISTEN_IP"
 
-if [ -n "$URL" ]; then
-  AUTOCONFIG_URL="--url $URL"
-else
-  AUTOCONFIG_URL=""
-fi
-
-if [ -n "$CORE_PATH" ]; then
-  CORE_PATH="--core_path $CORE_PATH"
-else
-  CORE_PATH=""
-fi
-
-if [ -n "$DB_HOST" ]; then
-  DB_HOST="--db_host $DB_HOST"
-else
-  DB_HOST=""
-fi
-if [ -n "$DB_PORT" ]; then
-  DB_PORT="--db_port $DB_PORT"
-else
-  DB_PORT=""
-fi
-if [ -n "$DB_NAME" ]; then
-  DB_NAME="--db_name $DB_NAME"
-else
-  DB_NAME=""
-fi
-if [ -n "$DB_USER_NAME" ]; then
-  DB_USER_NAME="--db_user $DB_USER_NAME"
-else
-  DB_USER_NAME=""
-fi
-if [ -n "$DB_USER_PASSWORD" ]; then
-  DB_USER_PASSWORD="--db_pass $DB_USER_PASSWORD"
-else
-  DB_USER_PASSWORD=""
-fi
-
-if [ -n "$SERVER_ISC_PORT" ]; then
-  SERVER_ISC_PORT="--isc_port $SERVER_ISC_PORT"
-else
-  SERVER_ISC_PORT=""
-fi
-
-if [ -n "$AUTOCONFIG_URL" ]; then
-  AUTOCONFIG_URL="--url $AUTOCONFIG_URL"
-else
-  AUTOCONFIG_URL=""
-fi
-
-if [ -n "$EXTERNAL_IP" ]; then
-  EXTERNAL_IP="--external_ip $EXTERNAL_IP"
-else
-  EXTERNAL_IP=""
-fi
-
-if [ -n "$LISTEN_IP" ]; then
-  LISTEN_IP="--client_ip $LISTEN_IP"
-else
-  LISTEN_IP=""
-fi
-
-# Check to see if we have a server set
-if [ -n "$SERVER" ]; then
-  if [ -e "$SERVER" ]; then
-      exec ./"$SERVER" "$CONFIG_FILE $LISTEN_IP $EXTERNAL_IP $AUTOCONFIG_URL $SERVER_ISC_PORT $DB_HOST $DB_PORT $DB_NAME $DB_USER_NAME $DB_USER_PASSWORD"
-  else
-    echo "$SERVER does not exist."
-    exit 1
-  fi
-else
-  echo "SERVER Env variable is not set exiting."
-  exit 2
-fi
+[ -z "$SERVER" ] && { echo "SERVER Env variable is not set exiting."; exit 1; }
+[ -e "$SERVER" ] || { echo "$SERVER does not exist."; exit 2; }
+exec ./"$SERVER" $CONFIG_FILE $LISTEN_IP $EXTERNAL_IP $AUTOCONFIG_URL $SERVER_ISC_PORT $DB_HOST $DB_PORT $DB_NAME $DB_USER_NAME $DB_USER_PASSWORD
