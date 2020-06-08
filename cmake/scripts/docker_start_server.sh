1d4b1213a93d1eda0c05c3549e9f1086b99e8101
Raven <7156279+RavenX8@users.noreply.github.com>
2020-06-08 10:02:44 -0400

    updated docker start script for testing

--- cmake/scripts/docker_start_server.sh
+++ cmake/scripts/docker_start_server.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
+set -v
 
 [ -z "$CONFIG_FILE" ] && CONFIG_FILE="-f /srv/osirose/server.json" || CONFIG_FILE="-f $CONFIG_FILE"
 [ -z "$URL" ] && AUTOCONFIG_URL="" || AUTOCONFIG_URL="--url $URL"
+[ -z "$AUTOCONFIG_URL" ] && AUTOCONFIG_URL="" || AUTOCONFIG_URL="--url $AUTOCONFIG_URL"
 [ -z "$CORE_PATH" ] && CORE_PATH="" || CORE_PATH="--core_path $CORE_PATH"
 [ -z "$DB_HOST" ] && DB_HOST="" || DB_HOST="--db_host $DB_HOST"
 [ -z "$DB_PORT" ] && DB_PORT="" || DB_PORT="--db_port $DB_PORT"
@@ -10,10 +12,9 @@ set -e
 [ -z "$MYSQL_USER" ] && DB_USER_NAME="" || DB_USER_NAME="--db_user $MYSQL_USER"
 [ -z "$MYSQL_PASSWORD" ] && DB_USER_PASSWORD="" || DB_USER_PASSWORD="--db_pass $MYSQL_PASSWORD"
 [ -z "$SERVER_ISC_PORT" ] && SERVER_ISC_PORT="" || SERVER_ISC_PORT="--isc_port $SERVER_ISC_PORT"
-[ -z "$AUTOCONFIG_URL" ] && AUTOCONFIG_URL="" || AUTOCONFIG_URL="--url $AUTOCONFIG_URL"
 [ -z "$EXTERNAL_IP" ] && EXTERNAL_IP="" || EXTERNAL_IP="--external_ip $EXTERNAL_IP"
 [ -z "$LISTEN_IP" ] && LISTEN_IP="" || LISTEN_IP="--client_ip $LISTEN_IP"
 
 [ -z "$SERVER" ] && { echo "SERVER Env variable is not set exiting."; exit 1; }
 [ -e "$SERVER" ] || { echo "$SERVER does not exist."; exit 2; }
-exec ./"$SERVER" $CONFIG_FILE $LISTEN_IP $EXTERNAL_IP $AUTOCONFIG_URL $SERVER_ISC_PORT $DB_HOST $DB_PORT $DB_NAME $DB_USER_NAME $DB_USER_PASSWORD
+exec ./"$SERVER" $CONFIG_FILE $CORE_PATH $LISTEN_IP $EXTERNAL_IP $AUTOCONFIG_URL $SERVER_ISC_PORT $DB_HOST $DB_PORT $DB_NAME $DB_USER_NAME $DB_USER_PASSWORD
