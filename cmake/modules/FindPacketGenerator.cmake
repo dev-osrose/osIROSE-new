420907740b9059d38f6f6ce37259a161c6c19227
Raven <7156279+RavenX8@users.noreply.github.com>
2020-08-15 14:39:05 -0400

    Added mariadb to search paths Fixed some cmake warnings

--- cmake/modules/FindPacketGenerator.cmake
+++ cmake/modules/FindPacketGenerator.cmake
@@ -11,10 +11,10 @@ find_program( IDL_GEN_EXE_PATH
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LUA_FOUND to TRUE if
 # all listed variables are TRUE
-find_package_handle_standard_args(IDL
+find_package_handle_standard_args(PacketGenerator
                                   REQUIRED_VARS IDL_GEN_EXE_PATH)
                                   
-if(IDL_FOUND AND NOT TARGET utils::packet_generator)
+if(PacketGenerator_FOUND AND NOT TARGET utils::packet_generator)
   add_executable(utils::packet_generator IMPORTED GLOBAL)
   add_dependencies(utils::packet_generator ${IDL_GEN_EXE_PATH})
   set_target_properties(utils::packet_generator PROPERTIES IMPORTED_LOCATION ${IDL_GEN_EXE_PATH})
