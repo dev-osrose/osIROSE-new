20a7370e100ef1aef0cb3fc0f7cb90677e6033a0
RavenX8 <7156279+RavenX8@users.noreply.github.com>
2023-09-13 14:56:22 -0400

    Updated path to sqlpp11 ddl2cpp script

--- cmake/modules/SQLPPGenerator.cmake
+++ cmake/modules/SQLPPGenerator.cmake
@@ -94,7 +94,7 @@ function(GENERATE_SQL_HEADER HDRS)
       OUTPUT "${H_FILE}"
       COMMAND ${CMAKE_COMMAND} -E make_directory ${OUTPATH}
       COMMAND Python::Interpreter
-      ARGS ${SQLPP11_EXE_PATH} ${MATCH_PATH} "${OUTPATH}/${ARG_NAME}" ${ARG_NAME}
+       ARGS ${sqlpp11_SOURCE_DIR}/scripts/ddl2cpp ${MATCH_PATH} "${OUTPATH}/${ARG_NAME}" ${ARG_NAME}
       DEPENDS ${ABS_FILE} sqlpp11::sqlpp11 sqlpp11::mysql
       COMMENT "Running C++ sqlpp11 ddl12cpp compiler on ${MATCH_PATH} with root ${SQLROOT}, generating: ${H_FILE}"
       VERBATIM)
