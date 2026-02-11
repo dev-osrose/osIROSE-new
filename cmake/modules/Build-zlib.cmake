05b27e32f71fe1980474b9de091be18d943b652b
RavenX8 <7156279+RavenX8@users.noreply.github.com>
2026-02-10 21:42:32 -0500

    Fixed cmake generation issue on windows

--- cmake/modules/Build-zlib.cmake
+++ cmake/modules/Build-zlib.cmake
@@ -1,42 +1,21 @@
-set(ZLIB_INSTALL_DIR ${CMAKE_THIRD_PARTY_DIR})
+find_package(ZLIB QUIET)
 
-set(_byproducts
-  ${ZLIB_INSTALL_DIR}/zlib.lib
-  ${ZLIB_INSTALL_DIR}/libzlib.a
-)
+if(NOT ZLIB_FOUND)
+  include(FetchContent)
 
-ExternalProject_Add(
-  zlib
-  URL https://www.zlib.net/zlib13.zip
-  URL_HASH SHA256=c561d09347f674f0d72692e7c75d9898919326c532aab7f8c07bb43b07efeb38
-  DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-  CMAKE_ARGS -G${CMAKE_GENERATOR} -DCMAKE_INSTALL_PREFIX=${ZLIB_INSTALL_DIR}
-  BUILD_BYPRODUCTS ${_byproducts}
-  INSTALL_DIR ${ZLIB_INSTALL_DIR}
-)
+  FetchContent_Declare(
+    zlib
+    URL https://www.zlib.net/current/zlib.tar.gz
+  )
 
-ExternalProject_Get_Property(
-  zlib
-  source_dir
-  install_dir
-)
+  set(BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
+  FetchContent_MakeAvailable(zlib)
 
-set(ZLIB_INCLUDE_DIRS "${source_dir}")
-if(NOT MINGW AND WIN32)
-  set(ZLIB_LIBRARY_DIR "${install_dir}")
-  set(ZLIB_LIBRARY "${install_dir}/zlib.lib")
-  set(ZLIB_LIBRARIES "${install_dir}/zlib.lib" "${ZLIB_LIBRARIES}")
-else()
-  set(ZLIB_LIBRARY_DIR "${install_dir}")
-  set(ZLIB_LIBRARY "${install_dir}/zlib.a")
-  set(ZLIB_LIBRARIES "${install_dir}/zlib.a" "${ZLIB_LIBRARIES}")
+  if(TARGET zlibstatic)
+    add_library(ZLIB::ZLIB ALIAS zlibstatic)
+  elseif(TARGET zlib)
+    add_library(ZLIB::ZLIB ALIAS zlib)
+  endif()
+
+  set(ZLIB_FOUND TRUE CACHE BOOL "" FORCE)
 endif()
-
-if(NOT TARGET ZLIB::ZLIB)
-  add_library(ZLIB::ZLIB INTERFACE IMPORTED)
-  add_dependencies(ZLIB::ZLIB zlib)
-  set_target_properties(ZLIB::ZLIB PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${ZLIB_INCLUDE_DIR}")
-  set_target_properties(ZLIB::ZLIB PROPERTIES INTERFACE_LINK_LIBRARIES "${ZLIB_LIBRARIES}")
-endif()
-
-mark_as_advanced( ZLIB_INCLUDE_DIRS ZLIB_LIBRARIES ZLIB_LIBRARY ZLIB_LIBRARY_PATH )
--- cmake/modules/BuildCURL.cmake
+++ cmake/modules/BuildCURL.cmake
@@ -1,61 +1,33 @@
-set(CURL_INSTALL_DIR ${CMAKE_THIRD_PARTY_DIR})
+include(FetchContent)
 
-set(_byproducts
-    ${CURL_INSTALL_DIR}/lib/libcurl_imp.lib
-    ${CURL_INSTALL_DIR}/lib/libcurl-d_imp.lib
-    ${CURL_INSTALL_DIR}/lib/libcurl.a
-    ${CURL_INSTALL_DIR}/lib/libcurl.so
-  )
+# Prefer system curl first (optional but recommended)
+find_package(CURL QUIET)
 
-if(WIN32)
-  ExternalProject_Add(
+if(NOT CURL_FOUND)
+
+  FetchContent_Declare(
     curl
     GIT_REPOSITORY https://github.com/curl/curl.git
-    GIT_TAG cb529b713f4882ac65a074ae8d87faa41d19168e
-    CMAKE_ARGS -G ${CMAKE_GENERATOR} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${CURL_INSTALL_DIR} -DBUILD_TESTING=OFF -DHTTP_ONLY=ON
-	DEPENDS ZLIB::ZLIB
-    BUILD_BYPRODUCTS ${_byproducts}
-    INSTALL_DIR ${CURL_INSTALL_DIR}
+    GIT_TAG curl-8_18_0
   )
-else()
-  ExternalProject_Add(
-    curl
-    GIT_REPOSITORY https://github.com/curl/curl.git
-    GIT_TAG cb529b713f4882ac65a074ae8d87faa41d19168e
-    CMAKE_ARGS -G ${CMAKE_GENERATOR} -DCMAKE_INSTALL_PREFIX=${CURL_INSTALL_DIR} -DBUILD_TESTING=OFF -DHTTP_ONLY=ON
-	DEPENDS ZLIB::ZLIB
-    BUILD_BYPRODUCTS ${_byproducts}
-    INSTALL_DIR ${CURL_INSTALL_DIR}
-  )
-endif()
 
-ExternalProject_Get_Property(
-  curl
-  install_dir
-)
+  # Configure curl options BEFORE MakeAvailable
+  set(BUILD_TESTING OFF CACHE BOOL "" FORCE)
+  set(BUILD_CURL_EXE OFF CACHE BOOL "" FORCE)
+  set(HTTP_ONLY ON CACHE BOOL "" FORCE)
+  set(BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE) # static (recommended)
+  set(CURL_USE_LIBPSL OFF CACHE BOOL "" FORCE)
 
-set(CURL_INCLUDE_DIR "${install_dir}/include")
-if(WIN32)
-  if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
-    set(CURL_LIBRARY "${install_dir}/lib/libcurl-d_imp.lib")
-  elseif("${CMAKE_BUILD_TYPE}" STREQUAL "$<CONFIG>")
-    set(CURL_LIBRARY "${install_dir}/lib/libcurl$<$<CONFIG:Debug>:-d>_imp.lib")
-  else()
-    set(CURL_LIBRARY "${install_dir}/lib/libcurl_imp.lib")
+  FetchContent_MakeAvailable(curl)
+
+  # curl defines different targets depending on version
+  # Normalize to CURL::libcurl
+  if(TARGET libcurl AND NOT TARGET CURL::libcurl)
+    add_library(CURL::libcurl ALIAS libcurl)
   endif()
-  set(CURL_LIBRARIES "${CURL_LIBRARY}")
-  set(CURL_INSTALL_LIBS "${install_dir}/bin/libcurl.dll")
-else()
-  set(CURL_LIBRARY "${install_dir}/lib/libcurl.so")
-  set(CURL_LIBRARIES "${CURL_LIBRARY}")
-  set(CURL_INSTALL_LIBS "${install_dir}/lib/libcurl.so")
-endif()
 
-if(NOT TARGET CURL::libcurl)
-    add_library(CURL::libcurl INTERFACE IMPORTED)
-    add_dependencies(CURL::libcurl curl)
-    set_target_properties(CURL::libcurl PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${CURL_INCLUDE_DIR}")
-    set_target_properties(CURL::libcurl PROPERTIES INTERFACE_LINK_LIBRARIES "${CURL_LIBRARIES}")
-endif()
+  # Satisfy legacy FindCURL users
+  set(CURL_FOUND TRUE CACHE BOOL "" FORCE)
+  set(CURL_LIBRARIES CURL::libcurl CACHE STRING "" FORCE)
 
-mark_as_advanced( CURL_INCLUDE_DIR CURL_LIBRARIES CURL_LIBRARY CURL_INSTALL_LIBS )
+endif()
