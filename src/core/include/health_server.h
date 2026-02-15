0e43bfc13302e75a17a06467e78a66dc2847f0e1
raven <7156279+RavenX8@users.noreply.github.com>
2026-02-15 01:52:47 -0500

    refactor: migrate HealthServer to use ASIO. Should have just made it using asio in the first place.

--- src/core/include/health_server.h
+++ src/core/include/health_server.h
@@ -1,43 +1,47 @@
 #ifndef _HEALTH_SERVER_H_
 #define _HEALTH_SERVER_H_
 
-#include <string>
-#include <functional>
+#include <asio.hpp>
 #include <atomic>
+#include <functional>
+#include <mutex>
+#include <string>
 #include <thread>
 #include <vector>
-#include <mutex>
 
 namespace Core {
 
 class HealthServer {
-public:
-    using HealthCheck = std::function<std::pair<bool, std::string>()>;
+ public:
+  using HealthCheck = std::function<std::pair<bool, std::string>()>;
 
-    HealthServer();
-    ~HealthServer();
+  HealthServer();
+  ~HealthServer();
 
-    void start(uint16_t port);
-    void stop();
+  void start(uint16_t port);
+  void stop();
 
-    void addCheck(HealthCheck check);
-    void setUnhealthy(const std::string& reason);
+  void addCheck(HealthCheck check);
+  void setUnhealthy(const std::string& reason);
 
-private:
-    void run();
-    void handleRequest(int clientSocket);
+ private:
+  void run();
+  void handleAccept();
+  void handleRequest(std::shared_ptr<asio::ip::tcp::socket> socket);
 
-    uint16_t port_;
-    std::atomic<bool> running_;
-    std::atomic<bool> forcedUnhealthy_;
-    std::string unhealthyReason_;
-    std::mutex reasonMutex_;
-    
-    std::vector<HealthCheck> checks_;
-    std::thread serverThread_;
-    int listenSocket_;
+  uint16_t port_;
+  std::atomic<bool> running_;
+  std::atomic<bool> forcedUnhealthy_;
+  std::string unhealthyReason_;
+  std::mutex reasonMutex_;
+
+  std::vector<HealthCheck> checks_;
+  std::thread serverThread_;
+
+  asio::io_context io_context_;
+  std::unique_ptr<asio::ip::tcp::acceptor> acceptor_;
 };
 
-} // namespace Core
+}  // namespace Core
 
 #endif // _HEALTH_SERVER_H_
